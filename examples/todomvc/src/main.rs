1d9ce847f21a1822989365144843c65fe149667c
Denis Kolodin <deniskolodin@gmail.com>
2017-12-17 20:48:05 +0300

    Add sections to TodoMVC template

--- examples/todomvc/src/main.rs
+++ examples/todomvc/src/main.rs
@@ -1,3 +1,5 @@
+#![recursion_limit="128"]
+
 #[macro_use]
 extern crate yew;
 
@@ -5,6 +7,16 @@ use yew::html;
 
 struct Model {
     entries: Vec<Entry>,
+}
+
+impl Model {
+    fn total(&self) -> usize {
+        self.entries.len()
+    }
+
+    fn total_completed(&self) -> usize {
+        self.entries.iter().filter(|entry| entry.completed).count()
+    }
 }
 
 struct Entry {
@@ -33,30 +45,49 @@ fn view(model: &Model) -> html::Html<Msg> {
         <div class="todomvc-wrapper",>
             <section class="todoapp",>
                 <header class="header",>
-                    <h1>{ "Todos" }</h1>
-                    { viewInput() }
-                    { viewEntries(&model.entries) }
+                    <h1>{ "todos" }</h1>
+                    { view_input() }
+                    { view_entries(&model.entries) }
                 </header>
+                <section class="main",>
+                </section>
+                <footer class="footer",>
+                    <span class="todo-count",>
+                        <strong>{ model.total() }</strong>
+                        { " item(s) left" }
+                    </span>
+                    <ul class="filters",>
+                        <li><a>{ "All" }</a></li>
+                        <li><a>{ "Active" }</a></li>
+                        <li><a>{ "Completed" }</a></li>
+                    </ul>
+                    <button class="clear-completed",>{ format!("Clear completed ({})", model.total_completed()) }</button>
+                </footer>
             </section>
+            <footer class="info",>
+                <p>{ "Double-click to edit a todo" }</p>
+                <p>{ "Written by " }<a>{ "Denis Kolodin" }</a></p>
+                <p>{ "Part of " }<a>{ "TodoMVC" }</a></p>
+            </footer>
         </div>
     }
 }
 
-fn viewInput() -> html::Html<Msg> {
+fn view_input() -> html::Html<Msg> {
     html! {
         <input class="new-todo", (onclick)=|_| Msg::Add, />
     }
 }
 
-fn viewEntries(entries: &Vec<Entry>) -> html::Html<Msg> {
+fn view_entries(entries: &Vec<Entry>) -> html::Html<Msg> {
     html! {
         <section class="main",>
-            { for entries.iter().map(viewEntry) }
+            { for entries.iter().map(view_entry) }
         </section>
     }
 }
 
-fn viewEntry(entry: &Entry) -> html::Html<Msg> {
+fn view_entry(entry: &Entry) -> html::Html<Msg> {
     html! {
         <li>{ &entry.description }</li>
     }
