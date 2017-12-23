6cf274eba4bb2cfda78482b5f1aa509ffcad0bdd
Denis Kolodin <deniskolodin@gmail.com>
2017-12-23 23:54:25 +0300

    Add counter demo

---
+++ examples/counter/src/main.rs
@@ -0,0 +1,46 @@
+extern crate chrono;
+#[macro_use]
+extern crate yew;
+
+use chrono::prelude::*;
+use yew::html;
+
+struct Model {
+    value: i64,
+}
+
+enum Msg {
+    Increment,
+    Decrement,
+}
+
+fn update(model: &mut Model, msg: Msg) {
+    match msg {
+        Msg::Increment => {
+            model.value = model.value + 1;
+        }
+        Msg::Decrement => {
+            model.value = model.value - 1;
+        }
+    }
+}
+
+fn view(model: &Model) -> html::Html<Msg> {
+    html! {
+        <div>
+            <nav class="menu",>
+                <button (onclick)=|_| Msg::Increment,>{ "Increment" }</button>
+                <button (onclick)=|_| Msg::Decrement,>{ "Decrement" }</button>
+            </nav>
+            <p>{ model.value }</p>
+            <p>{ Local::now() }</p>
+        </div>
+    }
+}
+
+fn main() {
+    let model = Model {
+        value: 0,
+    };
+    html::program(model, update, view);
+}
