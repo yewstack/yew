6dcecbc52ec3b62d35c83c91379cd4a742c1d94b
Denis Kolodin <deniskolodin@gmail.com>
2017-12-30 21:01:26 +0300

    Add websockets example to dashboard demo

---
+++ examples/dashboard/README.md
@@ -0,0 +1,19 @@
+# Yew Dashboard Example (Fetch & WebSocket)
+
+## Build and run the client
+
+Enter to `client` folder and run:
+
+```sh
+cargo web start
+```
+
+## Build and run the server
+
+Enter to `server` folder and run:
+
+```sh
+cargo build && ./target/debug/server
+```
+
+It opens `ws://localhost:9001/` websocket endpoint.
---
+++ examples/dashboard/client/src/main.rs
@@ -0,0 +1,113 @@
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate yew;
+
+use yew::html::*;
+use yew::services::Task;
+use yew::services::format::{Nothing, Json};
+use yew::services::fetch::{FetchService, Method};
+use yew::services::websocket::{WebSocketService, WebSocketHandle};
+
+struct Model {
+    fetching: bool,
+    data: Option<Status>,
+    ws: Option<WebSocketHandle>,
+}
+
+enum WsAction {
+    Connect,
+    SendData,
+    Disconnect,
+}
+
+enum Msg {
+    FetchData,
+    WsAction(WsAction),
+    DataReady(Result<Status, ()>),
+}
+
+impl From<WsAction> for Msg {
+    fn from(action: WsAction) -> Self {
+        Msg::WsAction(action)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Status {
+    value: u32,
+}
+
+
+fn update(context: &mut Context<Msg>, model: &mut Model, msg: Msg) {
+    match msg {
+        Msg::FetchData => {
+            context.fetch(Method::Get, "./data.json", Nothing, |Json(data)| Msg::DataReady(data));
+        }
+        Msg::WsAction(action) => {
+            match action {
+                WsAction::Connect => {
+                    let handle = context.ws_connect("ws://localhost:9001/", |Json(data)| Msg::DataReady(data));
+                    model.ws = Some(handle);
+                }
+                WsAction::SendData => {
+                    let data = Status {
+                        value: 321,
+                    };
+                    model.ws.as_mut().unwrap().send(Json(&data));
+                }
+                WsAction::Disconnect => {
+                    model.ws.take().unwrap().cancel();
+                }
+            }
+        }
+        Msg::DataReady(response) => {
+            model.fetching = false;
+            match response {
+                Ok(data) => {
+                    model.data = Some(data);
+                }
+                Err(_) => {
+                }
+            }
+        }
+    }
+}
+
+fn view(model: &Model) -> Html<Msg> {
+    html! {
+        <div>
+            <nav class="menu",>
+                <button onclick=|_| Msg::FetchData,>{ "Fetch Data" }</button>
+                { view_data(model) }
+                <button disabled=model.ws.is_some(),
+                        onclick=|_| WsAction::Connect.into(),>{ "Connect To WebSocket" }</button>
+                <button disabled=model.ws.is_none(),
+                        onclick=|_| WsAction::SendData.into(),>{ "Send To WebSocket" }</button>
+                <button disabled=model.ws.is_none(),
+                        onclick=|_| WsAction::Disconnect.into(),>{ "Close WebSocket connection" }</button>
+            </nav>
+        </div>
+    }
+}
+
+fn view_data(model: &Model) -> Html<Msg> {
+    if let Some(ref data) = model.data {
+        html! {
+            <p>{ data.value }</p>
+        }
+    } else {
+        html! {
+            <p>{ "Data hasn't fetched yet." }</p>
+        }
+    }
+}
+
+fn main() {
+    let model = Model {
+        fetching: false,
+        data: None,
+        ws: None,
+    };
+    program(model, update, view);
+}
