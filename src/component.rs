1b18a07fb81618ad4e162131d3e94124f9e5277d
Denis Kolodin <deniskolodin@gmail.com>
2018-01-08 18:36:33 +0300

    Add local sender to keep a context

--- src/component.rs
+++ src/component.rs
@@ -1,8 +1,8 @@
-use html::Html;
+use html::{Html, LocalSender};
 
 pub trait Component<CTX>: Default {
     type Msg;
-    fn update(&mut self, msg: Self::Msg, context: &mut CTX);
+    fn update(&mut self, msg: Self::Msg, context: LocalSender<Self::Msg, CTX>);
     fn view(&self) -> Html<Self::Msg, CTX>;
 }
 
