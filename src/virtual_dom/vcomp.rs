fbaf010de1a5f1d2eafe8e90531e0a40423c5539
Denis Kolodin <deniskolodin@gmail.com>
2018-01-10 02:31:25 +0300

    Add ScopeBuilder

--- src/virtual_dom/vcomp.rs
+++ src/virtual_dom/vcomp.rs
@@ -1,33 +1,29 @@
 //! This module contains the implementation of a virtual component `VComp`.
 
-use std::marker::PhantomData;
 use stdweb::web::Element;
-use html::{Scope, SharedContext, Component};
+use html::{ScopeBuilder, SharedContext, Component};
 
 /// A virtual component.
 pub struct VComp<CTX, COMP: Component<CTX>> {
-    mounter: Box<Fn(Element, SharedContext<CTX>)>,
-    _msg: PhantomData<COMP>,
+    builder: Option<ScopeBuilder<CTX, COMP>>,
 }
 
 impl<CTX: 'static, COMP: Component<CTX>> VComp<CTX, COMP> {
     /// This method prepares a generator to make a new instance of the `Component`.
     pub fn lazy() -> Self {
-        let generator = |element, context| {
-            let app: Scope<CTX, COMP> = Scope::reuse(context);
-            app.mount(element);
-        };
+        let builder: ScopeBuilder<CTX, COMP> = ScopeBuilder::new();
+        let prop_sender = builder.sender();
         VComp {
-            mounter: Box::new(generator),
-            _msg: PhantomData,
+            builder: Some(builder),
         }
     }
 }
 
-impl<CTX, COMP: Component<CTX>> VComp<CTX, COMP> {
+impl<CTX: 'static, COMP: Component<CTX>> VComp<CTX, COMP> {
     /// This methods mount a virtual component with a generator created with `lazy` call.
-    pub fn mount(&self, element: &Element, context: SharedContext<CTX>) {
-        (self.mounter)(element.clone(), context);
+    pub fn mount(&mut self, element: &Element, context: SharedContext<CTX>) {
+        let builder = self.builder.take().expect("tried to mount component twice");
+        builder.build(context).mount(element.clone());
     }
 }
 
