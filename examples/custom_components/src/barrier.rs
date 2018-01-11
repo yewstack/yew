b5949c349f3a030ff810748b4c1cd8fab2e5747b
Denis Kolodin <deniskolodin@gmail.com>
2018-01-11 22:23:39 +0300

    Add scopes hierarchy example
    Shows how to send a message to a parent scope

---
+++ examples/custom_components/src/barrier.rs
@@ -0,0 +1,76 @@
+use yew::html::*;
+use button::Button;
+
+pub struct Barrier {
+    limit: u32,
+    counter: u32,
+    onsignal: Option<Callback<()>>,
+}
+
+pub enum Msg {
+    ChildClicked,
+}
+
+#[derive(PartialEq, Clone)]
+pub struct Props {
+    pub limit: u32,
+    pub onsignal: Option<Callback<()>>,
+}
+
+impl Default for Props {
+    fn default() -> Self {
+        Props {
+            limit: 0,
+            onsignal: None,
+        }
+    }
+}
+
+
+impl<CTX: 'static> Component<CTX> for Barrier {
+    type Msg = Msg;
+    type Properties = Props;
+
+    fn create(_: &mut ScopeRef<CTX, Self>) -> Self {
+        Barrier {
+            limit: 0,
+            counter: 0,
+            onsignal: None,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Msg, _: &mut ScopeRef<CTX, Self>) -> ShouldUpdate {
+        match msg {
+            Msg::ChildClicked => {
+                self.counter += 1;
+                if self.counter >= self.limit {
+                    if let Some(ref mut callback) = self.onsignal {
+                        callback.emit(());
+                        self.counter = 0;
+                    }
+                }
+            }
+        }
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties, _: &mut ScopeRef<CTX, Self>) -> ShouldUpdate {
+        self.limit = props.limit;
+        self.onsignal = props.onsignal;
+        true
+    }
+
+    fn view(&self) -> Html<CTX, Self> {
+        html! {
+            <div>
+                <p>{ format!("{} on {} clicked", self.counter, self.limit) }</p>
+                <Button: onsignal=|_| Msg::ChildClicked, />
+                <Button: onsignal=|_| Msg::ChildClicked, />
+                <Button: onsignal=|_| Msg::ChildClicked, title="Middle", />
+                <Button: onsignal=|_| Msg::ChildClicked, />
+                <Button: onsignal=|_| Msg::ChildClicked, />
+            </div>
+        }
+    }
+}
+
---
+++ examples/custom_components/src/button.rs
@@ -0,0 +1,61 @@
+use yew::html::*;
+
+pub struct Button {
+    title: String,
+    onsignal: Option<Callback<()>>,
+}
+
+pub enum Msg {
+    Clicked,
+}
+
+#[derive(PartialEq, Clone)]
+pub struct Props {
+    pub title: String,
+    pub onsignal: Option<Callback<()>>,
+}
+
+impl Default for Props {
+    fn default() -> Self {
+        Props {
+            title: "Send Signal".into(),
+            onsignal: None,
+        }
+    }
+}
+
+impl<CTX: 'static> Component<CTX> for Button {
+    type Msg = Msg;
+    type Properties = Props;
+
+    fn create(_: &mut ScopeRef<CTX, Self>) -> Self {
+        Button {
+            title: "Send Signal".into(),
+            onsignal: None,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Msg, _: &mut ScopeRef<CTX, Self>) -> ShouldUpdate {
+        match msg {
+            Msg::Clicked => {
+                if let Some(ref mut callback) = self.onsignal {
+                    callback.emit(());
+                }
+            }
+        }
+        false
+    }
+
+    fn change(&mut self, props: Self::Properties, _: &mut ScopeRef<CTX, Self>) -> ShouldUpdate {
+        self.title = props.title;
+        self.onsignal = props.onsignal;
+        true
+    }
+
+    fn view(&self) -> Html<CTX, Self> {
+        html! {
+            <button onclick=|_| Msg::Clicked,>{ &self.title }</button>
+        }
+    }
+}
+
