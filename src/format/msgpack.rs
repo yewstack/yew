3fb50e150c368355672d74590744390920e725f2
Denis Kolodin <deniskolodin@gmail.com>
2018-05-05 15:13:10 +0300

    Add MessagePack serialization format

---
+++ src/format/msgpack.rs
@@ -0,0 +1,17 @@
+//! Contains an implementation of MessagePack serialization format.
+
+use rmp_serde;
+
+/// A representation of a MessagePack data. Use it as wrapper to
+/// set a format you want to use for conversion:
+///
+/// ```rust
+/// // Converts (lazy) data to a MsgPack
+/// let dump = MsgPack(&data);
+///
+/// // Converts MessagePack string to a data (lazy).
+/// let MsgPack(data) = dump;
+/// ```
+pub struct MsgPack<T>(pub T);
+
+binary_format!(MsgPack based on rmp_serde);
