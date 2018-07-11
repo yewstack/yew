1ed58d54facb414f6b90398ed2029b595e5cf551
Denis Kolodin <deniskolodin@gmail.com>
2018-07-11 23:21:05 +0300

    Support text node as a root

---
+++ tests/vtext_test.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate yew;
+
+use yew::prelude::*;
+use yew::virtual_dom::VNode;
+
+struct Comp;
+
+impl Component for Comp {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Comp
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        unimplemented!();
+    }
+}
+
+impl Renderable<Comp> for Comp {
+    fn view(&self) -> Html<Self> {
+        unimplemented!();
+    }
+}
+
+#[test]
+fn text_as_root() {
+    let _: VNode<Comp> = html! {
+        { "Text Node As Root" }
+    };
+}
+
