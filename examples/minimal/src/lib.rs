a0192243ae2371199d0f4e8b3f533c257e4ebb70
Denis Kolodin <deniskolodin@gmail.com>
2018-10-19 19:13:30 +0300

    Add minimal example
    Helps to create your own apps from scratch.

---
+++ examples/minimal/src/lib.rs
@@ -0,0 +1,39 @@
+#[macro_use]
+extern crate yew;
+
+use yew::prelude::*;
+
+pub struct Model {
+}
+
+pub enum Msg {
+    Click,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model { }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => {
+            }
+        }
+        true
+    }
+}
+
+impl Renderable<Model> for Model {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <div>
+                <button onclick=|_| Msg::Click,>{ "Click" }</button>
+            </div>
+        }
+    }
+}
+
