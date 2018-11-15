e615a79dd5b62cc2bc79a1f569980fea898b6449
Denis Kolodin <deniskolodin@gmail.com>
2018-11-15 08:35:44 +0300

    Use Select component in Showcase example

--- examples/showcase/src/main.rs
+++ examples/showcase/src/main.rs
@@ -25,7 +25,7 @@ extern crate todomvc;
 extern crate two_apps;
 
 use strum::IntoEnumIterator;
-use std::str::FromStr;
+use yew::components::Select;
 use yew::prelude::*;
 use counter::Model as Counter;
 use crm::Model as Crm;
@@ -43,9 +43,8 @@ use timer::Model as Timer;
 use todomvc::Model as Todomvc;
 use two_apps::Model as TwoApps;
 
-#[derive(Debug, Display, EnumString, EnumIter)]
+#[derive(Clone, Debug, Display, EnumString, EnumIter, PartialEq)]
 enum Scene {
-    NotSelected,
     Counter,
     Crm,
     CustomComponents,
@@ -63,57 +62,47 @@ enum Scene {
     TwoApps,
 }
 
+struct Model {
+    scene: Option<Scene>,
+}
+
 enum Msg {
     SwitchTo(Scene),
 }
 
-impl Component for Scene {
+impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Scene::NotSelected
+        Self {
+            scene: None,
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::SwitchTo(scene) => {
-                *self = scene;
+                self.scene = Some(scene);
                 true
             }
         }
     }
 }
 
-impl Renderable<Scene> for Scene {
+impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
-        let _options = Scene::iter().map(|scene| {
-            html! {
-                <option value={ scene.to_string() }, > { scene.to_string() } </option>
-            }
-        });
-
         html! {
             <div id="fullscreen",>
                 <div id="left_pane",>
                     <h2>{ "Yew showcase" }</h2>
-                    <select size="20", value={Scene::NotSelected.to_string()},
-                        onchange=|cd| {
-                            let scene = match cd {
-                                ChangeData::Select(se) => se.value().unwrap(),
-                                _ => unreachable!()
-                            };
-                            match Scene::from_str(&scene) {
-                                Ok(scene) => Msg::SwitchTo(scene),
-                                _ => unreachable!(),
-                            }
-                        }
-                    , >
-                        { for _options }
-                    </select>
+                    <Select<Scene>:
+                        selected=self.scene.clone(),
+                        options=Scene::iter().collect::<Vec<_>>(),
+                        onchange=Msg::SwitchTo,
+                        />
                 </div>
                 <div id="right_pane",>
-                    <h2>{ self.to_string() }</h2>
                     { self.view_scene() }
                 </div>
             </div>
@@ -121,88 +110,89 @@ impl Renderable<Scene> for Scene {
     }
 }
 
-impl Scene {
+impl Model {
     fn view_scene(&self) -> Html<Self> {
-        match *self {
-            Scene::NotSelected => {
-                html! {
-                    <p>{ "Select the scene, please." }</p>
+        if let Some(scene) = self.scene.as_ref() {
+            match scene {
+                Scene::Counter => {
+                    html! {
+                        <Counter: />
+                    }
+                }
+                Scene::Crm => {
+                    html! {
+                        <Crm: />
+                    }
+                }
+                Scene::CustomComponents => {
+                    html! {
+                        <CustomComponents: />
+                    }
+                }
+                Scene::Dashboard => {
+                    html! {
+                        <Dashboard: />
+                    }
+                }
+                Scene::Fragments => {
+                    html! {
+                        <Fragments: />
+                    }
+                }
+                Scene::GameOfLife => {
+                    html! {
+                        <GameOfLife: />
+                    }
+                }
+                Scene::InnerHtml => {
+                    html! {
+                        <InnerHtml: />
+                    }
+                }
+                Scene::LargeTable => {
+                    html! {
+                        <LargeTable: />
+                    }
+                }
+                Scene::MountPoint => {
+                    html! {
+                        <MountPoint: />
+                    }
+                }
+                Scene::NpmAndRest => {
+                    html! {
+                        <NpmAndRest: />
+                    }
+                }
+                Scene::Routing => {
+                    html! {
+                        <Routing: />
+                    }
+                }
+                Scene::Textarea => {
+                    html! {
+                        <Textarea: />
+                    }
+                }
+                Scene::Timer => {
+                    html! {
+                        <Timer: />
+                    }
+                }
+                Scene::Todomvc => {
+                    html! {
+                        <Todomvc: />
+                    }
+                }
+                Scene::TwoApps => {
+                    html! {
+                        <TwoApps: />
+                    }
                 }
             }
-            Scene::Counter => {
-                html! {
-                    <Counter: />
-                }
-            }
-            Scene::Crm => {
-                html! {
-                    <Crm: />
-                }
-            }
-            Scene::CustomComponents => {
-                html! {
-                    <CustomComponents: />
-                }
-            }
-            Scene::Dashboard => {
-                html! {
-                    <Dashboard: />
-                }
-            }
-            Scene::Fragments => {
-                html! {
-                    <Fragments: />
-                }
-            }
-            Scene::GameOfLife => {
-                html! {
-                    <GameOfLife: />
-                }
-            }
-            Scene::InnerHtml => {
-                html! {
-                    <InnerHtml: />
-                }
-            }
-            Scene::LargeTable => {
-                html! {
-                    <LargeTable: />
-                }
-            }
-            Scene::MountPoint => {
-                html! {
-                    <MountPoint: />
-                }
-            }
-            Scene::NpmAndRest => {
-                html! {
-                    <NpmAndRest: />
-                }
-            }
-            Scene::Routing => {
-                html! {
-                    <Routing: />
-                }
-            }
-            Scene::Textarea => {
-                html! {
-                    <Textarea: />
-                }
-            }
-            Scene::Timer => {
-                html! {
-                    <Timer: />
-                }
-            }
-            Scene::Todomvc => {
-                html! {
-                    <Todomvc: />
-                }
-            }
-            Scene::TwoApps => {
-                html! {
-                    <TwoApps: />
-                }
+        } else {
+            html! {
+                <p>{ "Select the scene, please." }</p>
             }
         }
     }
@@ -213,7 +203,7 @@ fn main() {
     trace!("Initializing yew...");
     yew::initialize();
     trace!("Creating an application instance...");
-    let app: App<Scene> = App::new();
+    let app: App<Model> = App::new();
     trace!("Mount the App to the body of the page...");
     app.mount_to_body();
     trace!("Run");
