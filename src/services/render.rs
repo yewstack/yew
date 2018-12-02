5ba8767aebe1e8b4cfa4f2d6a5268b9e3c37ae78
Eduard Knyshov <edvorg@gmail.com>
2018-12-03 00:21:04 +0800

    Add render service

---
+++ src/services/render.rs
@@ -0,0 +1,66 @@
+//! This module contains the implementation of a service to
+//! request frame rendering
+
+use stdweb::Value;
+use stdweb::unstable::TryInto;
+use services::Task;
+use callback::Callback;
+
+/// A handle to cancel a render task.
+pub struct RenderTask(Option<Value>);
+
+/// A service to request animation frames.
+#[derive(Default)]
+pub struct RenderService {}
+
+impl RenderService {
+    /// Create a new service instance
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    /// Request animation frame. Callback will be notified when frame should be rendered.
+    pub fn request_animation_frame(&mut self, callback: Callback<f64>) -> RenderTask {
+        let callback = move |v| {
+            let time: f64 = match v {
+                Value::Number(n) => n.try_into().unwrap(),
+                _ => 0.0
+            };
+            callback.emit(time);
+        };
+        let handle = js! {
+            var callback = @{callback};
+            var action = function(time) {
+                callback(time);
+                callback.drop();
+            };
+            return {
+                render_id: requestAnimationFrame(action),
+                callback: callback,
+            };
+        };
+        RenderTask(Some(handle))
+    }
+}
+
+impl Task for RenderTask {
+    fn is_active(&self) -> bool {
+        self.0.is_some()
+    }
+    fn cancel(&mut self) {
+        let handle = self.0.take().expect("tried to cancel render twice");
+        js! { @(no_return)
+            var handle = @{handle};
+            cancelAnimationFrame(handle.timeout_id);
+            handle.callback.drop();
+        }
+    }
+}
+
+impl Drop for RenderTask {
+    fn drop(&mut self) {
+        if self.is_active() {
+            self.cancel();
+        }
+    }
+}
