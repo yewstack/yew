62b6fea76eca5ecbee913092bb297f37c076609a
Denis Kolodin <deniskolodin@gmail.com>
2019-02-01 20:18:53 +0300

    Make macro compatible with Rust 2018

--- examples/minimal/src/main.rs
+++ examples/minimal/src/main.rs
@@ -1,11 +1,5 @@
-extern crate yew;
-extern crate minimal;
-
-use yew::prelude::*;
-use minimal::Model;
-
 fn main() {
     yew::initialize();
-    App::<Model>::new().mount_to_body();
+    yew::App::<minimal::Model>::new().mount_to_body();
     yew::run_loop();
 }
