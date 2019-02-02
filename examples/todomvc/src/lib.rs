23f728aff95680837eba0e90503baba083fb7a39
Denis Kolodin <deniskolodin@gmail.com>
2019-02-02 19:11:05 +0300

    Fix styles of TodoMVC example

--- examples/todomvc/src/lib.rs
+++ examples/todomvc/src/lib.rs
@@ -190,8 +190,15 @@ impl Model {
 }
 
 fn view_entry((idx, entry): (usize, &Entry)) -> Html<Model> {
+    let mut class = "todo".to_string();
+    if entry.editing {
+        class.push_str(" editing");
+    }
+    if entry.completed {
+        class.push_str(" completed");
+    }
     html! {
-        <li class=if entry.editing == true { "editing" } else { "" },>
+        <li class=class,>
             <div class="view",>
                 <input class="toggle", type="checkbox", checked=entry.completed, onclick=|_| Msg::Toggle(idx), />
                 <label ondoubleclick=|_| Msg::ToggleEdit(idx),>{ &entry.description }</label>
