22ec126264eb2b5cc0e0869d63e1e015e6ae6c4b
Justin Starry <jstarry@users.noreply.github.com>
2019-05-25 09:55:46 -0400

    Merge html-common and html-impl crates

--- crates/html-common/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![recursion_limit = "128"]
-
-pub mod html_tree;
-
-use syn::buffer::Cursor;
-
-pub trait Peek<T> {
-    fn peek(cursor: Cursor) -> Option<T>;
-}
--- crates/html-impl/src/lib.rs
+++ crates/html-impl/src/lib.rs
@@ -1,10 +1,18 @@
+#![recursion_limit = "128"]
 extern crate proc_macro;
 
+mod html_tree;
+
+use html_tree::HtmlRoot;
 use proc_macro::TokenStream;
 use proc_macro_hack::proc_macro_hack;
 use quote::quote;
+use syn::buffer::Cursor;
 use syn::parse_macro_input;
-use yew_html_common::html_tree::HtmlRoot;
+
+trait Peek<T> {
+    fn peek(cursor: Cursor) -> Option<T>;
+}
 
 #[proc_macro_hack]
 pub fn html(input: TokenStream) -> TokenStream {
