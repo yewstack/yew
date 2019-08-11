9787c9699bf3c841898a7b5b547f8a427fbcc4d4
Henry Zimmerman <zimhen7@gmail.com>
2019-08-11 09:19:42 -0400

    actually add the resize service to the project, add proper imports, add doc comments, add window dimensions when the callback is called

--- src/services/resize.rs
+++ src/services/resize.rs
@@ -1,5 +1,7 @@
+//! This module contains the implementation of a service that listens for browser window resize events.
 use stdweb::Value;
 use yew::callback::Callback;
+use stdweb::{js, web::{window, Window}};
 
 
 /// A service that fires events when the browser window resizes.
@@ -10,16 +12,36 @@ pub struct ResizeService {}
 #[must_use]
 pub struct ResizeTask(Option<Value>);
 
+/// Dimensions of the window.
+pub struct WindowDimensions {
+    /// The width of the viewport of the browser window.
+    pub width: i32,
+    /// The height of the viewport of the browser window.
+    pub height: i32,
+}
+
+impl WindowDimensions {
+    /// Gets the dimensions of the browser window.
+    pub fn get_dimensions(window: &Window) -> Self {
+        WindowDimensions {
+            width: window.inner_width(),
+            height: window.inner_height(),
+        }
+    }
+}
+
 impl ResizeService {
-    /// Creates a new ResizeService
+    /// Creates a new ResizeService.
     pub fn new() -> ResizeService {
         ResizeService {}
     }
 
     /// Register a callback that will be called when the browser window resizes.
-    pub fn register(&mut self, callback: Callback<()>) -> ResizeTask {
+    pub fn register(&mut self, callback: Callback<WindowDimensions>) -> ResizeTask {
         let callback = move || {
-            callback.emit(());
+            let window = window();
+            let dimensions = WindowDimensions::get_dimensions(&window);
+            callback.emit(dimensions);
         };
         let handle = js! {
             var callback = @{callback};
