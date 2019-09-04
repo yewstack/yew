0afd1f21b98603bf642539ae70f048eda9e088cd
Henry Zimmerman <zimhen7@gmail.com>
2019-09-04 13:01:37 -0400

    add neq_assign blanket trait

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@
 
 use failure::{err_msg, Error};
 use stdweb::web::document;
+use crate::html::ShouldRender;
 
 /// Returns `host` for the current document. Useful to connect to a server that server the app.
 pub fn host() -> Result<String, Error> {
@@ -9,4 +10,23 @@ pub fn host() -> Result<String, Error> {
         .location()
         .ok_or_else(|| err_msg("can't get location"))
         .and_then(|l| l.host().map_err(Error::from))
+}
+
+
+
+/// Blanket trait to provide a convenience method for assigning props in `changed` or values in `update`
+pub trait NeqAssign {
+    /// If `self` and `new` aren't equal, assigns `new` to `self` and returns true, otherwise returns false.
+    fn neq_assign(&mut self, new: Self) -> ShouldRender;
+}
+
+impl <T: PartialEq> NeqAssign for T {
+    fn neq_assign(&mut self, new: T) -> ShouldRender {
+        if self != &new {
+            *self = new;
+            true
+        } else {
+            false
+        }
+    }
 }
