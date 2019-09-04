d2d254076de81db7d68c10c858b19bac1df41a4b
Henry Zimmerman <zimhen7@gmail.com>
2019-09-04 14:54:53 -0400

    added example

--- src/utils.rs
+++ src/utils.rs
@@ -14,9 +14,42 @@ pub fn host() -> Result<String, Error> {
 
 
 
-/// Blanket trait to provide a convenience method for assigning props in `changed` or values in `update`
+/// Blanket trait to provide a convenience method for assigning props in `changed` or updating values in `update`
 pub trait NeqAssign {
     /// If `self` and `new` aren't equal, assigns `new` to `self` and returns true, otherwise returns false.
+    ///
+    /// Short for "Not equal assign".
+    ///
+    /// # Example
+    /// ```
+    /// # use yew::{Component, ShouldRender, ComponentLink};
+    /// # use yew::utils::NeqAssign;
+    /// # use yew::Properties;
+    /// ##[derive(Properties, PartialEq)]
+    ///  struct Props {
+    ///     field1: String,
+    ///     field2: usize
+    ///  }
+    ///  struct Model {
+    ///     props: Props
+    ///  }
+    ///  impl Component for Model {
+    /// #    type Message = ();
+    ///     type Properties = Props;
+    ///     // ...
+    /// #
+    /// #    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+    /// #        unimplemented!()
+    /// #    }
+    /// #    fn update(&mut self, msg: ()) -> ShouldRender {
+    /// #        unimplemented!()
+    /// #    }
+    /// #
+    ///     fn change(&mut self, props: Self::Properties) -> ShouldRender{
+    ///         self.props.neq_assign(props)
+    ///     }
+    ///  }
+    /// ```
     fn neq_assign(&mut self, new: Self) -> ShouldRender;
 }
 
