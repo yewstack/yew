c7af13bad1896c1dcf8f8408a5db8323452849be
Henry Zimmerman <zimhen7@gmail.com>
2019-09-15 22:46:22 -0400

    update custome_components example to make use of pure component

--- examples/custom_components/src/button.rs
+++ examples/custom_components/src/button.rs
@@ -1,52 +1,27 @@
 use yew::prelude::*;
-
-pub struct Button {
-    title: String,
-    onsignal: Callback<()>,
-}
+use yew::html::PureComponent;
 
 pub enum Msg {
     Clicked,
 }
 
 #[derive(PartialEq, Properties)]
-pub struct Props {
+pub struct Button {
     pub title: String,
     #[props(required)]
     pub onsignal: Callback<()>,
 }
 
-impl Component for Button {
+impl PureComponent for Button {
     type Message = Msg;
-    type Properties = Props;
 
-    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Button {
-            title: props.title,
-            onsignal: props.onsignal,
-        }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::Clicked => {
-                self.onsignal.emit(());
-            }
-        }
-        false
-    }
-
-    fn change(&mut self, props: Self::Properties) -> ShouldRender {
-        self.title = props.title;
-        self.onsignal = props.onsignal;
-        true
-    }
-}
-
-impl Renderable<Button> for Button {
-    fn view(&self) -> Html<Self> {
+    fn render(&self) -> Html<Self> {
         html! {
             <button onclick=|_| Msg::Clicked>{ &self.title }</button>
         }
+    }
+
+    fn emit(&self, _: Self::Message) {
+        self.onsignal.emit(())
     }
 }
