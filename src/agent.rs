258ed838ac4a0599f4a24809b696a18c30e986fa
Henry Zimmerman <hgzimmerman@users.noreply.github.com>
2019-09-22 15:29:32 -0400

    Implement Debug for ComponentLink and AgentLink (#652)

--- src/agent.rs
+++ src/agent.rs
@@ -8,6 +8,7 @@ use log::warn;
 use serde::{Deserialize, Serialize};
 use slab::Slab;
 use std::cell::RefCell;
+use std::fmt;
 use std::marker::PhantomData;
 use std::rc::Rc;
 use stdweb::Value;
@@ -650,6 +651,12 @@ impl<AGN: Agent> AgentLink<AGN> {
             scope.clone().send(msg);
         };
         closure.into()
+    }
+}
+
+impl<AGN: Agent> fmt::Debug for AgentLink<AGN> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("AgentLink<_>")
     }
 }
 
