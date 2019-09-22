30e67d06d4e61147dc51f61eab7cbe744ed7dd9a
Henry Zimmerman <hgzimmerman@users.noreply.github.com>
2019-09-22 15:28:22 -0400

    derive Debug for Classes (#650)

--- src/virtual_dom/mod.rs
+++ src/virtual_dom/mod.rs
@@ -41,7 +41,7 @@ type Listeners<COMP> = Vec<Box<dyn Listener<COMP>>>;
 type Attributes = HashMap<String, String>;
 
 /// A set of classes.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Classes {
     set: IndexSet<String>,
 }
