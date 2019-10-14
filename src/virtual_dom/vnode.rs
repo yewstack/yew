ea7346c4b8cdd7e747f602a9c4773a565833d5e6
Henry Zimmerman <zimhen7@gmail.com>
2019-10-13 20:16:24 -0400

    revert changes

--- src/virtual_dom/vnode.rs
+++ src/virtual_dom/vnode.rs
@@ -14,7 +14,7 @@ pub enum VNode<COMP: Component> {
     /// A bind between `VText` and `TextNode`.
     VText(VText<COMP>),
     /// A bind between `VComp` and `Element`.
-    VComp(Box<VComp<COMP>>),
+    VComp(VComp<COMP>),
     /// A holder for a list of other nodes.
     VList(VList<COMP>),
     /// A holder for any `Node` (necessary for replacing node).
@@ -92,7 +92,7 @@ impl<COMP: Component> From<VTag<COMP>> for VNode<COMP> {
 
 impl<COMP: Component> From<VComp<COMP>> for VNode<COMP> {
     fn from(vcomp: VComp<COMP>) -> Self {
-        VNode::VComp(Box::new(vcomp))
+        VNode::VComp(vcomp)
     }
 }
 
@@ -102,7 +102,7 @@ where
     CHILD: Component + Renderable<CHILD>,
 {
     fn from(vchild: VChild<CHILD, COMP>) -> Self {
-        VNode::from(VComp::from(vchild))
+        VNode::VComp(VComp::from(vchild))
     }
 }
 
