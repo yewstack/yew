06fb65bc94535937992140c3dd11d77f0632ddd1
Henry Zimmerman <zimhen7@gmail.com>
2019-11-03 16:00:12 -0500

    change example to fetch markdown instead of just resolving a string

--- examples/futures/src/lib.rs
+++ examples/futures/src/lib.rs
@@ -1,6 +1,10 @@
 use crate::Msg::FutureFinished;
 use std::fmt::{Error, Formatter};
+use std::future::Future;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response, Window};
 use yew::{html, Component, ComponentLink, Html, ShouldRender};
 
 struct Model {
@@ -13,13 +17,45 @@ enum Msg {
 
 /// An error that can never happen (because an instance of this can not be created).
 #[derive(Debug, Clone, PartialEq)]
-pub enum InfallibleFutureError {}
-impl std::fmt::Display for InfallibleFutureError {
+pub struct FetchError {
+    err: JsValue,
+}
+impl std::fmt::Display for FetchError {
     fn fmt(&self, _f: &mut Formatter<'_>) -> Result<(), Error> {
         unimplemented!()
     }
 }
-impl std::error::Error for InfallibleFutureError {}
+impl std::error::Error for FetchError {}
+
+impl From<JsValue> for FetchError {
+    fn from(value: JsValue) -> Self {
+        FetchError { err: value }
+    }
+}
+
+/// Gets the markdown from yew's readme.
+///
+/// Consult the following for an example of the fetch api by the team behind web_sys:
+/// https://rustwasm.github.io/wasm-bindgen/examples/fetch.html
+async fn get_markdown() -> Result<String, FetchError> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let request = Request::new_with_str_and_init(
+        "https://raw.githubusercontent.com/yewstack/yew/master/README.md",
+        &opts,
+    )?;
+
+    let window: Window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    assert!(resp_value.is_instance_of::<Response>());
+
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let text = JsFuture::from(resp.text()?).await?;
+    Ok(text.as_string().unwrap())
+}
 
 impl Component for Model {
     // Some details omitted. Explore the examples to see more.
@@ -29,9 +65,8 @@ impl Component for Model {
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         let future = async {
-            let x: Result<Msg, InfallibleFutureError> =
-                Ok(Msg::FutureFinished("Hello Future World!".to_string()));
-            x
+            let md = get_markdown().await;
+            md.map(Msg::FutureFinished)
         };
         link.send_future(future);
         Model { future_data: None }
