568fcbb414ad654db596d93ee465b7428e5bdfaf
Justin Starry <justin.starry@icloud.com>
2019-11-10 20:32:51 -0500

    Update emscripten

--- ci/check_examples.sh
+++ ci/check_examples.sh
@@ -4,9 +4,13 @@ set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_
 # Showcase includes all other examples
 cd examples/showcase
 # TODO Can't build some demos with release, need fix
-cargo web build --target asmjs-unknown-emscripten
-cargo web build --target wasm32-unknown-emscripten
+echo "$(rustup default)" | grep -q "stable"
+if [ "$?" == "0" ]; then
+  # asmjs-unknown-emscripten cargo-web builds are broken on nightly
+  cargo web build --target asmjs-unknown-emscripten --use-system-emscripten
+fi
+cargo web build --target wasm32-unknown-emscripten --use-system-emscripten
 # TODO showcase doesn't support wasm-bindgen yet
-cargo web build --target wasm32-unknown-unknown
+cargo web build --target wasm32-unknown-unknown --use-system-emscripten
 # Reset cwd
 cd ../..
--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
-cargo web test --features web_test --target asmjs-unknown-emscripten
-cargo web test --features web_test --target wasm32-unknown-emscripten
+echo "$(rustup default)" | grep -q "stable"
+if [ "$?" == "0" ]; then
+  # asmjs-unknown-emscripten cargo-web builds are broken on nightly
+  cargo web test --features web_test --target asmjs-unknown-emscripten --use-system-emscripten
+fi
+cargo web test --features web_test --target wasm32-unknown-emscripten --use-system-emscripten
 cargo test --features wasm_test --target wasm32-unknown-unknown
 cargo test --test macro_test
 cargo test --test derive_props_test
