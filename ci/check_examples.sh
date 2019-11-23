8efb187b21aca8a845b502abd280dee88230931f
Justin Starry <justin.starry@icloud.com>
2019-11-23 17:11:54 -0500

    Revert system emscripten stuff

--- ci/check_examples.sh
+++ ci/check_examples.sh
@@ -5,13 +5,17 @@ set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_
 
 # Showcase includes all other examples
 cd examples/showcase
+
 # TODO Can't build some demos with release, need fix
+
 if [ "$is_stable" == "0" ]; then
   # TODO - Emscripten builds are broken on beta/nightly
-  cargo web build --target asmjs-unknown-emscripten --use-system-emscripten
-  cargo web build --target wasm32-unknown-emscripten --use-system-emscripten
+  cargo web build --target asmjs-unknown-emscripten
+  cargo web build --target wasm32-unknown-emscripten
 fi
+
 # TODO showcase doesn't support wasm-bindgen yet
-cargo web build --target wasm32-unknown-unknown --use-system-emscripten
+cargo web build --target wasm32-unknown-unknown
+
 # Reset cwd
 cd ../..
