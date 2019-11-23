167c160e28321a469e1ead6e57409acab41d289f
Justin Starry <justin.starry@icloud.com>
2019-11-23 16:48:31 -0500

    Try with cargo-web emscripten

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -5,8 +5,8 @@ set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_
 
 if [ "$is_stable" == "0" ]; then
   # TODO - Emscripten builds are broken on beta/nightly
-  cargo web test --features web_test --target asmjs-unknown-emscripten --use-system-emscripten
-  cargo web test --features web_test --target wasm32-unknown-emscripten --use-system-emscripten
+  cargo web test --features web_test --target asmjs-unknown-emscripten
+  cargo web test --features web_test --target wasm32-unknown-emscripten
 fi
 
 cargo test --features wasm_test --target wasm32-unknown-unknown
