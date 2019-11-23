ff042058e50b261742623f07859142f434b55d3c
Justin Starry <justin.starry@icloud.com>
2019-11-23 17:13:13 -0500

    Disable emscripten in CI for beta/nightly (#731)
    * Update emscripten
    
    * Don't fail on rustup check
    
    * Disable emscripten for non-stable builds
    
    * Try with cargo-web emscripten
    
    * Revert system emscripten stuff

--- ci/check_examples.sh
+++ ci/check_examples.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+echo "$(rustup default)" | grep -q "stable"
+is_stable=$?
 set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
 # Showcase includes all other examples
 cd examples/showcase
+
 # TODO Can't build some demos with release, need fix
-cargo web build --target asmjs-unknown-emscripten
-cargo web build --target wasm32-unknown-emscripten
+
+if [ "$is_stable" == "0" ]; then
+  # TODO - Emscripten builds are broken on beta/nightly
+  cargo web build --target asmjs-unknown-emscripten
+  cargo web build --target wasm32-unknown-emscripten
+fi
+
 # TODO showcase doesn't support wasm-bindgen yet
 cargo web build --target wasm32-unknown-unknown
+
 # Reset cwd
 cd ../..
