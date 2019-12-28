eb1393bb9472b80723fb8d075fb27d4c222fc278
Justin Starry <justin.starry@icloud.com>
2019-12-28 16:12:21 -0600

    Enable emscripten in CI for rustc 1.39.0

--- ci/check_examples.sh
+++ ci/check_examples.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo "$(rustup default)" | grep -q "1.39.0"
+emscripten_supported=$?
 set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
 # Showcase includes all other examples
@@ -6,9 +8,11 @@ cd examples/showcase
 
 # TODO Can't build some demos with release, need fix
 
-# TODO - Emscripten builds are broken
-# cargo web build --target asmjs-unknown-emscripten
-# cargo web build --target wasm32-unknown-emscripten
+if [ "$emscripten_supported" == "0" ]; then
+  # TODO - Emscripten builds are broken on rustc > 1.39.0
+  cargo web build --target asmjs-unknown-emscripten
+  cargo web build --target wasm32-unknown-emscripten
+fi
 
 # TODO showcase doesn't support wasm-bindgen yet
 cargo web build --target wasm32-unknown-unknown
