3ffde502a7400abbbba413a1399cfb2fb82c835a
Justin Starry <justin.starry@icloud.com>
2019-12-28 18:03:39 -0600

    Enable emscripten in CI for rustc 1.39.0 (#828)

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+echo "$(rustup default)" | grep -q "1.39.0"
+emscripten_supported=$?
 set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
-# TODO - Emscripten builds are broken
-# cargo web test --features web_test --target asmjs-unknown-emscripten
-# cargo web test --features web_test --target wasm32-unknown-emscripten
+if [ "$emscripten_supported" == "0" ]; then
+  # TODO - Emscripten builds are broken on rustc > 1.39.0
+  cargo web test --features web_test --target asmjs-unknown-emscripten
+  cargo web test --features web_test --target wasm32-unknown-emscripten
+fi
 
 cargo test --features wasm_test --target wasm32-unknown-unknown
 cargo test --test macro_test
