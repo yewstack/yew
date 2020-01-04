yewstack/yew

commit a900fbee496d1140b424bcb2f1c91402d20eca2b

Improve nested html! expansion by unwrapping VNodes (#820)
* Improve nested html! expansion by omitting VNodes

* Add unittest and fix child node expansion

* Fix existing unittests and check backward compat

* Fix rebase conflicts

* Parse expression

* Fix clippy

Co-authored-by: Justin Starry <justin.starry@icloud.com>

crates/macro/src/html_tree/html_iterable.rs
@@ -7,6 +7,10 @@ use syn::parse::{Parse, ParseStream, Result as ParseResult};
 use syn::spanned::Spanned;
 use syn::{Expr, Token};
 
+use proc_macro2::{Ident, Span};
+use syn::visit_mut::{self, VisitMut};
+use syn::Macro;
+
 pub struct HtmlIterable(Expr);
 
 impl PeekValue<()> for HtmlIterable {
@@ -16,12 +20,28 @@ impl PeekValue<()> for HtmlIterable {
     }
 }
 
+struct HtmlInnerModifier;
+impl VisitMut for HtmlInnerModifier {
+    fn visit_macro_mut(&mut self, node: &mut Macro) {
+        if node.path.is_ident("html") {
+            let ident = &mut node.path.segments.last_mut().unwrap().ident;
+            *ident = Ident::new("html_nested", Span::call_site());
+        }
+
+        // Delegate to the default impl to visit any nested functions.
+        visit_mut::visit_macro_mut(self, node);
+    }
+}
+
 impl Parse for HtmlIterable {
     fn parse(input: ParseStream) -> ParseResult<Self> {
         let for_token = input.parse::<Token![for]>()?;
 
         match input.parse() {
-            Ok(expr) => Ok(HtmlIterable(expr)),
+            Ok(mut expr) => {
+                HtmlInnerModifier.visit_expr_mut(&mut expr);
+                Ok(HtmlIterable(expr))
+            }
             Err(err) => {
                 if err.to_string().starts_with("unexpected end of input") {
                     Err(syn::Error::new_spanned(

crates/macro/src/html_tree/html_node.rs
@@ -4,7 +4,25 @@ use quote::{quote, quote_spanned, ToTokens};
 use syn::buffer::Cursor;
 use syn::parse::{Parse, ParseStream, Result};
 use syn::spanned::Spanned;
+use syn::Expr;
 use syn::Lit;
+
+use proc_macro2::{Ident, Span};
+use syn::visit_mut::{self, VisitMut};
+use syn::Macro;
+
+struct HtmlInnerModifier;
+impl VisitMut for HtmlInnerModifier {
+    fn visit_macro_mut(&mut self, node: &mut Macro) {
+        if node.path.is_ident("html") {
+            let ident = &mut node.path.segments.last_mut().unwrap().ident;
+            *ident = Ident::new("html_nested", Span::call_site());
+        }
+
+        // Delegate to the default impl to visit any nested functions.
+        visit_mut::visit_macro_mut(self, node);
+    }
+}
 
 pub struct HtmlNode(Node);
 
@@ -18,7 +36,9 @@ impl Parse for HtmlNode {
             }
             Node::Literal(lit)
         } else {
-            Node::Raw(input.parse()?)
+            let mut expr: Expr = input.parse()?;
+            HtmlInnerModifier.visit_expr_mut(&mut expr);
+            Node::Expression(expr)
         };
 
         Ok(HtmlNode(node))
@@ -46,12 +66,8 @@ impl ToTokens for HtmlNode {
 impl ToTokens for Node {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let node_token = match &self {
-            Node::Literal(lit) => quote! {
-                ::yew::virtual_dom::VNode::from(#lit)
-            },
-            Node::Raw(stream) => quote_spanned! {stream.span()=>
-                ::yew::virtual_dom::VNode::from({#stream})
-            },
+            Node::Literal(lit) => quote! {#lit},
+            Node::Expression(expr) => quote_spanned! {expr.span()=> {#expr} },
         };
 
         tokens.extend(node_token);
@@ -60,5 +76,5 @@ impl ToTokens for Node {
 
 enum Node {
     Literal(Lit),
-    Raw(TokenStream),
+    Expression(Expr),
 }

crates/macro/Cargo.toml
@@ -23,7 +23,7 @@ lazy_static = "1.3.0"
 proc-macro-hack = "0.5"
 proc-macro2 = "1.0"
 quote = "1.0"
-syn = { version = "1.0", features = ["full", "extra-traits"] }
+syn = { version = "1.0", features = ["full", "extra-traits", "visit-mut"] }
 
 [dev-dependencies]
 yew = { path = "../.." }

crates/macro/src/html_tree/html_block.rs
@@ -26,7 +26,7 @@ impl PeekValue<()> for HtmlBlock {
 
 impl Parse for HtmlBlock {
     fn parse(input: ParseStream) -> ParseResult<Self> {
-        let content;
+        let content: syn::parse::ParseBuffer<'_>;
         let brace = braced!(content in input);
         let content = if HtmlIterable::peek(content.cursor()).is_some() {
             BlockContent::Iterable(content.parse()?)

crates/macro/src/html_tree/html_component.rs
@@ -11,7 +11,7 @@ use syn::parse;
 use syn::parse::{Parse, ParseStream, Result as ParseResult};
 use syn::punctuated::Punctuated;
 use syn::spanned::Spanned;
-use syn::{Expr, Ident, Path, PathArguments, PathSegment, Token, Type, TypePath};
+use syn::{Expr, Ident, Index, Path, PathArguments, PathSegment, Token, Type, TypePath};
 
 pub struct HtmlComponent {
     ty: Type,
@@ -123,10 +123,17 @@ impl ToTokens for HtmlComponent {
         };
 
         let set_children = if !children.is_empty() {
+            let i = (0..children.len())
+                .map(|x| Index::from(x))
+                .collect::<Vec<_>>();
             quote! {
-                .children(::yew::html::ChildrenRenderer::new(
-                    vec![#(#children.into(),)*]
-                ))
+                .children(::yew::html::ChildrenRenderer::new({
+                    let mut v = Vec::new();
+                    let comps = (#(#children,)*);
+                    #(::yew::utils::NodeSeq::from(comps.#i).into_iter()
+                        .for_each(|x| v.push(x.into()));)*
+                    v
+                }))
             }
         } else {
             quote! {}

crates/macro/src/html_tree/mod.rs
@@ -126,6 +126,19 @@ impl HtmlTree {
     }
 }
 
+pub struct HtmlRootNested(HtmlTreeNested);
+impl Parse for HtmlRootNested {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(HtmlRootNested(HtmlTreeNested::parse(input)?))
+    }
+}
+
+impl ToTokens for HtmlRootNested {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        self.0.to_tokens(tokens);
+    }
+}
+
 pub struct HtmlTreeNested(HtmlTree);
 impl Parse for HtmlTreeNested {
     fn parse(input: ParseStream) -> Result<Self> {

crates/macro/src/lib.rs
@@ -63,7 +63,7 @@ mod derive_props;
 mod html_tree;
 
 use derive_props::DerivePropsInput;
-use html_tree::HtmlRoot;
+use html_tree::{HtmlRoot, HtmlRootNested};
 use proc_macro::TokenStream;
 use proc_macro_hack::proc_macro_hack;
 use quote::{quote, ToTokens};
@@ -92,6 +92,12 @@ fn non_capitalized_ascii(string: &str) -> bool {
 pub fn derive_props(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DerivePropsInput);
     TokenStream::from(input.into_token_stream())
+}
+
+#[proc_macro_hack]
+pub fn html_nested(input: TokenStream) -> TokenStream {
+    let root = parse_macro_input!(input as HtmlRootNested);
+    TokenStream::from(quote! {#root})
 }
 
 #[proc_macro_hack]

src/lib.rs
@@ -72,9 +72,14 @@ use proc_macro_hack::proc_macro_hack;
 #[proc_macro_hack(support_nested)]
 pub use yew_macro::html;
 
+#[doc(hidden)]
+#[proc_macro_hack(support_nested)]
+pub use yew_macro::html_nested;
+
 /// This module contains macros which implements html! macro and JSX-like templates
 pub mod macros {
     pub use crate::html;
+    pub use crate::html_nested;
     pub use yew_macro::Properties;
 }
 
@@ -158,6 +163,7 @@ pub mod prelude {
         Renderable, ShouldRender,
     };
     pub use crate::macros::*;
+    pub use crate::utils::NodeSeq;
     pub use crate::virtual_dom::Classes;
 
     /// Prelude module for creating worker.

commit 34dc935821cb32526c3085505649c0d70b163a13

Resolve #839 regression issue introduced by #820

crates/macro/src/html_tree/html_iterable.rs
@@ -5,11 +5,38 @@ use quote::{quote_spanned, ToTokens};
 use syn::buffer::Cursor;
 use syn::parse::{Parse, ParseStream, Result as ParseResult};
 use syn::spanned::Spanned;
-use syn::{Expr, Token};
+use syn::{Expr, ExprIf, Token};
 
-use proc_macro2::{Ident, Span};
+use proc_macro2::Ident;
 use syn::visit_mut::{self, VisitMut};
 use syn::Macro;
+
+struct HtmlInnerYin;
+impl VisitMut for HtmlInnerYin {
+    fn visit_expr_if_mut(&mut self, node: &mut ExprIf) {
+        if node.else_branch.is_some() {
+            HtmlInnerYang.visit_expr_if_mut(node);
+        } else {
+            visit_mut::visit_expr_if_mut(self, node);
+        }
+    }
+
+    fn visit_macro_mut(&mut self, node: &mut Macro) {
+        if node.path.is_ident("html") {
+            let ident = &mut node.path.segments.last_mut().unwrap().ident;
+            *ident = Ident::new("html_nested", ident.span());
+        }
+
+        visit_mut::visit_macro_mut(self, node);
+    }
+}
+
+struct HtmlInnerYang;
+impl VisitMut for HtmlInnerYang {
+    fn visit_macro_mut(&mut self, node: &mut Macro) {
+        visit_mut::visit_macro_mut(self, node);
+    }
+}
 
 pub struct HtmlIterable(Expr);
 
@@ -20,26 +47,13 @@ impl PeekValue<()> for HtmlIterable {
     }
 }
 
-struct HtmlInnerModifier;
-impl VisitMut for HtmlInnerModifier {
-    fn visit_macro_mut(&mut self, node: &mut Macro) {
-        if node.path.is_ident("html") {
-            let ident = &mut node.path.segments.last_mut().unwrap().ident;
-            *ident = Ident::new("html_nested", Span::call_site());
-        }
-
-        // Delegate to the default impl to visit any nested functions.
-        visit_mut::visit_macro_mut(self, node);
-    }
-}
-
 impl Parse for HtmlIterable {
     fn parse(input: ParseStream) -> ParseResult<Self> {
         let for_token = input.parse::<Token![for]>()?;
 
         match input.parse() {
             Ok(mut expr) => {
-                HtmlInnerModifier.visit_expr_mut(&mut expr);
+                HtmlInnerYin.visit_expr_mut(&mut expr);
                 Ok(HtmlIterable(expr))
             }
             Err(err) => {

crates/macro/src/html_tree/html_node.rs
@@ -4,22 +4,35 @@ use quote::{quote, quote_spanned, ToTokens};
 use syn::buffer::Cursor;
 use syn::parse::{Parse, ParseStream, Result};
 use syn::spanned::Spanned;
-use syn::Expr;
-use syn::Lit;
+use syn::{Expr, ExprIf, Lit};
 
-use proc_macro2::{Ident, Span};
+use proc_macro2::Ident;
 use syn::visit_mut::{self, VisitMut};
 use syn::Macro;
 
-struct HtmlInnerModifier;
-impl VisitMut for HtmlInnerModifier {
+struct HtmlInnerYin;
+impl VisitMut for HtmlInnerYin {
+    fn visit_expr_if_mut(&mut self, node: &mut ExprIf) {
+        if node.else_branch.is_some() {
+            HtmlInnerYang.visit_expr_if_mut(node);
+        } else {
+            visit_mut::visit_expr_if_mut(self, node);
+        }
+    }
+
     fn visit_macro_mut(&mut self, node: &mut Macro) {
         if node.path.is_ident("html") {
             let ident = &mut node.path.segments.last_mut().unwrap().ident;
-            *ident = Ident::new("html_nested", Span::call_site());
+            *ident = Ident::new("html_nested", ident.span());
         }
 
-        // Delegate to the default impl to visit any nested functions.
+        visit_mut::visit_macro_mut(self, node);
+    }
+}
+
+struct HtmlInnerYang;
+impl VisitMut for HtmlInnerYang {
+    fn visit_macro_mut(&mut self, node: &mut Macro) {
         visit_mut::visit_macro_mut(self, node);
     }
 }
@@ -37,7 +50,7 @@ impl Parse for HtmlNode {
             Node::Literal(lit)
         } else {
             let mut expr: Expr = input.parse()?;
-            HtmlInnerModifier.visit_expr_mut(&mut expr);
+            HtmlInnerYin.visit_expr_mut(&mut expr);
             Node::Expression(expr)
         };
 