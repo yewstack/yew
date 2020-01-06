yewstack/yew

commit a900fbee496d1140b424bcb2f1c91402d20eca2b

Improve nested html! expansion by unwrapping VNodes (#820)
* Improve nested html! expansion by omitting VNodes

* Add unittest and fix child node expansion

* Fix existing unittests and check backward compat

* Fix rebase conflicts

* Parse expression

* Fix clippy

Co-authored-by: Justin Starry <justin.starry@icloud.com>

crates/macro/src/html_tree/html_component.rs
@@ -11,7 +11,7 @@ use syn::parse;
 use syn::parse::{Parse, ParseStream, Result as ParseResult};
 use syn::punctuated::Punctuated;
 use syn::spanned::Spanned;
-use syn::{Expr, Ident, Path, PathArguments, PathSegment, Token, Type, TypePath};
+use syn::{Expr, Ident, Index, Path, PathArguments, PathSegment, Token, Type, TypePath};
 
 pub struct HtmlComponent {
     ty: Type,
@@ -123,10 +123,17 @@ impl ToTokens for HtmlComponent {
         };
 
         let set_children = if !children.is_empty() {
+            let i = (0..children.len())
+                .map(|x| Index::from(x))
+                .collect::<Vec<_>>();
             quote! {
-                .children(::yew::html::ChildrenRenderer::new(
-                    vec![#(#children.into(),)*]
-                ))
+                .children(::yew::html::ChildrenRenderer::new({
+                    let mut v = Vec::new();
+                    let comps = (#(#children,)*);
+                    #(::yew::utils::NodeSeq::from(comps.#i).into_iter()
+                        .for_each(|x| v.push(x.into()));)*
+                    v
+                }))
             }
         } else {
             quote! {}

crates/macro/src/html_tree/html_node.rs
@@ -4,7 +4,25 @@ use quote::{quote, quote_spanned, ToTokens};
 use syn::buffer::Cursor;
 use syn::parse::{Parse, ParseStream, Result};
 use syn::spanned::Spanned;
+use syn::Expr;
 use syn::Lit;
+
+use proc_macro2::{Ident, Span};
+use syn::visit_mut::{self, VisitMut};
+use syn::Macro;
+
+struct HtmlInnerModifier;
+impl VisitMut for HtmlInnerModifier {
+    fn visit_macro_mut(&mut self, node: &mut Macro) {
+        if node.path.is_ident("html") {
+            let ident = &mut node.path.segments.last_mut().unwrap().ident;
+            *ident = Ident::new("html_nested", Span::call_site());
+        }
+
+        // Delegate to the default impl to visit any nested functions.
+        visit_mut::visit_macro_mut(self, node);
+    }
+}
 
 pub struct HtmlNode(Node);
 
@@ -18,7 +36,9 @@ impl Parse for HtmlNode {
             }
             Node::Literal(lit)
         } else {
-            Node::Raw(input.parse()?)
+            let mut expr: Expr = input.parse()?;
+            HtmlInnerModifier.visit_expr_mut(&mut expr);
+            Node::Expression(expr)
         };
 
         Ok(HtmlNode(node))
@@ -46,12 +66,8 @@ impl ToTokens for HtmlNode {
 impl ToTokens for Node {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let node_token = match &self {
-            Node::Literal(lit) => quote! {
-                ::yew::virtual_dom::VNode::from(#lit)
-            },
-            Node::Raw(stream) => quote_spanned! {stream.span()=>
-                ::yew::virtual_dom::VNode::from({#stream})
-            },
+            Node::Literal(lit) => quote! {#lit},
+            Node::Expression(expr) => quote_spanned! {expr.span()=> {#expr} },
         };
 
         tokens.extend(node_token);
@@ -60,5 +76,5 @@ impl ToTokens for Node {
 
 enum Node {
     Literal(Lit),
-    Raw(TokenStream),
+    Expression(Expr),
 }

crates/macro/src/html_tree/mod.rs
@@ -126,6 +126,19 @@ impl HtmlTree {
     }
 }
 
+pub struct HtmlRootNested(HtmlTreeNested);
+impl Parse for HtmlRootNested {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(HtmlRootNested(HtmlTreeNested::parse(input)?))
+    }
+}
+
+impl ToTokens for HtmlRootNested {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        self.0.to_tokens(tokens);
+    }
+}
+
 pub struct HtmlTreeNested(HtmlTree);
 impl Parse for HtmlTreeNested {
     fn parse(input: ParseStream) -> Result<Self> {

commit 2beb59502b164da4c1c46bb1695bab773501326b

Add `html_nested!` macro to support nested iterable children access (#843)
* Revert "Revert "Improve nested html! expansion by unwrapping VNodes (#820)" (#842)"

This reverts commit 70862a46

* Add minor fix to VList to conform with #820

* Add unittest for the regression bug #839

* Resolve #839 regression issue introduced by #820

* wip

* Revert "Resolve #839 regression issue introduced by #820"

This reverts commit 34dc935821cb32526c3085505649c0d70b163a13.

* Use explicit html_nested

* Remove NodeSeq from prelude

* Clean up tests

* Add list and tag tests

* Revert vtag children change

Co-authored-by: Justin Starry <justin.m.starry@gmail.com>

crates/macro/src/html_tree/html_component.rs
@@ -127,9 +127,11 @@ impl ToTokens for HtmlComponent {
 
         let set_children = if !children.is_empty() {
             quote! {
-                .children(::yew::html::ChildrenRenderer::new(
-                    vec![#(#children.into(),)*]
-                ))
+                .children(::yew::html::ChildrenRenderer::new({
+                    let mut v = ::std::vec::Vec::new();
+                    #(v.extend(::yew::utils::NodeSeq::from(#children));)*
+                    v
+                }))
             }
         } else {
             quote! {}

crates/macro/src/html_tree/html_list.rs
@@ -49,10 +49,14 @@ impl Parse for HtmlList {
 
 impl ToTokens for HtmlList {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let html_trees = &self.0;
+        let children = &self.0;
         tokens.extend(quote! {
             ::yew::virtual_dom::VNode::VList(
-                ::yew::virtual_dom::vlist::VList::new_with_children(vec![#(#html_trees,)*])
+                ::yew::virtual_dom::vlist::VList::new_with_children({
+                    let mut v = ::std::vec::Vec::new();
+                    #(v.extend(::yew::utils::NodeSeq::from(#children));)*
+                    v
+                })
             )
         });
     }

crates/macro/src/html_tree/html_node.rs
@@ -4,6 +4,7 @@ use quote::{quote, quote_spanned, ToTokens};
 use syn::buffer::Cursor;
 use syn::parse::{Parse, ParseStream, Result};
 use syn::spanned::Spanned;
+use syn::Expr;
 use syn::Lit;
 
 pub struct HtmlNode(Node);
@@ -18,7 +19,7 @@ impl Parse for HtmlNode {
             }
             Node::Literal(lit)
         } else {
-            Node::Raw(input.parse()?)
+            Node::Expression(input.parse()?)
         };
 
         Ok(HtmlNode(node))
@@ -46,12 +47,8 @@ impl ToTokens for HtmlNode {
 impl ToTokens for Node {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let node_token = match &self {
-            Node::Literal(lit) => quote! {
-                ::yew::virtual_dom::VNode::from(#lit)
-            },
-            Node::Raw(stream) => quote_spanned! {stream.span()=>
-                ::yew::virtual_dom::VNode::from({#stream})
-            },
+            Node::Literal(lit) => quote! {#lit},
+            Node::Expression(expr) => quote_spanned! {expr.span()=> {#expr} },
         };
 
         tokens.extend(node_token);
@@ -60,5 +57,5 @@ impl ToTokens for Node {
 
 enum Node {
     Literal(Lit),
-    Raw(TokenStream),
+    Expression(Expr),
 }

crates/macro/src/html_tree/mod.rs
@@ -126,6 +126,19 @@ impl HtmlTree {
     }
 }
 
+pub struct HtmlRootNested(HtmlTreeNested);
+impl Parse for HtmlRootNested {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(HtmlRootNested(HtmlTreeNested::parse(input)?))
+    }
+}
+
+impl ToTokens for HtmlRootNested {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        self.0.to_tokens(tokens);
+    }
+}
+
 pub struct HtmlTreeNested(HtmlTree);
 impl Parse for HtmlTreeNested {
     fn parse(input: ParseStream) -> Result<Self> {

crates/macro/src/lib.rs
@@ -63,7 +63,7 @@ mod derive_props;
 mod html_tree;
 
 use derive_props::DerivePropsInput;
-use html_tree::HtmlRoot;
+use html_tree::{HtmlRoot, HtmlRootNested};
 use proc_macro::TokenStream;
 use proc_macro_hack::proc_macro_hack;
 use quote::{quote, ToTokens};
@@ -92,6 +92,12 @@ fn non_capitalized_ascii(string: &str) -> bool {
 pub fn derive_props(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DerivePropsInput);
     TokenStream::from(input.into_token_stream())
+}
+
+#[proc_macro_hack]
+pub fn html_nested(input: TokenStream) -> TokenStream {
+    let root = parse_macro_input!(input as HtmlRootNested);
+    TokenStream::from(quote! {#root})
 }
 
 #[proc_macro_hack]

src/lib.rs
@@ -72,9 +72,14 @@ use proc_macro_hack::proc_macro_hack;
 #[proc_macro_hack(support_nested)]
 pub use yew_macro::html;
 
+#[doc(hidden)]
+#[proc_macro_hack(support_nested)]
+pub use yew_macro::html_nested;
+
 /// This module contains macros which implements html! macro and JSX-like templates
 pub mod macros {
     pub use crate::html;
+    pub use crate::html_nested;
     pub use yew_macro::Properties;
 }
 

src/utils.rs
@@ -1,6 +1,7 @@
 //! This module contains useful utils to get information about the current document.
 
 use failure::{err_msg, Error};
+use std::marker::PhantomData;
 use stdweb::web::document;
 
 /// Returns `host` for the current document. Useful to connect to a server that server the app.
@@ -9,4 +10,32 @@ pub fn host() -> Result<String, Error> {
         .location()
         .ok_or_else(|| err_msg("can't get location"))
         .and_then(|l| l.host().map_err(Error::from))
+}
+
+/// Specialty type necessary for helping flattening components returned from nested html macros.
+#[derive(Debug)]
+pub struct NodeSeq<IN, OUT>(Vec<OUT>, PhantomData<IN>);
+
+impl<IN: Into<OUT>, OUT> From<IN> for NodeSeq<IN, OUT> {
+    fn from(val: IN) -> Self {
+        Self(vec![val.into()], PhantomData::default())
+    }
+}
+
+impl<IN: Into<OUT>, OUT> From<Vec<IN>> for NodeSeq<IN, OUT> {
+    fn from(val: Vec<IN>) -> Self {
+        Self(
+            val.into_iter().map(|x| x.into()).collect(),
+            PhantomData::default(),
+        )
+    }
+}
+
+impl<IN: Into<OUT>, OUT> IntoIterator for NodeSeq<IN, OUT> {
+    type Item = OUT;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
 }

tests/macro/html-component-fail.rs
@@ -2,7 +2,6 @@
 
 use std::marker::PhantomData;
 use yew::prelude::*;
-use yew::virtual_dom::{VChild, VComp, VNode};
 
 #[derive(Clone, Properties, PartialEq)]
 pub struct ChildProperties {
@@ -19,18 +18,6 @@ impl Component for Child {
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self { unimplemented!() }
     fn update(&mut self, _: Self::Message) -> ShouldRender { unimplemented!() }
     fn view(&self) -> Html { unimplemented!() }
-}
-
-impl From<VChild<Child>> for ChildProperties {
-    fn from(comp: VChild<Child>) -> Self {
-        comp.props
-    }
-}
-
-impl Into<VNode> for ChildProperties {
-    fn into(self) -> VNode {
-        VComp::new::<Child>(self, NodeRef::default()).into()
-    }
 }
 
 #[derive(Clone, Properties)]
@@ -92,7 +79,7 @@ fn compile_fail() {
     html! { <ChildContainer></ChildContainer> };
     html! { <ChildContainer>{ "Not allowed" }</ChildContainer> };
     html! { <ChildContainer><></></ChildContainer> };
-    html! { <ChildContainer><Child int=1 /><other /></ChildContainer> };
+    html! { <ChildContainer><other /></ChildContainer> };
 
     html! { <Generic<String>></Generic> };
     html! { <Generic<String>></Generic<Vec<String>>> };

tests/macro/html-component-fail.stderr
@@ -1,145 +1,145 @@
 error: this open tag has no corresponding close tag
-  --> $DIR/html-component-fail.rs:66:13
+  --> $DIR/html-component-fail.rs:53:13
    |
-66 |     html! { <Child> };
+53 |     html! { <Child> };
    |             ^^^^^^^
 
 error: expected identifier
-  --> $DIR/html-component-fail.rs:67:22
+  --> $DIR/html-component-fail.rs:54:22
    |
-67 |     html! { <Child:: /> };
+54 |     html! { <Child:: /> };
    |                      ^
 
 error: unexpected end of input, expected identifier
-  --> $DIR/html-component-fail.rs:68:5
+  --> $DIR/html-component-fail.rs:55:5
    |
-68 |     html! { <Child with /> };
+55 |     html! { <Child with /> };
    |     ^^^^^^^^^^^^^^^^^^^^^^^^^
    |
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)
 
 error: unexpected token
-  --> $DIR/html-component-fail.rs:69:20
+  --> $DIR/html-component-fail.rs:56:20
    |
-69 |     html! { <Child props /> };
+56 |     html! { <Child props /> };
    |                    ^^^^^
 
 error: this open tag has no corresponding close tag
-  --> $DIR/html-component-fail.rs:70:13
+  --> $DIR/html-component-fail.rs:57:13
    |
-70 |     html! { <Child with props > };
+57 |     html! { <Child with props > };
    |             ^^^^^^^^^^^^^^^^^^^
 
 error: unexpected token
-  --> $DIR/html-component-fail.rs:71:38
+  --> $DIR/html-component-fail.rs:58:38
    |
-71 |     html! { <Child with props ref=() ref=() /> };
+58 |     html! { <Child with props ref=() ref=() /> };
    |                                      ^^^
 
 error: unexpected token
-  --> $DIR/html-component-fail.rs:72:27
+  --> $DIR/html-component-fail.rs:59:27
    |
-72 |     html! { <Child ref=() with props /> };
+59 |     html! { <Child ref=() with props /> };
    |                           ^^^^
 
 error: unexpected token
-  --> $DIR/html-component-fail.rs:74:31
+  --> $DIR/html-component-fail.rs:61:31
    |
-74 |     html! { <Child with props () /> };
+61 |     html! { <Child with props () /> };
    |                               ^^
 
 error: expected identifier
-  --> $DIR/html-component-fail.rs:75:20
+  --> $DIR/html-component-fail.rs:62:20
    |
-75 |     html! { <Child type=0 /> };
+62 |     html! { <Child type=0 /> };
    |                    ^^^^
 
 error: expected identifier
-  --> $DIR/html-component-fail.rs:76:20
+  --> $DIR/html-component-fail.rs:63:20
    |
-76 |     html! { <Child invalid-prop-name=0 /> };
+63 |     html! { <Child invalid-prop-name=0 /> };
    |                    ^^^^^^^^^^^^^^^^^
 
 error: unexpected end of input, expected expression
-  --> $DIR/html-component-fail.rs:78:5
+  --> $DIR/html-component-fail.rs:65:5
    |
-78 |     html! { <Child string= /> };
+65 |     html! { <Child string= /> };
    |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    |
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)
 
 error: too many refs set
-  --> $DIR/html-component-fail.rs:83:33
+  --> $DIR/html-component-fail.rs:70:33
    |
-83 |     html! { <Child int=1 ref=() ref=() /> };
+70 |     html! { <Child int=1 ref=() ref=() /> };
    |                                 ^^^
 
 error: this close tag has no corresponding open tag
-  --> $DIR/html-component-fail.rs:86:13
+  --> $DIR/html-component-fail.rs:73:13
    |
-86 |     html! { </Child> };
+73 |     html! { </Child> };
    |             ^^^^^^^^
 
 error: this open tag has no corresponding close tag
-  --> $DIR/html-component-fail.rs:87:13
+  --> $DIR/html-component-fail.rs:74:13
    |
-87 |     html! { <Child><Child></Child> };
+74 |     html! { <Child><Child></Child> };
    |             ^^^^^^^
 
 error: only one root html element allowed
-  --> $DIR/html-component-fail.rs:88:28
+  --> $DIR/html-component-fail.rs:75:28
    |
-88 |     html! { <Child></Child><Child></Child> };
+75 |     html! { <Child></Child><Child></Child> };
    |                            ^^^^^^^^^^^^^^^
 
 error: this close tag has no corresponding open tag
-  --> $DIR/html-component-fail.rs:97:30
+  --> $DIR/html-component-fail.rs:84:30
    |
-97 |     html! { <Generic<String>></Generic> };
+84 |     html! { <Generic<String>></Generic> };
    |                              ^^^^^^^^^^
 
 error: this close tag has no corresponding open tag
-  --> $DIR/html-component-fail.rs:98:30
+  --> $DIR/html-component-fail.rs:85:30
    |
-98 |     html! { <Generic<String>></Generic<Vec<String>>> };
+85 |     html! { <Generic<String>></Generic<Vec<String>>> };
    |                              ^^^^^^^^^^^^^^^^^^^^^^^
 
 error[E0425]: cannot find value `blah` in this scope
-  --> $DIR/html-component-fail.rs:73:25
+  --> $DIR/html-component-fail.rs:60:25
    |
-73 |     html! { <Child with blah /> };
+60 |     html! { <Child with blah /> };
    |                         ^^^^ not found in this scope
 
 error[E0609]: no field `unknown` on type `ChildProperties`
-  --> $DIR/html-component-fail.rs:77:20
+  --> $DIR/html-component-fail.rs:64:20
    |
-77 |     html! { <Child unknown="unknown" /> };
+64 |     html! { <Child unknown="unknown" /> };
    |                    ^^^^^^^ unknown field
    |
    = note: available fields are: `string`, `int`
 
 error[E0599]: no method named `unknown` found for type `ChildPropertiesBuilder<ChildPropertiesBuilderStep_missing_required_prop_int>` in the current scope
-  --> $DIR/html-component-fail.rs:77:20
+  --> $DIR/html-component-fail.rs:64:20
    |
-7  | #[derive(Clone, Properties, PartialEq)]
+6  | #[derive(Clone, Properties, PartialEq)]
    |                          - method `unknown` not found for this
 ...
-77 |     html! { <Child unknown="unknown" /> };
+64 |     html! { <Child unknown="unknown" /> };
    |                    ^^^^^^^ method not found in `ChildPropertiesBuilder<ChildPropertiesBuilderStep_missing_required_prop_int>`
 
 error[E0308]: mismatched types
-  --> $DIR/html-component-fail.rs:79:33
+  --> $DIR/html-component-fail.rs:66:33
    |
-79 |     html! { <Child int=1 string={} /> };
+66 |     html! { <Child int=1 string={} /> };
    |                                 ^^ expected struct `std::string::String`, found ()
    |
    = note: expected type `std::string::String`
               found type `()`
 
 error[E0308]: mismatched types
-  --> $DIR/html-component-fail.rs:80:33
+  --> $DIR/html-component-fail.rs:67:33
    |
-80 |     html! { <Child int=1 string=3 /> };
+67 |     html! { <Child int=1 string=3 /> };
    |                                 ^
    |                                 |
    |                                 expected struct `std::string::String`, found integer
@@ -149,9 +149,9 @@ error[E0308]: mismatched types
               found type `{integer}`
 
 error[E0308]: mismatched types
-  --> $DIR/html-component-fail.rs:81:33
+  --> $DIR/html-component-fail.rs:68:33
    |
-81 |     html! { <Child int=1 string={3} /> };
+68 |     html! { <Child int=1 string={3} /> };
    |                                 ^^^
    |                                 |
    |                                 expected struct `std::string::String`, found integer
@@ -161,90 +161,93 @@ error[E0308]: mismatched types
               found type `{integer}`
 
 error[E0308]: mismatched types
-  --> $DIR/html-component-fail.rs:82:30
+  --> $DIR/html-component-fail.rs:69:30
    |
-82 |     html! { <Child int=1 ref=() /> };
+69 |     html! { <Child int=1 ref=() /> };
    |                              ^^ expected struct `yew::html::NodeRef`, found ()
    |
    = note: expected type `yew::html::NodeRef`
               found type `()`
 
 error[E0308]: mismatched types
-  --> $DIR/html-component-fail.rs:84:24
+  --> $DIR/html-component-fail.rs:71:24
    |
-84 |     html! { <Child int=0u32 /> };
+71 |     html! { <Child int=0u32 /> };
    |                        ^^^^ expected i32, found u32
    |
 help: you can convert an `u32` to `i32` and panic if the converted value wouldn't fit
    |
-84 |     html! { <Child int=0u32.try_into().unwrap() /> };
+71 |     html! { <Child int=0u32.try_into().unwrap() /> };
    |                        ^^^^^^^^^^^^^^^^^^^^^^^^
 
 error[E0599]: no method named `string` found for type `ChildPropertiesBuilder<ChildPropertiesBuilderStep_missing_required_prop_int>` in the current scope
-  --> $DIR/html-component-fail.rs:85:20
+  --> $DIR/html-component-fail.rs:72:20
    |
-7  | #[derive(Clone, Properties, PartialEq)]
+6  | #[derive(Clone, Properties, PartialEq)]
    |                          - method `string` not found for this
 ...
-85 |     html! { <Child string="abc" /> };
+72 |     html! { <Child string="abc" /> };
    |                    ^^^^^^ method not found in `ChildPropertiesBuilder<ChildPropertiesBuilderStep_missing_required_prop_int>`
 
 error[E0599]: no method named `children` found for type `ChildPropertiesBuilder<ChildPropertiesBuilderStep_missing_required_prop_int>` in the current scope
-  --> $DIR/html-component-fail.rs:89:5
+  --> $DIR/html-component-fail.rs:76:5
    |
-7  | #[derive(Clone, Properties, PartialEq)]
+6  | #[derive(Clone, Properties, PartialEq)]
    |                          - method `children` not found for this
 ...
-89 |     html! { <Child>{ "Not allowed" }</Child> };
+76 |     html! { <Child>{ "Not allowed" }</Child> };
    |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ method not found in `ChildPropertiesBuilder<ChildPropertiesBuilderStep_missing_required_prop_int>`
    |
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)
 
 error[E0599]: no method named `build` found for type `ChildContainerPropertiesBuilder<ChildContainerPropertiesBuilderStep_missing_required_prop_children>` in the current scope
-  --> $DIR/html-component-fail.rs:91:5
+  --> $DIR/html-component-fail.rs:78:5
    |
-36 | #[derive(Clone, Properties)]
+23 | #[derive(Clone, Properties)]
    |                          - method `build` not found for this
 ...
-91 |     html! { <ChildContainer /> };
+78 |     html! { <ChildContainer /> };
    |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ method not found in `ChildContainerPropertiesBuilder<ChildContainerPropertiesBuilderStep_missing_required_prop_children>`
    |
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)
 
 error[E0599]: no method named `build` found for type `ChildContainerPropertiesBuilder<ChildContainerPropertiesBuilderStep_missing_required_prop_children>` in the current scope
-  --> $DIR/html-component-fail.rs:92:5
+  --> $DIR/html-component-fail.rs:79:5
    |
-36 | #[derive(Clone, Properties)]
+23 | #[derive(Clone, Properties)]
    |                          - method `build` not found for this
 ...
-92 |     html! { <ChildContainer></ChildContainer> };
+79 |     html! { <ChildContainer></ChildContainer> };
    |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ method not found in `ChildContainerPropertiesBuilder<ChildContainerPropertiesBuilderStep_missing_required_prop_children>`
    |
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)
 
-error[E0277]: the trait bound `yew::virtual_dom::vcomp::VChild<Child>: std::convert::From<yew::virtual_dom::vnode::VNode>` is not satisfied
-  --> $DIR/html-component-fail.rs:93:5
+error[E0277]: the trait bound `yew::virtual_dom::vcomp::VChild<Child>: std::convert::From<&str>` is not satisfied
+  --> $DIR/html-component-fail.rs:80:5
    |
-93 |     html! { <ChildContainer>{ "Not allowed" }</ChildContainer> };
-   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the trait `std::convert::From<yew::virtual_dom::vnode::VNode>` is not implemented for `yew::virtual_dom::vcomp::VChild<Child>`
+80 |     html! { <ChildContainer>{ "Not allowed" }</ChildContainer> };
+   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the trait `std::convert::From<&str>` is not implemented for `yew::virtual_dom::vcomp::VChild<Child>`
    |
-   = note: required because of the requirements on the impl of `std::convert::Into<yew::virtual_dom::vcomp::VChild<Child>>` for `yew::virtual_dom::vnode::VNode`
+   = note: required because of the requirements on the impl of `std::convert::Into<yew::virtual_dom::vcomp::VChild<Child>>` for `&str`
+   = note: required because of the requirements on the impl of `std::iter::IntoIterator` for `yew::utils::NodeSeq<&str, yew::virtual_dom::vcomp::VChild<Child>>`
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)
 
 error[E0277]: the trait bound `yew::virtual_dom::vcomp::VChild<Child>: std::convert::From<yew::virtual_dom::vnode::VNode>` is not satisfied
-  --> $DIR/html-component-fail.rs:94:5
+  --> $DIR/html-component-fail.rs:81:5
    |
-94 |     html! { <ChildContainer><></></ChildContainer> };
+81 |     html! { <ChildContainer><></></ChildContainer> };
    |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the trait `std::convert::From<yew::virtual_dom::vnode::VNode>` is not implemented for `yew::virtual_dom::vcomp::VChild<Child>`
    |
    = note: required because of the requirements on the impl of `std::convert::Into<yew::virtual_dom::vcomp::VChild<Child>>` for `yew::virtual_dom::vnode::VNode`
+   = note: required because of the requirements on the impl of `std::iter::IntoIterator` for `yew::utils::NodeSeq<yew::virtual_dom::vnode::VNode, yew::virtual_dom::vcomp::VChild<Child>>`
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)
 
 error[E0277]: the trait bound `yew::virtual_dom::vcomp::VChild<Child>: std::convert::From<yew::virtual_dom::vnode::VNode>` is not satisfied
-  --> $DIR/html-component-fail.rs:95:5
+  --> $DIR/html-component-fail.rs:82:5
    |
-95 |     html! { <ChildContainer><Child int=1 /><other /></ChildContainer> };
-   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the trait `std::convert::From<yew::virtual_dom::vnode::VNode>` is not implemented for `yew::virtual_dom::vcomp::VChild<Child>`
+82 |     html! { <ChildContainer><other /></ChildContainer> };
+   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the trait `std::convert::From<yew::virtual_dom::vnode::VNode>` is not implemented for `yew::virtual_dom::vcomp::VChild<Child>`
    |
    = note: required because of the requirements on the impl of `std::convert::Into<yew::virtual_dom::vcomp::VChild<Child>>` for `yew::virtual_dom::vnode::VNode`
+   = note: required because of the requirements on the impl of `std::iter::IntoIterator` for `yew::utils::NodeSeq<yew::virtual_dom::vnode::VNode, yew::virtual_dom::vcomp::VChild<Child>>`
    = note: this error originates in a macro outside of the current crate (in Nightly builds, run with -Z external-macro-backtrace for more info)

tests/macro/html-component-pass.rs
@@ -3,7 +3,7 @@
 use std::marker::PhantomData;
 use yew::prelude::*;
 use yew::html::ChildrenRenderer;
-use yew::virtual_dom::{VChild, VComp, VNode};
+use yew::virtual_dom::{VChild, VNode};
 
 pub struct Generic<G> {
     marker: PhantomData<G>,
@@ -46,37 +46,27 @@ impl Component for Container {
 
 #[derive(Clone)]
 pub enum ChildrenVariants {
-    Child(<Child as Component>::Properties),
-    AltChild(<AltChild as Component>::Properties),
+    Child(VChild<Child>),
+    AltChild(VChild<AltChild>),
 }
 
-impl From<ChildProperties> for ChildrenVariants {
-    fn from(props: ChildProperties) -> Self {
-        ChildrenVariants::Child(props)
+impl From<VChild<Child>> for ChildrenVariants {
+    fn from(comp: VChild<Child>) -> Self {
+        ChildrenVariants::Child(comp)
     }
 }
 
-impl From<()> for ChildrenVariants {
-    fn from(props: ()) -> Self {
-        ChildrenVariants::AltChild(props)
-    }
-}
-
-impl<CHILD> From<VChild<CHILD>> for ChildrenVariants
-where
-    CHILD: Component,
-    CHILD::Properties: Into<ChildrenVariants>,
-{
-    fn from(comp: VChild<CHILD>) -> Self {
-        comp.props.into()
+impl From<VChild<AltChild>> for ChildrenVariants {
+    fn from(comp: VChild<AltChild>) -> Self {
+        ChildrenVariants::AltChild(comp)
     }
 }
 
 impl Into<VNode> for ChildrenVariants {
     fn into(self) -> VNode {
         match self {
-            Self::Child(props) => VComp::new::<Child>(props, NodeRef::default()).into(),
-            Self::AltChild(props) => VComp::new::<AltChild>(props, NodeRef::default()).into(),
+            Self::Child(comp) => VNode::VComp(comp.into()),
+            Self::AltChild(comp) => VNode::VComp(comp.into()),
         }
     }
 }
@@ -230,7 +220,49 @@ fn compile_pass() {
         <ChildContainer int=1>
             <AltChild />
             <Child int=1 />
+            {
+                html_nested! {
+                    <Child int=1 />
+                }
+            }
+            {
+                (0..2)
+                    .map(|i| { html_nested! { <Child int=i /> } })
+                    .collect::<Vec<_>>()
+            }
         </ChildContainer>
+    };
+
+    let variants = || -> Vec<ChildrenVariants> {
+        vec![
+            ChildrenVariants::Child(VChild::new(ChildProperties::default(), NodeRef::default())),
+            ChildrenVariants::AltChild(VChild::new((), NodeRef::default())),
+        ]
+    };
+
+    html! {
+        <>
+            {
+                variants()
+                    .into_iter()
+                    .filter(|c| match c {
+                        ChildrenVariants::Child(_) => true,
+                        _ => false,
+                    })
+                    .collect::<VNode>()
+            }
+            <div>
+                {
+                    variants()
+                        .into_iter()
+                        .filter(|c| match c {
+                            ChildrenVariants::AltChild(_) => true,
+                            _ => false,
+                        })
+                        .collect::<VNode>()
+                }
+            </div>
+        </>
     };
 
     html! {