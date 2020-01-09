7adfef1c89233c13770e47d00289a0663ffd2d64
daxpedda <daxpedda@gmail.com>
2020-01-09 08:40:19 +0100

    `web-sys` listener conversion (#813)
    * `web-sys` listener initial try.
    
    * Improve macros?
    
    * Remove generic from `EventListenerHandle`.
    
    * Fix build.
    
    * A cleaner solution?
    
    * Even cleaner.
    
    * Fix `build.rs`.
    
    * Minor improvements.
    
    * Following the yew toml style.
    
    * Fixing visibility.
    
    * Fix `rustfmt`.
    
    * Add `web-sys` re-exports.
    
    * Move general changes to different PR.
    
    * Remove compat.
    
    * Actually remove `compat.rs`.
    
    * Rename `stdweb` feature to `std_web`.
    
    * Move to gloo's `EventListener` and some polish.
    
    * Remove outdated comment.
    
    * Change `EventHandler` to be cancelled on drop.

---
+++ src/html/listener/macros.rs
@@ -0,0 +1,68 @@
+#[macro_use]
+macro_rules! impl_action {
+    ($($action:ident(event: $type:ident) -> $ret:ty => $convert:expr)*) => {$(
+        impl_action!($action(name: "", event: $type) -> $ret => $convert);
+    )*};
+    ($($action:ident(name: $name:literal, event: $type:ident) -> $ret:ty => $convert:expr)*) => {$(
+        /// An abstract implementation of a listener.
+        pub mod $action {
+            use crate::callback::Callback;
+            #[allow(unused_imports)]
+            use crate::html::listener::*;
+            use crate::virtual_dom::Listener;
+            #[cfg(feature = "std_web")]
+            use stdweb::web::{
+                event::{$type, IEvent},
+                Element, IEventTarget,
+            };
+            #[cfg(feature = "web_sys")]
+            use ::{
+                gloo::events::EventListener,
+                wasm_bindgen::JsValue,
+                web_sys::{$type as WebSysType, Element, EventTarget},
+            };
+
+            /// A wrapper for a callback which attaches event listeners to elements.
+            #[derive(Clone, Debug)]
+            pub struct Wrapper {
+                callback: Callback<Event>,
+            }
+
+            impl Wrapper {
+                /// Create a wrapper for an event-typed callback
+                pub fn new(callback: Callback<Event>) -> Self {
+                    Wrapper { callback }
+                }
+            }
+
+            /// And event type which keeps the returned type.
+            pub type Event = $ret;
+
+            impl Listener for Wrapper {
+                fn kind(&self) -> &'static str {
+                    stringify!($action)
+                }
+
+                fn attach(&self, element: &Element) -> EventListener {
+                    let this = element.clone();
+                    let callback = self.callback.clone();
+                    let listener = move |
+                        #[cfg(feature = "std_web")] event: $type,
+                        #[cfg(feature = "web_sys")] event: &web_sys::Event
+                    | {
+                        event.stop_propagation();
+                        #[cfg(feature = "web_sys")]
+                        let event: WebSysType = JsValue::from(event).into();
+                        callback.emit($convert(&this, event));
+                    };
+                    #[cfg(feature = "std_web")]
+                    {
+                        EventListener(Some(element.add_event_listener(listener)))
+                    }
+                    #[cfg(feature = "web_sys")]
+                    EventListener::new(&EventTarget::from(element.clone()), $name, listener)
+                }
+            }
+        }
+    )*};
+}
