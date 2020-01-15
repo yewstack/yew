a4cc2bd60a7c22785b8520a886c597613516f4d0
Justin Starry <justin.starry@icloud.com>
2020-01-15 17:56:52 +0800

    feedback

--- crates/macro/src/html_tree/html_iterable.rs
+++ crates/macro/src/html_tree/html_iterable.rs
@@ -41,7 +41,7 @@ impl ToTokens for HtmlIterable {
         let expr = &self.0;
         let new_tokens = quote_spanned! {expr.span()=> {
             let mut __yew_vlist = ::yew::virtual_dom::VList::default();
-            for __yew_node in ::std::iter::IntoIterator::into_iter(#expr) {
+            for __yew_node in #expr {
                 __yew_vlist.add_child(__yew_node.into());
             }
             ::yew::virtual_dom::VNode::from(__yew_vlist)
