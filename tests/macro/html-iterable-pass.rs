185ee0f913d0870502f2a1de3614c99192b0cc46
Justin Starry <justin.starry@icloud.com>
2020-01-15 17:19:22 +0800

    Improve html! iteratable ergonomics

--- tests/macro/html-iterable-pass.rs
+++ tests/macro/html-iterable-pass.rs
@@ -3,12 +3,16 @@ use std::iter;
 
 fn compile_pass() {
     html! { for iter::empty::<Html>() };
+    html! { for Vec::<Html>::new() };
     html! { for Vec::<Html>::new().into_iter() };
     html! { for (0..3).map(|num| { html! { <span>{num}</span> } }) };
     html! { for {iter::empty::<Html>()} };
 
     let empty: Vec<Html> = Vec::new();
     html! { for empty.into_iter() };
+
+    let empty: Vec<Html> = Vec::new();
+    html! { for empty };
 }
 
 fn main() {}
