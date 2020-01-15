ca4e976d65c22603bf2848d794281ab88a52a805
Justin Starry <justin.starry@icloud.com>
2020-01-15 20:45:51 +0800

    Add build guards for invalid build configs

--- build.rs
+++ build.rs
@@ -2,14 +2,26 @@ use std::env;
 
 pub fn main() {
     if cfg!(all(feature = "web_sys", feature = "std_web")) {
-        panic!("the `web_sys` and `std_web` cargo features cannot be used simultaneously")
+        panic!("Yew does not allow the `web_sys` and `std_web` cargo features to be used simultaneously");
     } else if cfg!(not(any(feature = "web_sys", feature = "std_web"))) {
-        panic!("please select either the `web_sys` or `std_web` cargo feature")
+        panic!("Yew requires selecting either the `web_sys` or `std_web` cargo feature");
+    }
+
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let using_wasi = target_os == "wasi";
+
+    let cargo_web = env::var("COMPILING_UNDER_CARGO_WEB").unwrap_or_default();
+    let using_cargo_web = cargo_web == "1";
+    if using_cargo_web && cfg!(feature = "web_sys") {
+        panic!("cargo-web is not compatible with web-sys");
     }
 
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
-    let cargo_web = env::var("COMPILING_UNDER_CARGO_WEB").unwrap_or_default();
-    if target_arch == "wasm32" && cargo_web != "1" {
+    let using_wasm_bindgen = target_arch == "wasm32" && !using_cargo_web && !using_wasi;
+    if using_wasm_bindgen {
         println!("cargo:rustc-cfg=feature=\"wasm_bindgen_test\"");
+    } else if cfg!(all(feature = "web_sys", not(feature = "doc_test"))) {
+        let target = env::var("TARGET").unwrap_or_default();
+        panic!("Selected target `{}` is not compatible with web-sys", target);
     }
 }
