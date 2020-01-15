37d856c01b1178b1fbd08671e23a557b90c012fd
daxpedda <daxpedda@gmail.com>
2020-01-15 15:42:11 +0100

    Revert "Revert split."
    This reverts commit 6e3f101dbedde2142f041467a8ae40ef5e3920c5.

--- src/services/fetch/web_sys.rs
+++ src/services/fetch/web_sys.rs
@@ -1,104 +1,96 @@
-//! Service to send HTTP-request to a server.
+//! `web-sys` implementation for the fetch service.
 
-use super::Task;
 use crate::callback::Callback;
 use crate::format::{Binary, Format, Text};
-use serde::Serialize;
-use std::collections::HashMap;
+use crate::services::Task;
+use failure::Fail;
+use js_sys::Reflect;
+use js_sys::{Array, Promise, Uint8Array};
 use std::fmt;
-use stdweb::serde::Serde;
-use stdweb::unstable::{TryFrom, TryInto};
-use stdweb::web::ArrayBuffer;
-use stdweb::{JsSerialize, Value};
-#[allow(unused_imports)]
-use stdweb::{_js_impl, js};
-use thiserror::Error;
+use std::rc::Rc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{self, Receiver};
+use wasm_bindgen::{closure::Closure, JsValue};
+use web_sys::{
+    AbortController, Headers, Request as WebRequest, RequestInit, Response as WebResponse,
+};
+pub use web_sys::{
+    RequestCache as Cache, RequestCredentials as Credentials, RequestMode as Mode,
+    RequestRedirect as Redirect, Window, WorkerGlobalScope,
+};
 
 pub use http::{HeaderMap, Method, Request, Response, StatusCode, Uri};
 
-/// Type to set cache for fetch.
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "kebab-case")]
-pub enum Cache {
-    /// `default` value of cache.
-    #[serde(rename = "default")]
-    DefaultCache,
-    /// `no-store` value of cache.
-    NoStore,
-    /// `reload` value of cache.
-    Reload,
-    /// `no-cache` value of cache.
-    NoCache,
-    /// `force-cache` value of cache
-    ForceCache,
-    /// `only-if-cached` value of cache
-    OnlyIfCached,
+struct ArrayBuffer(Uint8Array);
+
+impl From<ArrayBuffer> for Vec<u8> {
+    fn from(from: ArrayBuffer) -> Self {
+        from.0.to_vec()
+    }
 }
 
-/// Type to set credentials for fetch.
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "kebab-case")]
-pub enum Credentials {
-    /// `omit` value of credentials.
-    Omit,
-    /// `include` value of credentials.
-    Include,
-    /// `same-origin` value of credentials.
-    SameOrigin,
-}
-
-/// Type to set mode for fetch.
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "kebab-case")]
-pub enum Mode {
-    /// `same-origin` value of mode.
-    SameOrigin,
-    /// `no-cors` value of mode.
-    NoCors,
-    /// `cors` value of mode.
-    Cors,
-}
-
-/// Type to set redirect behaviour for fetch.
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "kebab-case")]
-pub enum Redirect {
-    /// `follow` value of redirect.
-    Follow,
-    /// `error` value of redirect.
-    Error,
-    /// `manual` value of redirect.
-    Manual,
+impl From<JsValue> for ArrayBuffer {
+    fn from(from: JsValue) -> Self {
+        ArrayBuffer(Uint8Array::new_with_byte_offset(&from, 0))
+    }
 }
 
 /// Init options for `fetch()` function call.
 /// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
-#[derive(Serialize, Default, Debug)]
+#[derive(Default, Debug)]
 pub struct FetchOptions {
     /// Cache of a fetch request.
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub cache: Option<Cache>,
     /// Credentials of a fetch request.
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub credentials: Option<Credentials>,
     /// Redirect behaviour of a fetch request.
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub redirect: Option<Redirect>,
     /// Request mode of a fetch request.
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub mode: Option<Mode>,
 }
 
+impl Into<RequestInit> for FetchOptions {
+    fn into(self) -> RequestInit {
+        let mut init = RequestInit::new();
+
+        if let Some(cache) = self.cache {
+            init.cache(cache);
+        }
+
+        if let Some(credentials) = self.credentials {
+            init.credentials(credentials);
+        }
+
+        if let Some(redirect) = self.redirect {
+            init.redirect(redirect);
+        }
+
+        if let Some(mode) = self.mode {
+            init.mode(mode);
+        }
+
+        init
+    }
+}
+
 /// Represents errors of a fetch service.
-#[derive(Debug, Error)]
+#[derive(Debug, Fail)]
 enum FetchError {
-    #[error("failed response")]
+    #[fail(display = "failed response")]
     FailedResponse,
+}
+
+#[derive(Debug)]
+struct Handle {
+    active: Rc<AtomicBool>,
+    callbacks: Receiver<Closure<dyn FnMut(JsValue)>>,
+    abort_controller: Option<AbortController>,
+    promise: Promise,
 }
 
 /// A handle to control sent requests. Can be canceled with a `Task::cancel` call.
 #[must_use]
-pub struct FetchTask(Option<Value>);
+pub struct FetchTask(Option<Handle>);
 
 impl fmt::Debug for FetchTask {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -157,10 +149,10 @@ impl FetchService {
     ///# fn dont_execute() {
     ///# let link: ComponentLink<Comp> = unimplemented!();
     ///# let mut fetch_service: FetchService = FetchService::new();
-    ///# let post_request: Request<Result<String, anyhow::Error>> = unimplemented!();
+    ///# let post_request: Request<Result<String, failure::Error>> = unimplemented!();
     /// let task = fetch_service.fetch(
     ///     post_request,
-    ///     link.callback(|response: Response<Result<String, anyhow::Error>>| {
+    ///     link.callback(|response: Response<Result<String, failure::Error>>| {
     ///         if response.status().is_success() {
     ///             Msg::Noop
     ///         } else {
@@ -201,7 +193,7 @@ impl FetchService {
     ///# fn dont_execute() {
     ///# let link: ComponentLink<Comp> = unimplemented!();
     /// let get_request = Request::get("/thing").body(Nothing).unwrap();
-    /// let callback = link.callback(|response: Response<Json<Result<Data, anyhow::Error>>>| {
+    /// let callback = link.callback(|response: Response<Json<Result<Data, failure::Error>>>| {
     ///     if let (meta, Json(Ok(body))) = response.into_parts() {
     ///         if meta.status.is_success() {
     ///             return Msg::FetchResourceComplete(body);
@@ -223,7 +215,14 @@ impl FetchService {
         IN: Into<Text>,
         OUT: From<Text>,
     {
-        fetch_impl::<IN, OUT, String, String>(false, request, None, callback)
+        fetch_impl::<IN, OUT, String, String, _, _>(
+            false,
+            request,
+            None,
+            callback,
+            Into::into,
+            |v| v.as_string().unwrap(),
+        )
     }
 
     /// `fetch` with provided `FetchOptions` object.
@@ -245,7 +244,7 @@ impl FetchService {
     ///# pub enum Msg {}
     ///# fn dont_execute() {
     ///# let link: ComponentLink<Comp> = unimplemented!();
-    ///# let callback = link.callback(|response: Response<Result<String, anyhow::Error>>| unimplemented!());
+    ///# let callback = link.callback(|response: Response<Result<String, failure::Error>>| unimplemented!());
     /// let request = fetch::Request::get("/path/")
     ///     .body(Nothing)
     ///     .unwrap();
@@ -266,7 +265,14 @@ impl FetchService {
         IN: Into<Text>,
         OUT: From<Text>,
     {
-        fetch_impl::<IN, OUT, String, String>(false, request, Some(options), callback)
+        fetch_impl::<IN, OUT, String, String, _, _>(
+            false,
+            request,
+            Some(options),
+            callback,
+            Into::into,
+            |v| v.as_string().unwrap(),
+        )
     }
 
     /// Fetch the data in binary format.
@@ -279,7 +285,14 @@ impl FetchService {
         IN: Into<Binary>,
         OUT: From<Binary>,
     {
-        fetch_impl::<IN, OUT, Vec<u8>, ArrayBuffer>(true, request, None, callback)
+        fetch_impl::<IN, OUT, Vec<u8>, ArrayBuffer, _, _>(
+            true,
+            request,
+            None,
+            callback,
+            |v| Uint8Array::from(v.as_slice()).into(),
+            From::from,
+        )
     }
 
     /// Fetch the data in binary format.
@@ -293,39 +306,50 @@ impl FetchService {
         IN: Into<Binary>,
         OUT: From<Binary>,
     {
-        fetch_impl::<IN, OUT, Vec<u8>, ArrayBuffer>(true, request, Some(options), callback)
+        fetch_impl::<IN, OUT, Vec<u8>, ArrayBuffer, _, _>(
+            true,
+            request,
+            Some(options),
+            callback,
+            |v| Uint8Array::from(v.as_slice()).into(),
+            From::from,
+        )
     }
 }
 
-fn fetch_impl<IN, OUT: 'static, T, X>(
+fn fetch_impl<
+    IN,
+    OUT: 'static,
+    T,
+    X: Into<T>,
+    IC: Fn(T) -> JsValue,
+    FC: 'static + Fn(JsValue) -> X,
+>(
     binary: bool,
     request: Request<IN>,
     options: Option<FetchOptions>,
     callback: Callback<Response<OUT>>,
+    into_conversion: IC,
+    from_conversion: FC,
 ) -> FetchTask
 where
     IN: Into<Format<T>>,
     OUT: From<Format<T>>,
-    T: JsSerialize,
-    X: TryFrom<Value> + Into<T>,
 {
     // Consume request as parts and body.
     let (parts, body) = request.into_parts();
 
     // Map headers into a Js serializable HashMap.
-    let header_map: HashMap<&str, &str> = parts
-        .headers
-        .iter()
-        .map(|(k, v)| {
-            (
-                k.as_str(),
-                v.to_str().unwrap_or_else(|_| {
-                    panic!("Unparsable request header {}: {:?}", k.as_str(), v)
-                }),
-            )
-        })
-        .collect();
-
+    let header_map = Headers::new().unwrap();
+    for (k, v) in parts.headers.iter().map(|(k, v)| {
+        (
+            k.as_str(),
+            v.to_str()
+                .unwrap_or_else(|_| panic!("Unparsable request header {}: {:?}", k.as_str(), v)),
+        )
+    }) {
+        header_map.append(k, v).unwrap();
+    }
     // Formats URI.
     let uri = format!("{}", parts.uri);
     let method = parts.method.as_str();
@@ -334,14 +358,25 @@ where
     // Prepare the response callback.
     // Notice that the callback signature must match the call from the javascript
     // side. There is no static check at this point.
-    let callback = move |success: bool, status: u16, headers: HashMap<String, String>, data: X| {
+    let callback = move |data: Option<X>, status: u16, headers: Headers| {
         let mut response_builder = Response::builder().status(status);
-        for (key, values) in headers {
-            response_builder = response_builder.header(key.as_str(), values.as_str());
+        // convert `headers` to `Iterator<Item = (String, String)>`
+        let headers = js_sys::try_iter(&headers)
+            .unwrap()
+            .unwrap()
+            .map(Result::unwrap)
+            .map(|entry| {
+                let entry = Array::from(&entry);
+                let key = entry.get(0);
+                let value = entry.get(1);
+                (key.as_string().unwrap(), value.as_string().unwrap())
+            });
+        for (key, value) in headers {
+            response_builder = response_builder.header(key.as_str(), value.as_str());
         }
 
         // Deserialize and wrap response data into a Text object.
-        let data = if success {
+        let data = if let Some(data) = data {
             Ok(data.into())
         } else {
             Err(FetchError::FailedResponse.into())
@@ -351,71 +386,92 @@ where
         callback.emit(response);
     };
 
-    #[allow(clippy::too_many_arguments)]
-    let handle = js! {
-        var body = @{body};
-        if (@{binary} && body != null) {
-            body = Uint8Array.from(body);
+    let mut data = RequestInit::new();
+    data.method(method);
+    data.body(body.map(into_conversion).as_ref());
+    data.headers(&header_map);
+    let request = WebRequest::new_with_str_and_init(&uri, &data).unwrap();
+    let active = Rc::new(AtomicBool::new(true));
+    let (sender, receiver) = mpsc::channel();
+    let active_outer_clone = Rc::clone(&active);
+    let callback_outer_clone = callback.clone();
+    let sender_clone = sender.clone();
+    let closure_then = move |response: JsValue| {
+        let response = WebResponse::from(response);
+        let promise = if binary {
+            response.array_buffer()
+        } else {
+            response.text()
         }
-        var data = {
-            method: @{method},
-            body: body,
-            headers: @{header_map},
-        };
-        var request = new Request(@{uri}, data);
-        var callback = @{callback};
-        var abortController = AbortController ? new AbortController() : null;
-        var handle = {
-            active: true,
-            callback,
-            abortController,
-        };
-        var init = @{Serde(options)} || {};
-        if (abortController && !("signal" in init)) {
-            init.signal = abortController.signal;
-        }
-        fetch(request, init).then(function(response) {
-            var promise = (@{binary}) ? response.arrayBuffer() : response.text();
-            var status = response.status;
-            var headers = {};
-            response.headers.forEach(function(value, key) {
-                headers[key] = value;
-            });
-            promise.then(function(data) {
-                if (handle.active == true) {
-                    handle.active = false;
-                    callback(true, status, headers, data);
-                    callback.drop();
-                }
-            }).catch(function(err) {
-                if (handle.active == true) {
-                    handle.active = false;
-                    callback(false, status, headers, data);
-                    callback.drop();
-                }
-            });
-        }).catch(function(e) {
-            if (handle.active == true) {
-                var data = (@{binary}) ? new ArrayBuffer() : "";
-                handle.active = false;
-                callback(false, 408, {}, data);
-                callback.drop();
+        .unwrap();
+        let status = response.status();
+        let headers = response.headers();
+        let active_clone = Rc::clone(&active_outer_clone);
+        let callback_clone = callback_outer_clone.clone();
+        let headers_clone = headers.clone();
+        let closure_then = move |data: JsValue| {
+            let data = from_conversion(data);
+            if active_clone.compare_and_swap(true, false, Ordering::SeqCst) {
+                callback_clone(Some(data), status, headers_clone);
             }
-        });
-        return handle;
+        };
+        let closure_then = Closure::once(closure_then);
+        let closure_catch = move |_| {
+            if active_outer_clone.compare_and_swap(true, false, Ordering::SeqCst) {
+                callback_outer_clone(None, status, headers);
+            }
+        };
+        let closure_catch = Closure::once(closure_catch);
+        #[allow(unused_must_use)]
+        {
+            promise.then(&closure_then).catch(&closure_catch);
+        }
+        sender_clone.send(closure_then).unwrap();
+        sender_clone.send(closure_catch).unwrap();
     };
-    FetchTask(Some(handle))
+    let closure_then = Closure::once(closure_then);
+    let active_clone = Rc::clone(&active);
+    let closure_catch = move |_| {
+        if active_clone.compare_and_swap(true, false, Ordering::SeqCst) {
+            callback(None, 408, Headers::new().unwrap());
+        }
+    };
+    let closure_catch = Closure::wrap(Box::new(closure_catch) as Box<dyn FnMut(JsValue)>);
+    let abort_controller = AbortController::new().ok();
+    let mut init = options.map_or_else(RequestInit::new, Into::into);
+    if let Some(abort_controller) = &abort_controller {
+        init.signal(Some(&abort_controller.signal()));
+    }
+    let global: JsValue = js_sys::global().into();
+    let promise = if Reflect::has(&global, &String::from("Window").into()).unwrap() {
+        Window::from(global).fetch_with_request_and_init(&request, &init)
+    } else if Reflect::has(&global, &String::from("WorkerGlobalScope").into()).unwrap() {
+        WorkerGlobalScope::from(global).fetch_with_request_and_init(&request, &init)
+    } else {
+        panic!("failed to get global context")
+    };
+    let promise = promise.then(&closure_then).catch(&closure_catch);
+    sender.send(closure_then).unwrap();
+    sender.send(closure_catch).unwrap();
+
+    FetchTask(Some(Handle {
+        active,
+        callbacks: receiver,
+        abort_controller,
+        promise,
+    }))
 }
 
 impl Task for FetchTask {
     fn is_active(&self) -> bool {
         if let Some(ref task) = self.0 {
-            let result = js! {
-                var the_task = @{task};
-                return the_task.active &&
-                        (!the_task.abortController || !the_task.abortController.signal.aborted);
-            };
-            result.try_into().unwrap_or(false)
+            task.active.load(Ordering::SeqCst)
+                && task
+                    .abort_controller
+                    .as_ref()
+                    .map(|abort_controller| abort_controller.signal().aborted())
+                    .filter(|value| *value)
+                    .is_none()
         } else {
             false
         }
@@ -428,14 +484,19 @@ impl Task for FetchTask {
             .0
             .take()
             .expect("tried to cancel request fetching twice");
-        js! {  @(no_return)
-            var handle = @{handle};
-            handle.active = false;
-            handle.callback.drop();
-            if (handle.abortController) {
-                handle.abortController.abort();
-            }
+
+        thread_local! {
+            static CATCH: Closure<dyn FnMut(JsValue)> = Closure::wrap(Box::new(|_| ()) as Box<dyn FnMut(JsValue)>);
         }
+        handle.active.store(false, Ordering::SeqCst);
+        #[allow(unused_must_use)]
+        {
+            CATCH.with(|c| handle.promise.catch(&c));
+        }
+        if let Some(abort_controller) = handle.abort_controller {
+            abort_controller.abort();
+        }
+        handle.callbacks.try_iter().for_each(drop);
     }
 }
 
