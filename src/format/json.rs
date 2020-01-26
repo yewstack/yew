045f54d13600543e156c5ac389566397f1335e3d
Justin Starry <justin.starry@icloud.com>
2020-01-26 17:53:55 +0800

    Move tests (#897) (#898)

--- src/format/json.rs
+++ src/format/json.rs
@@ -18,3 +18,27 @@ pub struct Json<T>(pub T);
 text_format!(Json based on serde_json);
 
 binary_format!(Json based on serde_json);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::format::{Binary, Text};
+    use serde::{Deserialize, Serialize};
+    #[cfg(feature = "wasm_test")]
+    use wasm_bindgen_test::wasm_bindgen_test as test;
+
+    #[test]
+    fn json_format() {
+        #[derive(Serialize, Deserialize)]
+        struct Data {
+            value: u8,
+        }
+
+        let Json(data): Json<Result<Data, _>> = Json::from(Ok(r#"{"value": 123}"#.to_string()));
+        let data = data.unwrap();
+        assert_eq!(data.value, 123);
+
+        let _stored: Text = Json(&data).into();
+        let _stored: Binary = Json(&data).into();
+    }
+}
