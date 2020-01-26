38f4054f97e1947427d43072a46b0e18e6bac41b
Justin Starry <justin.starry@icloud.com>
2020-01-26 17:36:16 +0800

    Move tests (#897)

--- src/virtual_dom/vcomp.rs
+++ src/virtual_dom/vcomp.rs
@@ -293,3 +293,67 @@ impl<COMP: Component> fmt::Debug for VChild<COMP> {
         f.write_str("VChild<_>")
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::macros::Properties;
+    use crate::{html, Component, ComponentLink, Html, ShouldRender};
+    #[cfg(feature = "wasm_test")]
+    use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
+
+    #[cfg(feature = "wasm_test")]
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    struct Comp;
+
+    #[derive(Clone, PartialEq, Properties)]
+    struct Props {
+        field_1: u32,
+        field_2: u32,
+    }
+
+    impl Component for Comp {
+        type Message = ();
+        type Properties = Props;
+
+        fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+            Comp
+        }
+
+        fn update(&mut self, _: Self::Message) -> ShouldRender {
+            unimplemented!();
+        }
+
+        fn view(&self) -> Html {
+            unimplemented!();
+        }
+    }
+
+    #[test]
+    fn set_properties_to_component() {
+        html! {
+            <Comp />
+        };
+
+        html! {
+            <Comp field_1=1 />
+        };
+
+        html! {
+            <Comp field_2=2 />
+        };
+
+        html! {
+            <Comp field_1=1 field_2=2 />
+        };
+
+        let props = Props {
+            field_1: 1,
+            field_2: 1,
+        };
+
+        html! {
+            <Comp with props />
+        };
+    }
+}
--- src/virtual_dom/vtext.rs
+++ src/virtual_dom/vtext.rs
@@ -101,3 +101,43 @@ impl PartialEq for VText {
         self.text == other.text
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{html, Component, ComponentLink, Html, ShouldRender};
+    #[cfg(feature = "wasm_test")]
+    use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
+
+    #[cfg(feature = "wasm_test")]
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    struct Comp;
+
+    impl Component for Comp {
+        type Message = ();
+        type Properties = ();
+
+        fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+            Comp
+        }
+
+        fn update(&mut self, _: Self::Message) -> ShouldRender {
+            unimplemented!();
+        }
+
+        fn view(&self) -> Html {
+            unimplemented!();
+        }
+    }
+
+    #[test]
+    fn text_as_root() {
+        html! {
+            "Text Node As Root"
+        };
+
+        html! {
+            { "Text Node As Root" }
+        };
+    }
+}
