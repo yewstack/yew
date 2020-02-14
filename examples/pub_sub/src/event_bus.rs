4b7ad994e79cbcff0900036a10ece329e9764854
Your Name <you@example.com>
2020-02-14 16:53:49 +0100

    Add components interaction example
    This add a small example of how to use agents in order to send
    messages between components.

---
+++ examples/pub_sub/src/event_bus.rs
@@ -0,0 +1,48 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use std::fmt::Debug;
+use yew::worker::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Request {
+    EventBusMsg(String),
+}
+
+pub struct EventBus {
+    link: AgentLink<EventBus>,
+    subscribers: HashSet<HandlerId>,
+}
+
+impl Agent for EventBus {
+    type Reach = Context;
+    type Message = ();
+    type Input = Request;
+    type Output = String;
+
+    fn create(link: AgentLink<Self>) -> Self {
+        EventBus {
+            link,
+            subscribers: HashSet::new(),
+        }
+    }
+
+    fn update(&mut self, _: Self::Message) {}
+
+    fn handle_input(&mut self, msg: Self::Input, _: HandlerId) {
+        match msg {
+            Request::EventBusMsg(s) => {
+                for sub in self.subscribers.iter() {
+                    self.link.respond(*sub, s.clone());
+                }
+            }
+        }
+    }
+
+    fn connected(&mut self, id: HandlerId) {
+        self.subscribers.insert(id);
+    }
+
+    fn disconnected(&mut self, id: HandlerId) {
+        self.subscribers.remove(&id);
+    }
+}
