d4dd3f6efb16431fa3dd8fdd5453c102dc04741b
Serhii <sergiybiluk@gmail.com>
2020-03-03 20:32:58 +0100

    init

--- src/html/scope.rs
+++ src/html/scope.rs
@@ -16,9 +16,9 @@ cfg_if! {
 /// Updates for a `Component` instance. Used by scope sender.
 pub(crate) enum ComponentUpdate<COMP: Component> {
     /// Wraps messages for a component.
-    Message(COMP::Message),
+    Message(Into<COMP::Message>),
     /// Wraps batch of messages for a component.
-    MessageBatch(Vec<COMP::Message>),
+    MessageBatch(Vec<Into<COMP::Message>>),
     /// Wraps properties for a component.
     Properties(COMP::Properties),
 }
@@ -108,12 +108,12 @@ impl<COMP: Component> Scope<COMP> {
     }
 
     /// Send a message to the component
-    pub fn send_message(&self, msg: COMP::Message) {
+    pub fn send_message(&self, msg: Into<COMP::Message>) {
         self.update(ComponentUpdate::Message(msg));
     }
 
     /// Send a batch of messages to the component
-    pub fn send_message_batch(&self, messages: Vec<COMP::Message>) {
+    pub fn send_message_batch(&self, messages:Vec<Into<COMP::Message>>) {
         self.update(ComponentUpdate::MessageBatch(messages));
     }
 
@@ -121,7 +121,7 @@ impl<COMP: Component> Scope<COMP> {
     /// update method when invoked.
     pub fn callback<F, IN>(&self, function: F) -> Callback<IN>
     where
-        F: Fn(IN) -> COMP::Message + 'static,
+        F: Fn(IN) -> Into<COMP::Message> + 'static,
     {
         let scope = self.clone();
         let closure = move |input| {
@@ -135,7 +135,7 @@ impl<COMP: Component> Scope<COMP> {
     /// component's update method when called.
     pub fn batch_callback<F, IN>(&self, function: F) -> Callback<IN>
     where
-        F: Fn(IN) -> Vec<COMP::Message> + 'static,
+        F: Fn(IN) -> Vec<Into<COMP::Message>> + 'static,
     {
         let scope = self.clone();
         let closure = move |input| {
