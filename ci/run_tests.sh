09f879544671a59568ed0f5faa7fbe871281629f
Mathis <mk@thepeaklab.com>
2020-03-16 11:14:46 +0100

    hook test now running as stdweb. also removed redundant deps

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -22,5 +22,5 @@ cargo test --doc --features doc_test,wasm_test,yaml,msgpack,cbor,web_sys
 
 (cd crates/functional \
   && cargo test --features web_sys,wasm_test --target wasm32-unknown-unknown \
-  && cargo test --features web_sys,wasm_test --target wasm32-unknown-unknown)
+  && cargo test --features std_web,wasm_test --target wasm32-unknown-unknown)
 
