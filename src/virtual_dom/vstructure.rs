6c58a462741ff9af556662431a3d99cf87d327ab
spieljs <spieljs@gmail.com>
2020-03-27 02:05:19 +0100

    add tests

--- src/virtual_dom/vstructure.rs
+++ src/virtual_dom/vstructure.rs
@@ -1,85 +1,81 @@
 //! This module contains the implementation to get all the VNode structure.
 //! it is useful for testing and debuging. For example we want to know what contain this VNode:
 //! ```
-//! let button = html! {
-//!     <button
-//!         onclick=link.callback(|_| Msg::Clicked)
-//!         class="button standard medium regular test-button">
-//!         <div id="parent">{"parent"}</div>
-//!     </button>
+//! let example = html! {
+//!     <div id="example">{"example"}</div>
 //! };
 //! ```
 //!
 //! Now we use VStructure to get the complete Vnode button and log the result:
 //!
 //! ```
-//!   let vnode_button = VNodeStruct::new(button);
+//!   let vnode_example = VNodeStruct::new(example);
 //!
-//!   console.log(&format!("{:#?}", vnode_button));
+//!   console.log(&format!("{:#?}", example));
 //! ```
 //!
 //! We will get this in the console:
 //!
 //! ```
 //! VNodeStruct {
-//! vtag: Some(
-//!     VTagStruct {
-//!         reference: None,
-//!         attributes: {
-//!             "id": "example",
-//!         },
-//!         classes: Classes {
-//!             set: {},
-//!         },
-//!         value: None,
-//!         kind: None,
-//!         checked: false,
-//!         node_ref: NodeRef(
-//!             RefCell {
-//!                 value: NodeRefInner {
-//!                     node: None,
-//!                     link: None,
-//!                 },
+//!     vtag: Some(
+//!         VTagStruct {
+//!             reference: None,
+//!             attributes: {
+//!                 "id": "example",
 //!             },
-//!         ),
-//!     },
-//! ),
-//! vlist: None,
-//! vtext: None,
-//! vcomp: None,
-//! vref: None,
-//! children: Some(
-//!     [
-//!         VNodeStruct {
-//!             vtag: None,
-//!             vlist: Some(
-//!                 VList {
-//!                     children: [
-//!                         VText { text: example },
-//!                     ],
-//!                     elide_placeholder: true,
+//!             classes: Classes {
+//!                 set: {},
+//!             },
+//!             value: None,
+//!             kind: None,
+//!             checked: false,
+//!             node_ref: NodeRef(
+//!                 RefCell {
+//!                     value: NodeRefInner {
+//!                         node: None,
+//!                         link: None,
+//!                     },
 //!                 },
 //!             ),
-//!             vtext: None,
-//!             vcomp: None,
-//!             vref: None,
-//!             children: Some(
-//!                 [
-//!                     VNodeStruct {
-//!                         vtag: None,
-//!                         vlist: None,
-//!                         vtext: Some(
-//!                             VText { text: example },
-//!                         ),
-//!                         vcomp: None,
-//!                         vref: None,
-//!                         children: None,
-//!                     },
-//!                 ],
-//!             ),
 //!         },
-//!     ],
-//! ),
+//!     ),
+//!     vlist: None,
+//!     vtext: None,
+//!     vcomp: None,
+//!     vref: None,
+//!     children: Some(
+//!         [
+//!             VNodeStruct {
+//!                 vtag: None,
+//!                 vlist: Some(
+//!                     VList {
+//!                         children: [
+//!                             VText { text: example },
+//!                         ],
+//!                         elide_placeholder: true,
+//!                     },
+//!                 ),
+//!                 vtext: None,
+//!                 vcomp: None,
+//!                 vref: None,
+//!                 children: Some(
+//!                     [
+//!                         VNodeStruct {
+//!                             vtag: None,
+//!                             vlist: None,
+//!                             vtext: Some(
+//!                                 VText { text: example },
+//!                             ),
+//!                             vcomp: None,
+//!                             vref: None,
+//!                             children: None,
+//!                         },
+//!                     ],
+//!                 ),
+//!             },
+//!         ],
+//!     ),
 //! }
 //! ```
 
@@ -193,5 +189,51 @@ impl VNodeStruct {
                 children: None,
             },
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::VNodeStruct;
+    use crate::html;
+
+    #[cfg(feature = "wasm_test")]
+    use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
+
+    #[cfg(feature = "wasm_test")]
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[test]
+    fn should_match_the_vnode_structures() {
+        let vnode = html! {
+            <div id="example">{"example"}</div>
+        };
+
+        let vnode_structure = VNodeStruct::new(vnode);
+
+        let vnode_expected = html! {
+            <div id="example">{"example"}</div>
+        };
+
+        let vnode_structure_expected = VNodeStruct::new(vnode_expected);
+
+        assert_eq!(vnode_structure, vnode_structure_expected);
+    }
+
+    #[test]
+    fn should_no_match_the_vnode_structures() {
+        let vnode = html! {
+            <div id="example">{"example"}</div>
+        };
+
+        let vnode_structure = VNodeStruct::new(vnode);
+
+        let vnode_expected = html! {
+            <div id="second-example">{"second example"}</div>
+        };
+
+        let vnode_structure_expected = VNodeStruct::new(vnode_expected);
+
+        assert_ne!(vnode_structure, vnode_structure_expected);
     }
 }
