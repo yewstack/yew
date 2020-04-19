a20ab9046767b8cd66aab1fec1e631fc989e1eb8
Bill Thiede <git@xinu.tv>
2020-04-19 09:23:18 -0700

    ci: verify all examples build.
    Using examples/showcase masked breakages in multiple examples.

--- ci/check_examples.sh
+++ ci/check_examples.sh
@@ -3,19 +3,21 @@ echo "$(rustup default)" | grep -q "1.39.0"
 emscripten_supported=$?
 set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
-# Showcase includes all other examples
-cd examples/showcase
+# Make sure all examples are buildable with stdweb and web-sys.
+for ex in $(find examples -maxdepth 1 -mindepth 1 -type d); do
+  pushd $ex
 
-# TODO Can't build some demos with release, need fix
+  # TODO Can't build some demos with release, need fix
 
-if [ "$emscripten_supported" == "0" ]; then
-  # TODO - Emscripten builds are broken on rustc > 1.39.0
-  cargo web build --target asmjs-unknown-emscripten --features std_web
-  cargo web build --target wasm32-unknown-emscripten --features std_web
-fi
+  if [ "$emscripten_supported" == "0" ]; then
+    # TODO - Emscripten builds are broken on rustc > 1.39.0
+    cargo web build --target asmjs-unknown-emscripten --features std_web
+    cargo web build --target wasm32-unknown-emscripten --features std_web
+  fi
 
-cargo web build --target wasm32-unknown-unknown --features std_web
-cargo build --target wasm32-unknown-unknown --features web_sys
+  cargo web build --target wasm32-unknown-unknown --features std_web
+  cargo build --target wasm32-unknown-unknown --features web_sys
 
-# Reset cwd
-cd ../..
+  # Reset cwd
+  popd
+done
