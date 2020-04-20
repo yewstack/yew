a25b1ebe9eeea731da809b39e6fb05cd898a4c3e
Bill Thiede <git@xinu.tv>
2020-04-19 19:25:21 -0700

    examples/future: add change implementation.

--- examples/futures/src/lib.rs
+++ examples/futures/src/lib.rs
@@ -101,6 +101,10 @@ impl Component for Model {
         }
     }
 
+    fn change(&mut self, _: Self::Properties) -> bool {
+        false
+    }
+
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::SetMarkdownFetchState(fetch_state) => {
