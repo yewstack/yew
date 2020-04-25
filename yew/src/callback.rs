7ca601d9a46a11fd1c08af20223b5763c9b836cf
Luke Randall <luke.randall@gmail.com>
2020-04-25 08:26:21 +0100

    Convert Callback to be an enum to support Fn and FnOnce. (#1125)
    * Convert Callback to be an enum to support Fn and FnOnce.
    
    * Run cargo fmt
    
    * Update macro_test stderr
    
    * Use type alias to simplify CallbackOnce
    
    * Add Callback::callback_once
    
    * Add Scope::callback_once
    
    * Run cargo fmt
    
    * Rename to Callback::once

--- yew/src/callback.rs
+++ yew/src/callback.rs
@@ -1,5 +1,6 @@
-//! This module contains structs to interact with `Scope`s.
+//! This module contains data types for interacting with `Scope`s.
 
+use std::cell::RefCell;
 use std::fmt;
 use std::rc::Rc;
 
@@ -10,36 +11,73 @@ use std::rc::Rc;
 /// Callbacks should be used from JS callbacks or `setTimeout` calls.
 /// </aside>
 /// `Rc` wrapper used to make it clonable.
-pub struct Callback<IN>(Rc<dyn Fn(IN)>);
+pub enum Callback<IN> {
+    /// A callback that can be called multiple times
+    Callback(Rc<dyn Fn(IN)>),
+    /// A callback that will only be called once. Panics if it is called again
+    CallbackOnce(Rc<CallbackOnce<IN>>),
+}
+
+type CallbackOnce<IN> = RefCell<Option<Box<dyn FnOnce(IN)>>>;
 
 impl<IN, F: Fn(IN) + 'static> From<F> for Callback<IN> {
     fn from(func: F) -> Self {
-        Callback(Rc::new(func))
+        Callback::Callback(Rc::new(func))
     }
 }
 
 impl<IN> Clone for Callback<IN> {
     fn clone(&self) -> Self {
-        Callback(self.0.clone())
+        match self {
+            Callback::Callback(cb) => Callback::Callback(cb.clone()),
+            Callback::CallbackOnce(cb) => Callback::CallbackOnce(cb.clone()),
+        }
     }
 }
 
 impl<IN> PartialEq for Callback<IN> {
     fn eq(&self, other: &Callback<IN>) -> bool {
-        Rc::ptr_eq(&self.0, &other.0)
+        match (&self, &other) {
+            (Callback::Callback(cb), Callback::Callback(other_cb)) => Rc::ptr_eq(cb, other_cb),
+            (Callback::CallbackOnce(cb), Callback::CallbackOnce(other_cb)) => {
+                Rc::ptr_eq(cb, other_cb)
+            }
+            _ => false,
+        }
     }
 }
 
 impl<IN> fmt::Debug for Callback<IN> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("Callback<_>")
+        let data = match self {
+            Callback::Callback(_) => "Callback<_>",
+            Callback::CallbackOnce(_) => "CallbackOnce<_>",
+        };
+
+        f.write_str(data)
     }
 }
 
 impl<IN> Callback<IN> {
     /// This method calls the actual callback.
     pub fn emit(&self, value: IN) {
-        (self.0)(value);
+        match self {
+            Callback::Callback(cb) => cb(value),
+            Callback::CallbackOnce(rc) => {
+                let cb = rc.replace(None);
+                let f = cb.expect("callback in CallbackOnce has already been used");
+                f(value)
+            }
+        };
+    }
+
+    /// Creates a callback from a FnOnce. You are responsible for ensuring
+    /// the callback is only called once otherwise it will panic.
+    pub fn once<F>(func: F) -> Self
+    where
+        F: FnOnce(IN) + 'static,
+    {
+        Callback::CallbackOnce(Rc::new(RefCell::new(Some(Box::new(func)))))
     }
 
     /// Creates a no-op callback which can be used when it is not suitable to use an
