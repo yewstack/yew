364ff359bb15a83fa476b374be58a2dfce366d0f
Justin Starry <justin.starry@icloud.com>
2020-04-25 18:08:14 +0800

    Move components out of core crate

---
+++ yew-components/src/lib.rs
@@ -0,0 +1,8 @@
+//! This crate contains useful Yew components.
+//! At this moment it only includes typed `Select`.
+
+#[doc(hidden)]
+pub mod select;
+
+#[doc(inline)]
+pub use self::select::Select;
