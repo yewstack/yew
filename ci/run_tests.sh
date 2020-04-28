d6b221af505481cc4675f8daf20285f1fcf2306f
Stig Johan Berggren <stigjb@gmail.com>
2020-04-28 14:44:33 +0200

    Run Yewtil tests in CI

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -16,3 +16,5 @@ set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_
   && cargo test --doc)
 
 (cd yew-stdweb && cargo test --target wasm32-unknown-unknown --features wasm_test)
+
+(cd yewtil && cargo test)
