fe972b851ccac1e1f95865398d16963ee9faff5e
Filip Bucek <fbucek@invloop.cz>
2020-04-28 15:55:31 +0200

    examples updated to work with wasm-bindgen

--- examples/build.sh
+++ examples/build.sh
@@ -3,9 +3,8 @@
 # src: https://gist.github.com/fbucek/f986da3cc3a9bbbd1573bdcb23fed2e1
 set -e # error -> trap -> exit
 function info() { echo -e "[\033[0;34m $@ \033[0m]"; } # blue: [ info message ]
-function pass() { echo -e "[\033[0;32mPASS\033[0m] $@"; } # green: [PASS]
 function fail() { FAIL="true"; echo -e "[\033[0;31mFAIL\033[0m] $@"; } # red: [FAIL]
-trap 'LASTRES=$?; LAST=$BASH_COMMAND; if [[ LASTRES -ne 0 ]]; then fail "Command: \"$LAST\" exited with exit code: $LASTRES"; elif [ "$FAIL" == "true"  ]; then fail finished with error; else echo -e "[\033[0;32m Finished \033[0m]";fi' EXIT
+trap 'LASTRES=$?; LAST=$BASH_COMMAND; if [[ LASTRES -ne 0 ]]; then fail "Command: \"$LAST\" exited with exit code: $LASTRES"; elif [ "$FAIL" == "true"  ]; then fail finished with error; else echo -e "[\033[0;32m Finished $@ \033[0m]";fi' EXIT
 SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # this source dir
 
 cd $SRCDIR # ensure current dir is this dir
@@ -13,28 +12,27 @@ cd $SRCDIR # ensure current dir is this dir
 EXAMPLE=$1
 cd $EXAMPLE
 
-
+# folder with _wp -> prepared for wasmpack
 if [[ $EXAMPLE == *_wp ]]; then
-    info "Using wasm-pack"
-    wasm-pack build --debug --target web --out-name wasm --out-dir ../static
+    info "Building: $EXAMPLE using wasm-pack"
+    wasm-pack build --release --target web --out-name wasm --out-dir ../static
 else 
-    # Remove -wb from variable
-    #EXAMPLE="${EXAMPLE%-wb}"
-
-    info "Using wasm-bindgen"
+    info "Building: $EXAMPLE using wasm-bindgen"
     # When CARGO_TARGET_DIR is set
     TARGET_DIR=../target/wasm32-unknown-unknown/release
     if [ ! -z "$CARGO_TARGET_DIR" ]; then 
         TARGET_DIR=$CARGO_TARGET_DIR/wasm32-unknown-unknown/release
     fi
 
-    info "Cargo build"
     cargo build --release --target wasm32-unknown-unknown 
-    info "wasm-bindgen"
     wasm-bindgen --target web --no-typescript --out-dir $SRCDIR/static/ --out-name wasm $TARGET_DIR/$EXAMPLE.wasm
-    # @see https://rustwasm.github.io/book/game-of-life/code-size.html
-    mv $SRCDIR/static/wasm_bg.wasm $SRCDIR/static/wasm_bg_orig.wasm
-    # Optimalization 
-    # -Os -> size
-    wasm-opt $SRCDIR/static/wasm_bg_orig.wasm -Os -o $SRCDIR/static/wasm_bg.wasm
+
+    if [[ $2 == "--opt" ]]; then
+        info "Using wasm-opt"
+        # @see https://rustwasm.github.io/book/game-of-life/code-size.html
+        mv $SRCDIR/static/wasm_bg.wasm $SRCDIR/static/wasm_bg_orig.wasm
+        # Optimalization 
+        # -Os -> size
+        wasm-opt $SRCDIR/static/wasm_bg_orig.wasm -Os -o $SRCDIR/static/wasm_bg.wasm
+    fi
 fi
--- examples/readme.md
+++ examples/readme.md
@@ -1,12 +1,34 @@
 # Yew Examples
 
-Currently `minimal` and `minimal_wb` is working using running script `build.sh`
+Use `build.sh` script to build examples. 
+
+Examples are prepared for `wasm-bindgen` expect `minimal_wp` which is prepared for `wasm-pack`.
 
 ## How to run
 
 ```
 git clone https://github.com/yewstack/yew.git
 cd yew
-examples/buils.sh minimal # or minimal_wb
-python3 -m http.server 8000 --directory examples/static # 
+examples/build.sh minimal # example subfolder
+python3 -m http.server 8000 --directory examples/static # open localhost:8000 in browser
 ```
+
+Note: VSCode has extension "Live Server" which can be used to run example in browser with automatic reload
+
+## Build size otimalization 
+
+- It is possible to optimize code size using `wasm-opt` when building using `wasm-bindgen`
+    - possible to enable opt when building `example/build.sh minimal --opt
+- `wasm-pack` does that by default in release build
+
+**Release size of example 'minimal'**
+
+| release commands            | size ( uncompressed )
+|---                          |---
+| wasm-bindgen                | 158KB  
+| wasm-binggen + wasm-opt -Os | 116KB 
+| wasm-pack                   | 99 KB
+
+## Known issues
+
+- `multi_thread` not working correctly. 
