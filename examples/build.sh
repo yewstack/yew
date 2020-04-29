138de4c276ec13ccc6b1fa97eb0e166ce6451456
fbucek <fbucek@atlas.cz>
2020-04-29 16:57:55 +0200

    updated examples, added build script (#1165)
    * script for building examples
    
    minimal example updated to work with wasm-pack
    minimal_wb exampel to be used with wasm-bindgen directly
    
    * minimal is wasm-bindgen as default ( wp wasm-pack )
    
    * examples updated to work with wasm-bindgen
    
    * script to run all yew/examples
    
    * upated example doc, added script to multi-thread
    
    * cargo fmt, disabled build_examples.sh
    
    * one build.sh for examples, updated doc
    
    * removed build size optimalization
    
    * wasm-pack requirement info in lib.rs
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/readme.md
    
    * Update examples/build.sh
    
    Co-Authored-By: Justin Starry <justin.m.starry@gmail.com>
    
    * Update examples/build.sh exit trap
    
    Co-Authored-By: Justin Starry <justin.m.starry@gmail.com>
    
    Co-authored-by: Justin Starry <justin.m.starry@gmail.com>

---
+++ examples/build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# src: https://gist.github.com/fbucek/f986da3cc3a9bbbd1573bdcb23fed2e1
+set -e # error -> trap -> exit
+function info() { echo -e "[\033[0;34m $@ \033[0m]"; } # blue: [ info message ]
+function fail() { FAIL="true"; echo -e "[\033[0;31mFAIL\033[0m] $@"; } # red: [FAIL]
+trap 'LASTRES=$?; LAST=$BASH_COMMAND; if [[ LASTRES -ne 0 ]]; then fail "Command: \"$LAST\" exited with exit code: $LASTRES"; elif [ "$FAIL" == "true"  ]; then fail finished with error; else echo -e "[\033[0;32m Finished! Run $@ by serving the generated files in examples/static/ \033[0m]";fi' EXIT
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # this source dir
+
+cd $SRCDIR # ensure this script can be run from anywhere
+
+# When using $CARGO_TARGET_DIR -> binary is located in different folder
+# Necessary to locate build files for wasm-bindgen
+TARGET_DIR=$SRCDIR/../target/wasm32-unknown-unknown/debug
+if [ ! -z "$CARGO_TARGET_DIR" ]; then 
+    TARGET_DIR=$CARGO_TARGET_DIR/wasm32-unknown-unknown/debug
+fi
+
+EXAMPLE=$1
+cd $EXAMPLE
+
+# wasm-pack build
+if [[ $EXAMPLE == *_wp ]]; then 
+    info "Building: $EXAMPLE using wasm-pack"
+    # wasm-pack overwrites .gitignore -> save -> restore
+    cp $SRCDIR/static/.gitignore $SRCDIR/static/.gitignore.copy
+    wasm-pack build --debug --target web --out-name wasm --out-dir $SRCDIR/static/
+    rm $SRCDIR/static/.gitignore; mv $SRCDIR/static/.gitignore.copy $SRCDIR/static/.gitignore # restore .gitignore
+
+# multi_thrread build -> two binary/wasm files
+elif [[ $EXAMPLE == multi_thread ]]; then
+    info "Building: $EXAMPLE app using wasm-bindgen"
+    cargo build --target wasm32-unknown-unknown --bin multi_thread_app
+    wasm-bindgen --target web --no-typescript --out-dir $SRCDIR/static/ --out-name wasm $TARGET_DIR/multi_thread_app.wasm
+
+    info "Building: $EXAMPLE worker using wasm-bindgen"
+    cargo build --target wasm32-unknown-unknown --bin multi_thread_worker
+    wasm-bindgen --target no-modules --no-typescript --out-dir $SRCDIR/static/ --out-name worker $TARGET_DIR/multi_thread_worker.wasm
+
+else # Default wasm-bindgen build
+    info "Building: $EXAMPLE using wasm-bindgen"
+    cargo build --target wasm32-unknown-unknown 
+    wasm-bindgen --target web --no-typescript --out-dir $SRCDIR/static/ --out-name wasm $TARGET_DIR/$EXAMPLE.wasm
+fi
---
+++ examples/readme.md
@@ -0,0 +1,39 @@
+# Yew Examples
+
+Use `build.sh` script to build examples. 
+
+Examples are prepared for `wasm-bindgen` except folders ending with `_wp` which are prepared for `wasm-pack`.
+
+- The examples do not use external bundlers and all use the same `static/index.html` after being built.
+- `wasm-bindgen` builds projects as binary crates (`main.rs`)
+- `wasm-pack` builds projects as library crates (`lib.rs`)
+
+For your own project, you may wish to check out the Yew [starter templates](https://yew.rs/docs/getting-started/starter-templates)
+
+
+## How to run
+
+```sh
+git clone https://github.com/yewstack/yew.git
+cd yew/examples
+./build.sh minimal # example subfolder
+python3 -m http.server --directory static # open localhost:8000 in browser
+```
+
+
+Note: [Visual Studio Code IDE](https://code.visualstudio.com/) has the [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) extension which can be used to run examples in the browser with automatic reload. After installing, open the context menu on `index.html` -> `Open with Live Server`
+
+## Requirements
+
+Default way to build is using `wasm-bindgen` which comes with `wasm-pack`
+
+- Install using cargo: `cargo install wasm-pack`
+
+Install guides: [Rust](https://www.rust-lang.org/learn/get-started) and [wasm-pack](https://rustwasm.github.io/wasm-pack/installer/)
+
+```bash
+# rust install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# wasm-pack install
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh;
+```
