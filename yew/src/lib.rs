40fb97f9d55bbf3630651e4138339021bddd942f
Franck Royer <franck@royer.one>
2020-05-04 08:35:55 +1000

    Re-export web-sys
    This would allow user to use web-sys functions such
    as console.log without having to add the web-sys
    dependency to their Cargo.toml.

--- yew/src/lib.rs
+++ yew/src/lib.rs
@@ -114,6 +114,9 @@ pub mod agent;
 #[cfg(feature = "services")]
 pub mod services;
 
+#[cfg(feature = "web_sys")]
+pub use web_sys;
+
 /// The module that contains all events available in the framework.
 pub mod events {
     use cfg_if::cfg_if;
