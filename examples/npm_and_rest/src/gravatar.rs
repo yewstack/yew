17c60c0f631e4202cb8ac90187d9bb97f3c60ed6
Thomas Lacroix <toto.rigolo@free.fr>
2020-05-14 05:22:58 +0200

    Enable Agents to send messages to themselves. (#1215)
    * Agents can now send messages to themselves
    
    * Agents callbacks now accept Into<Message>
    
    * Clean old comment
    
    * Remove AgentLink's send_message_batch

--- examples/npm_and_rest/src/gravatar.rs
+++ examples/npm_and_rest/src/gravatar.rs
@@ -38,7 +38,6 @@ impl GravatarService {
             if meta.status.is_success() {
                 callback.emit(data)
             } else {
-                // format_err! is a macro in crate `failure`
                 callback.emit(Err(anyhow!(
                     "{}: error getting profile https://gravatar.com/",
                     meta.status
