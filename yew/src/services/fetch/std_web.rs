7bee385960b89dd79025a25bced222269b093b99
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-05-15 11:46:05 +0100

    Updated documentation.

--- yew/src/services/fetch/std_web.rs
+++ yew/src/services/fetch/std_web.rs
@@ -209,7 +209,7 @@ impl FetchService {
     ///# let link: ComponentLink<Comp> = unimplemented!();
     ///# let mut fetch_service: FetchService = FetchService::new();
     ///# let post_request: Request<Result<String, anyhow::Error>> = unimplemented!();
-    /// let task = fetch_service.fetch(
+    /// let task = fetch_service::fetch(
     ///     post_request,
     ///     link.callback(|response: Response<Result<String, anyhow::Error>>| {
     ///         if response.status().is_success() {
@@ -262,7 +262,7 @@ impl FetchService {
     ///     Msg::FetchResourceFailed
     /// });
     ///
-    /// let task = FetchService::new().fetch(get_request, callback);
+    /// let task = FetchService::fetch(get_request, callback);
     ///# }
     /// ```
     ///
@@ -305,7 +305,7 @@ impl FetchService {
     ///     credentials: Some(Credentials::SameOrigin),
     ///     ..FetchOptions::default()
     /// };
-    /// let task = FetchService::new().fetch_with_options(request, options, callback);
+    /// let task = FetchService::fetch_with_options(request, options, callback);
     ///# }
     /// ```
     pub fn fetch_with_options<IN, OUT: 'static>(
@@ -322,7 +322,6 @@ impl FetchService {
 
     /// Fetch the data in binary format.
     pub fn fetch_binary<IN, OUT: 'static>(
-        &mut self,
         request: Request<IN>,
         callback: Callback<Response<OUT>>,
     ) -> Result<FetchTask, &str>
@@ -333,9 +332,8 @@ impl FetchService {
         fetch_impl::<IN, OUT, Vec<u8>, ArrayBuffer>(true, request, None, callback)
     }
 
-    /// Fetch the data in binary format.
+    /// Fetch the data in binary format using the provided request options.
     pub fn fetch_binary_with_options<IN, OUT: 'static>(
-        &mut self,
         request: Request<IN>,
         options: FetchOptions,
         callback: Callback<Response<OUT>>,
