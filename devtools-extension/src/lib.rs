b0c1b0372984c7c2be32579890db4cce19bb020b
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-06-11 07:51:13 +0100

    Add skeleton component.

--- devtools-extension/src/lib.rs
+++ devtools-extension/src/lib.rs
@@ -1,11 +1,65 @@
+#[macro_use]
+extern crate serde;
 use yew::prelude::*;
 
-struct DevToolsExtension {}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ComponentMessage {
+    /// The event which is to be logged.
+    event: ComponentEvent,
+    /// Optional additional data about the event (e.g. the component's location in the DOM).
+    data: Option<DebugComponent>,
+}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DebugComponent {
+    /// The name of the component
+    name: String,
+    selector: Option<String>,
+}
+
+struct DevToolsExtension {
+    ws_task: yew::services::websocket::WebSocketTask,
+    component_tree: indextree::Arena<ComponentMessage>,
+}
+
+enum DevToolsExtensionMsg {
+    ReceiveWSMessage(String),
+    ReceiveWSStatus(yew::services::websocket::WebSocketStatus),
+}
+
+impl Component for DevToolsExtension {
+    type Message = DevToolsExtensionMsg;
+    type Properties = ();
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let ws_task = yew::services::WebSocketService::new()
+            .connect(
+                "ws://localhost:8017/ws",
+                link.callback(|text| DevToolsExtensionMsg::ReceiveWSMessage(text)),
+                link.callback(|status| DevToolsExtensionMsg::ReceiveWSStatus(status)),
+            )
+            .unwrap();
+        DevToolsExtension {
+            ws_task,
+            component_tree: indextree::Arena::new(),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> bool {
+        match msg {
+            DevToolsExtensionMsg::ReceiveWSMessage(message) => {}
+            DevToolsExtensionMsg::ReceiveWSStatus(status) => {}
+        }
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> bool {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <h1>{"Yew DevTools"}</h1>
+        }
     }
 }
