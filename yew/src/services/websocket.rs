9c43b181b3c8a4934479b8f603e7b2999ad1c139
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-06-14 17:35:35 +0100

    Simplify code.

--- yew/src/services/websocket.rs
+++ yew/src/services/websocket.rs
@@ -183,26 +183,21 @@ impl WebSocketService {
     ) -> Result<ConnectCommon, WebSocketError> {
         let ws = WebSocket::new(url);
 
-        let ws = ws.map_err(
-            |#[cfg(feature = "std_web")] _, #[cfg(feature = "web_sys")] ws_error| {
-                #[cfg(feature = "web_sys")]
-                {
-                    WebSocketError::CreationError(
-                        ws_error
-                            .unchecked_into::<js_sys::Error>()
-                            .to_string()
-                            .as_string()
-                            .unwrap(),
-                    )
-                }
-                #[cfg(feature = "std_web")]
-                {
-                    WebSocketError::CreationError(
-                        "Error opening a WebSocket connection.".to_string(),
-                    )
-                }
-            },
-        )?;
+        #[cfg(feature = "std_web")]
+        let ws = ws.map_err(|_| {
+            WebSocketError::CreationError("Error opening a WebSocket connection.".to_string())
+        })?;
+
+        #[cfg(feature = "web_sys")]
+        let ws = ws.map_err(|ws_error| {
+            WebSocketError::CreationError(
+                ws_error
+                    .unchecked_into::<js_sys::Error>()
+                    .to_string()
+                    .as_string()
+                    .unwrap(),
+            )
+        })?;
 
         cfg_match! {
             feature = "std_web" => ws.set_binary_type(SocketBinaryType::ArrayBuffer),
