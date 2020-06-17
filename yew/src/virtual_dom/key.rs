25e374c0a595f06d9c6dfc9b66613040b006f812
Thomas Lacroix <toto.rigolo@free.fr>
2020-06-17 09:06:21 +0200

    Key from finite list of types

--- yew/src/virtual_dom/key.rs
+++ yew/src/virtual_dom/key.rs
@@ -23,8 +23,67 @@ impl core::fmt::Debug for Key {
     }
 }
 
-impl<K: Into<Rc<String>>> From<K> for Key {
-    fn from(key: K) -> Self {
+impl From<Rc<String>> for Key {
+    fn from(key: Rc<String>) -> Self {
         Key { key: key.into() }
+    }
+}
+
+macro_rules! key_impl_from_to_string {
+    ($type:ty) => {
+        impl From<$type> for Key {
+            fn from(key: $type) -> Self {
+                Key { key: Rc::new(key.to_string()) }
+            }
+        }
+    };
+}
+key_impl_from_to_string!(&'static str);
+key_impl_from_to_string!(String);
+key_impl_from_to_string!(char);
+key_impl_from_to_string!(usize);
+key_impl_from_to_string!(u8);
+key_impl_from_to_string!(u16);
+key_impl_from_to_string!(u32);
+key_impl_from_to_string!(u64);
+key_impl_from_to_string!(u128);
+key_impl_from_to_string!(i8);
+key_impl_from_to_string!(i16);
+key_impl_from_to_string!(i32);
+key_impl_from_to_string!(i64);
+key_impl_from_to_string!(i128);
+
+#[cfg(test)]
+mod test {
+    use crate::html;
+    use std::rc::Rc;
+
+    #[cfg(feature = "wasm_test")]
+    use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
+
+    #[cfg(feature = "wasm_test")]
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[test]
+    fn all_key_conversions() {
+        let rc_key = Rc::new("rc".to_string());
+        html! {
+            <key="string literal">
+                <img key="String".to_string() />
+                <p key=rc_key></p>
+                <key='a'>
+                    <p key=11_usize></p>
+                    <p key=12_u8></p>
+                    <p key=13_u16></p>
+                    <p key=14_u32></p>
+                    <p key=15_u64></p>
+                    <p key=15_u128></p>
+                    <p key=22_i8></p>
+                    <p key=23_i16></p>
+                    <p key=24_i32></p>
+                    <p key=25_i128></p>
+                </>
+            </>
+        };
     }
 }
