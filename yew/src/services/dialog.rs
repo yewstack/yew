fb5e394677e30b44ad0a55aa0939e937b341d2a6
Teymour Aldridge <42674621+teymour-aldridge@users.noreply.github.com>
2020-07-04 16:08:10 +0100

    Add `prompt` function to the dialogue service. (#1350)
    * Add `prompt` function to the dialogue service.
    * Improve the clarity of some doc comments
    * Fix a typo in a `map_err`
    
    * Fix `cfg_match!` usage.
    
    * Add `stdweb` support.
    
    * Fix the `prompt` method.
    
    * Add different docs for `stdweb` and `web_sys`.

--- yew/src/services/dialog.rs
+++ yew/src/services/dialog.rs
@@ -1,5 +1,8 @@
 //! This module contains the implementation of a service
 //! to show alerts and confirm dialogs in a browser.
+//!
+//! If you call these methods repeatably browsers tend to disable these options to give users
+//! a better experience.
 
 use cfg_if::cfg_if;
 use cfg_match::cfg_match;
@@ -39,6 +42,59 @@ impl DialogService {
                 }
             }),
             feature = "web_sys" => utils::window().confirm_with_message(message).unwrap(),
+        }
+    }
+
+    /// Prompts the user to input a message. In most browsers this will open an alert box with
+    /// an input field where the user can input a message.
+    #[cfg_attr(
+        feature = "web_sys",
+        doc = "A default value can be supplied which will be returned if the user doesn't input anything."
+    )]
+    ///
+    /// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt)
+    ///
+    /// This method will `panic!` if there is an error in the process of trying to carry out this
+    /// operation.
+    ///
+    /// Note that this function is blocking; no other code can be run on the thread while
+    /// the user inputs their message which means that the page will appear to have 'frozen'
+    /// while the user types in their message.
+    ///
+    #[cfg_attr(
+        feature = "web_sys",
+        doc = "This function will return `None` if the value of `default` is `None` and the user \
+        cancels the operation. (normally a 'cancel' button will be displayed to the user, \
+        clicking which cancels the operation)."
+    )]
+    #[cfg_attr(
+        feature = "std_web",
+        doc = "This function will return `None` if the user cancels the operation (normally a \
+        'cancel' button will be displayed to the user, clicking which cancels the operation)."
+    )]
+    pub fn prompt(
+        message: &str,
+        #[cfg(feature = "web_sys")] default: Option<&str>,
+    ) -> Option<String> {
+        cfg_if! {
+            if #[cfg(feature="web_sys")] {
+                if let Some(default) = default {
+                    utils::window()
+                           .prompt_with_message_and_default(message, default)
+                           .expect("Couldn't read input.")
+                }
+                else {
+                    utils::window()
+                           .prompt_with_message(message)
+                           .expect("Couldn't read input.")
+                }
+            } else if #[cfg(feature="std_web")] {
+                let value: Value = js! { return prompt(@{message}); };
+                match value {
+                    Value::String(result) => Some(result),
+                    _ => None,
+                }
+            }
         }
     }
 }
