d784eca2e0227e40fb535fa18d058775bdd38e5b
Philip Peterson <philip.c.peterson@gmail.com>
2020-07-04 13:22:59 -0700

    add test to run-tests.sh

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -23,3 +23,6 @@ set -euxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_
 (cd yewtil && cargo test)
 
 (cd yew-components && cargo test)
+
+(cd yew-validation \
+  && cargo test --target wasm32-unknown-unknown --features wasm_test)
