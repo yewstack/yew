14e213a91f4fe42d0dc7e2b617dad224fd19c3c4
Philip Peterson <philip.c.peterson@gmail.com>
2020-07-18 22:01:06 -0700

    use clap

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,70 +1,93 @@
-use std::collections::{VecDeque, HashMap};
-use maplit::hashmap;
-use std::{env, fs};
-use std::path::{Path, PathBuf};
-use rayon::prelude::IntoParallelIterator;
-use std::process::Command;
+use clap::{App, AppSettings, Arg, ArgGroup, ArgMatches, SubCommand};
 use lazy_static::lazy_static;
+use maplit::hashmap;
+use rayon::prelude::IntoParallelIterator;
+use std::collections::{HashMap, VecDeque};
+use std::path::{Path, PathBuf};
+use std::process::{exit, Command};
 use std::sync::Mutex;
+use std::{env, fs};
+
+use log::{info, warn, error};
 
 lazy_static! {
     static ref RELEASE: Mutex<bool> = Mutex::new(false);
 }
 
+// Usages:
+//  yew run directory/
+//  yew build --run directory/ (same as above)
+//  yew build directory/ (only builds)
+//  yew build examples/* (to build all examples) 
+
 fn main() {
-    let vars = env::args().collect::<Vec<String>>();
-    let (flags, vars): (Vec<&str>, Vec<&str>) = vars.iter().map(|s| s.as_str()).partition(|s| {
-        let chars = s.chars().collect::<Vec<char>>();
-        (chars.len() > 1 && chars[0] == '-')
-            ||
-            (chars.len() > 2 && chars[0] == '-' && chars[1] == '-')
-    });
-    let mut vars = vars.into_iter().collect::<VecDeque<&str>>();
-    {
-        let mut r = RELEASE.lock().unwrap();
-        *r = flags.contains(&"--release");
+    let matches = App::new("Yew CLI")
+        .version("0.1")
+        .about("Builds and runs Yew application projects")
+        .setting(AppSettings::SubcommandRequired)
+        .subcommand(
+            SubCommand::with_name("build")
+                .about("compiles a Yew application")
+                .arg(
+                    Arg::with_name("run")
+                        .help("Start a webserver for the built project and open it in a browser window")
+                        .long("run")
+                        .required(true)
+                        .short("r")
+                )
+                .arg(
+                    Arg::with_name("release")
+                        .help("Whether to invoke `cargo build` using the --release flag")
+                        .long("release")
+                )
+                .arg(
+                    Arg::with_name("PROJECT_DIR")
+                        .help("Path to the project directory for the Yew application that will be built")
+                        .required(true)
+                )
+        )
+        .get_matches();
+    
+    let subcommand = matches.subcommand_name().unwrap();
+    match subcommand {
+        "run" => cmd_run(matches),
+        "build" => cmd_build(matches),
+        _ => panic!("unknown subcommand")
     }
-    vars.pop_front();
-    println!("{:?}", vars);
-    match vars.pop_front() {
-        Some("examples") => {
-            enum BuildType {
-                Build,
-                Run,
-            }
-            let build_type = match vars.pop_front() {
-                Some("build") => BuildType::Build,
-                Some("run") => BuildType::Run,
-                None => panic!("you need to specify the build type"),
-                _ => panic!("not a valid build type"),
-                //TODO make these error messages better
-            };
-            let examples_path = cwd().join("examples");
-            match vars.pop_front() {
-                None => {
-                    fs::read_dir(examples_path.as_path())
-                        .expect("failed to read dir examples dir")
-                        .into_iter()
-                        .map(|dir|
-                            dir.expect("failed to read individual example directory")
-                                .path()
-                        )
-                        .filter(|dir|
-                            vec!["static", "server", "target"].contains(&dir.as_path().file_name().unwrap().to_str().unwrap())
-                        )
-                        .for_each(|dir| {
-                            build_example(dir.as_path());
-                        });
-                }
-                Some(example) => {
-                    build_example(examples_path.join(example).as_path());
-                }
-            };
-        }
-        _ => {
-            println!("not a valid command line argument")
-        }
-    };
+}
+
+fn cmd_run(matches: ArgMatches) {
+
+}
+
+enum BuildType {
+    Build,
+    Run,
+}
+
+// build all examples
+// fs::read_dir(examples_path.as_path())
+//     .expect("failed to read dir examples dir")
+//     .into_iter()
+//     .map(|dir| {
+//         dir.expect("failed to read individual example directory")
+//             .path()
+//     })
+//     .filter(|dir| {
+//         vec!["static", "server", "target"]
+//             .contains(&dir.as_path().file_name().unwrap().to_str().unwrap())
+//     })
+//     .for_each(|dir| {
+//         build_example(dir.as_path());
+//     });
+
+fn cmd_build(matches: ArgMatches) {
+    let has_run_flag = matches.is_present("run");
+
+    let examples_path = cwd().join("examples");
+
+    // build single example
+    build_example(examples_path.as_path());
 }
 
 fn cwd() -> PathBuf {
@@ -76,7 +99,9 @@ fn build_example(path: &Path) {
         cwd().join("target").join("wasm32-unknown-unknown")
     }
     let file_name = path.file_name().unwrap().to_str().unwrap();
-    if file_name.ends_with("_wp") {} else if file_name == "multi_thread" {} else {
+    if file_name.ends_with("_wp") {
+    } else if file_name == "multi_thread" {
+    } else {
         let mut args = vec!["build"];
         if *RELEASE.lock().unwrap() {
             args.push("--release")
@@ -87,13 +112,30 @@ fn build_example(path: &Path) {
             .args(&args[0..])
             .output()
             .expect("failed to execute cargo build process");
-        println!("{}", String::from_utf8(output.stdout).expect("failed to pass stdout from cargo build"));
+        println!(
+            "{}",
+            String::from_utf8(output.stdout).expect("failed to pass stdout from cargo build")
+        );
         let output = Command::new("wasm-bindgen")
             .current_dir(path)
-            .args(&["--target", "web", "--no-typescript", "--out-dir", "static/", "--out-name", "wasm",
-                target_dir().join(format!("{}.wasm", file_name)).to_str().unwrap()])
+            .args(&[
+                "--target",
+                "web",
+                "--no-typescript",
+                "--out-dir",
+                "static/",
+                "--out-name",
+                "wasm",
+                target_dir()
+                    .join(format!("{}.wasm", file_name))
+                    .to_str()
+                    .unwrap(),
+            ])
             .output()
             .expect("failed to execute wasm-bindgen process");
-        println!("{}", String::from_utf8(output.stdout).expect("failed to pass stdout from cargo build"));
+        println!(
+            "{}",
+            String::from_utf8(output.stdout).expect("failed to pass stdout from cargo build")
+        );
     }
 }
