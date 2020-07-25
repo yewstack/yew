c56a87986b662f866fb6ec059cb8ffdbd753299e
Philip Peterson <philip.c.peterson@gmail.com>
2020-07-25 00:44:09 -0700

    error handling

---
+++ yew-cli/src/error.rs
@@ -0,0 +1,47 @@
+use exitcode;
+use thiserror::Error;
+
+use std::convert::From;
+
+#[derive(Debug, Error)]
+pub enum RunError {
+    #[error("cannot run multiple projects at once")]
+    MultipleProjects,
+}
+
+impl From<RunError> for i32 {
+    fn from(error: RunError) -> i32 {
+        match error {
+            RunError::MultipleProjects => exitcode::USAGE
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum BuildError {
+    #[error("{0} doesn't have a Cargo.toml file")]
+    NoCargoToml(String),
+}
+
+impl From<BuildError> for i32 {
+    fn from(error: BuildError) -> i32 {
+        match error {
+            BuildError::NoCargoToml(_) => exitcode::NOINPUT
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum SubcommandError {
+    #[error("{0}")]
+    RunError(RunError),
+
+    #[error("{0}")]
+    BuildError(BuildError),
+}
+
+impl From<SubcommandError> for i32 {
+    fn from(error: SubcommandError) -> i32 {
+        error.into()
+    }
+}
