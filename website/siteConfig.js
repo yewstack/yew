9229ea4f336b845b8d71b2e8441917a7ee993b23
Justin Starry <justin.starry@icloud.com>
2020-08-17 18:42:13 +0800

    Setup versioned docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -18,7 +18,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
+  projectName: 'yew',
   organizationName: 'yewstack',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
