aa6406df2c440c1fcb797aa01bb2bdb354ca4181
Philip Peterson <philip.c.peterson@gmail.com>
2020-08-23 23:18:45 -0700

    auto-type the config

--- yew-config/src/lib.rs
+++ yew-config/src/lib.rs
@@ -1,12 +1,13 @@
 use paste::paste;
-use tt_call::{tt_call, tt_replace, tt_return};
 use quote::quote;
+use std::convert::TryFrom;
+use tt_call::{tt_call, tt_replace, tt_return};
 
-pub struct YewConfigNonspecific {
+pub struct YewConfig {
     pub version: String,
 
     // this actually points to a YewConfigBody, but we need to manually cast based on
-    // the version string.
+    // the version string. This can be done automatically with to_versioned_config().
     pub body: Box<()>,
 }
 
@@ -34,50 +35,87 @@ macro_rules! is_body_placeholder {
 
 macro_rules! declare_yew_config_version {
     (
-        #[version($maj:expr, $min:expr, $pat:expr)]
-        struct {
-            $($struct:tt)*
-        }
-        fn new( $( $new_arg:ident : $new_ty:ty ),* ) {
-            $($new_impl:tt)*
-        }
+        $(
+            #[version($maj:expr, $min:expr, $pat:expr)]
+            {
+                struct {
+                    $($struct:tt)*
+                }
+                fn new( $( $new_arg:ident : $new_ty:ty ),* ) {
+                    $($new_impl:tt)*
+                }
+            }
+        )*
     ) => {
+        $(
+            paste! {
+                pub struct [<YewConfigBody $maj _ $min _ $pat>] {
+                    $($struct)*
+                }
+
+                pub struct [<YewConfig $maj _ $min _ $pat>] {
+                    pub version: String,
+                    pub body: Box<[<YewConfigBody $maj _ $min _ $pat>]>
+                }
+
+                impl [<YewConfigBody $maj _ $min _ $pat>] {
+                    fn new(
+                        $( $new_arg: $new_ty ),*
+                    ) -> [<YewConfigBody $maj _ $min _ $pat>] {
+                        tt_call! {
+                            macro = [{ tt_replace }]
+                            condition = [{ is_body_placeholder }]
+                            replace_with = [{ [<YewConfigBody $maj _ $min _ $pat>] }]
+                            input = [{ $($new_impl)* }]
+                        }
+                    }
+                }
+
+                impl [<YewConfig $maj _ $min _ $pat>] {
+                    pub fn new(
+                        $( $new_arg: $new_ty ),*
+                    ) -> [<YewConfig $maj _ $min _ $pat>] {
+                        let body = [<YewConfigBody $maj _ $min _ $pat>]::new(
+                            $( $new_arg ),*
+                        );
+
+                        [<YewConfig $maj _ $min _ $pat>] {
+                            version: format!("{}.{}.{}", $maj, $min, $pat),
+                            body: Box::new(body),
+                        }
+                    }
+                }
+            }
+        )*
+
         paste! {
-            pub struct [<YewConfigBody $maj _ $min _ $pat>] {
-                $($struct)*
+            pub enum YewVersionedConfig {
+                $(
+                    [<Version $maj _ $min _ $pat>]([<YewConfig $maj _ $min _ $pat>])
+                ),*
             }
+        }
 
-            pub struct [<YewConfig $maj _ $min _ $pat>] {
-                pub version: String,
-                pub body: Box<[<YewConfigBody $maj _ $min _ $pat>]>
-            }
+        impl TryFrom<YewConfig> for YewVersionedConfig {
+            type Error = &'static str;
 
-            impl [<YewConfigBody $maj _ $min _ $pat>] {
-                fn new(
-                    $( $new_arg: $new_ty ),*
-                ) -> [<YewConfigBody $maj _ $min _ $pat>] {
-                    tt_call! {
-                        macro = [{ tt_replace }]
-                        condition = [{ is_body_placeholder }]
-                        replace_with = [{ [<YewConfigBody $maj _ $min _ $pat>] }]
-                        input = [{ $($new_impl)* }]
+            fn try_from(config: YewConfig) -> Result<YewVersionedConfig, Self::Error> {
+                $(
+                    if (config.version == format!("{}.{}.{}", $maj, $min, $pat)) {
+                        return Ok(
+                            paste! {
+                                YewVersionedConfig::[<Version $maj _ $min _ $pat>](
+                                    unsafe {
+                                        std::mem::transmute::<
+                                            YewConfig, [<YewConfig $maj _ $min _ $pat>]
+                                        >(config)
+                                    }
+                                )
+                            }
+                        )
                     }
-                }
-            }
-
-            impl [<YewConfig $maj _ $min _ $pat>] {
-                pub fn new(
-                    $( $new_arg: $new_ty ),*
-                ) -> [<YewConfig $maj _ $min _ $pat>] {
-                    let body = [<YewConfigBody $maj _ $min _ $pat>]::new(
-                        $( $new_arg ),*
-                    );
-
-                    [<YewConfig $maj _ $min _ $pat>] {
-                        version: format!("{}.{}.{}", quote!($maj), quote!($min), quote!($pat)).to_string(),
-                        body: Box::new(body),
-                    }
-                }
+                )*
+                Err("Unknown schema of loaded Yew project configuration. The `yew-config` crate is most likely out of date.")?
             }
         }
     }
@@ -85,13 +123,28 @@ macro_rules! declare_yew_config_version {
 
 declare_yew_config_version!(
     #[version(0,0,2)]
-    struct {
-        pub app_name: String,
+    {
+        struct {
+            pub app_name: String,
+        }
+
+        fn new(app_name: String) {
+            ConfigBody {
+                app_name,
+            }
+        }
     }
 
-    fn new(app_name: String) {
-        ConfigBody {
-            app_name,
+    #[version(0,0,3)]
+    {
+        struct {
+            pub app_name: String,
+        }
+
+        fn new(app_name: String) {
+            ConfigBody {
+                app_name,
+            }
         }
     }
 );
