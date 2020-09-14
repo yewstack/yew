6f061811f570f46a6696b831419c41709153504f
Simon Berger <simon@siku2.io>
2020-09-14 16:26:52 +0200

    update counter

--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -1,3 +1,71 @@
+use js_sys::Date;
+use yew::services::ConsoleService;
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+
+pub enum Msg {
+    Increment,
+    Decrement,
+}
+
+pub struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Model { link, value: 0 }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Increment => {
+                self.value += 1;
+                ConsoleService::log("plus one");
+                true
+            }
+            Msg::Decrement => {
+                self.value -= 1;
+                ConsoleService::log("minus one");
+                true
+            }
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <nav class="menu">
+                    <button onclick=self.link.callback(|_| Msg::Increment)>
+                        { "Increment" }
+                    </button>
+                    <button onclick=self.link.callback(|_| Msg::Decrement)>
+                        { "Decrement" }
+                    </button>
+                    <button onclick=self.link.batch_callback(|_| vec![Msg::Increment, Msg::Increment])>
+                        { "Increment Twice" }
+                    </button>
+                </nav>
+                <p>
+                    <b>{ "Current value: " }</b>
+                    { self.value }
+                </p>
+                <p>
+                    <b>{ "Rendered at: " }</b>
+                    { String::from(Date::new_0().to_string()) }
+                </p>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    yew::start_app::<counter::Model>();
+    yew::start_app::<Model>();
 }
