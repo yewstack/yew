59421b4d4c7e99484e5786dc163b0a5c83f6f9d1
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-09-27 14:51:28 +0200

    Fix test that was not running

--- yew-macro/tests/derive_props_test.rs
+++ yew-macro/tests/derive_props_test.rs
@@ -1,5 +1,5 @@
 #[allow(dead_code)]
-#[rustversion::attr(stable(1.45), test)]
+#[rustversion::attr(since(1.45), test)]
 fn tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/derive_props/pass.rs");
