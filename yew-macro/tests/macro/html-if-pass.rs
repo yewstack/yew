ac4a5b85c88c7de34cf7942570c380a05f4b8d8e
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-10-10 12:32:49 +0200

    CLEANUP
    Forked at: 66d506e13329a06f7dce0b55a9427972b8aad3ff
    Parent branch: origin/master

--- yew-macro/tests/macro/html-if-pass.rs
+++ yew-macro/tests/macro/html-if-pass.rs
@@ -4,16 +4,12 @@ fn compile_pass() {
     let boolean = true;
     html! { if boolean { html!() } };
     html! { if boolean { html!() } else { html!() } };
-
-    let option = Some("text");
-    html! { if let Some(text) = option { html!( {text} ) } };
-    html! { if let Some(text) = option { html!( {text} ) } else { html!() } };
-
-    let boolean = true;
     html! { <div>{if boolean { html!() }}</div> };
     html! { <div>{if boolean { html!() } else { html!() }}</div> };
 
     let option = Some("text");
+    html! { if let Some(text) = option { html!( {text} ) } };
+    html! { if let Some(text) = option { html!( {text} ) } else { html!() } };
     html! { <div>{if let Some(text) = option { html!( {text} ) }}</div> };
     html! { <div>{if let Some(text) = option { html!( {text} ) } else { html!() }}</div> };
 }
