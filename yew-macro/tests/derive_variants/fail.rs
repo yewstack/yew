04ce62b3374d047c2793a72a1f8f30c37b1d97d3
Jack Johannesen <jack@insertdomain.name>
2020-10-12 13:51:33 -0400

    Add another test

--- yew-macro/tests/derive_variants/fail.rs
+++ yew-macro/tests/derive_variants/fail.rs
@@ -21,7 +21,24 @@ mod t2 {
 
     #[derive(Clone, Variants)]
     pub enum MyVariants {
+        // ERROR: only unnamed fields are supported
         MyFirstComponent { x: MyFirstComponentProps, }
+    }
+}
+
+mod t3 {
+    use super::*;
+
+    #[derive(Clone, Properties)]
+    pub struct MyFirstComponentProps {
+        #[prop_or_default]
+        pub foo: String,
+    }
+
+    #[derive(Clone, Variants)]
+    pub enum MyVariants {
+        // ERROR: only fields with a single field are supported
+        MyFirstComponent(MyFirstComponentProps, MyFirstComponentProps),
     }
 }
 
