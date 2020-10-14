54d6ba15c664efccbcbe747d92eb0bff318b8289
Jay Graves <jay@skabber.com>
2020-10-13 20:38:15 -0600

    Add a ticking clock to the timer example.

--- examples/timer/src/main.rs
+++ examples/timer/src/main.rs
@@ -1,19 +1,24 @@
+use js_sys;
 use std::time::Duration;
 use yew::services::interval::{IntervalService, IntervalTask};
 use yew::services::{ConsoleService, Task, TimeoutService};
 use yew::{html, Callback, Component, ComponentLink, Html, ShouldRender};
 
 pub enum Msg {
+    StartClock,
     StartTimeout,
     StartInterval,
     Cancel,
     Done,
     Tick,
+    Tock,
 }
 
 pub struct Model {
     link: ComponentLink<Self>,
     job: Option<Box<dyn Task>>,
+    clock_job: Option<Box<dyn Task>>,
+    time: String,
     messages: Vec<&'static str>,
     _standalone: IntervalTask,
 }
@@ -23,6 +28,7 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        link.send_message(Msg::StartClock);
         let handle = IntervalService::spawn(
             Duration::from_secs(10),
             // This callback doesn't send any message to a scope
@@ -34,6 +40,8 @@ impl Component for Model {
         Self {
             link,
             job: None,
+            clock_job: None,
+            time: "".to_string(),
             messages: Vec::new(),
             _standalone: handle,
         }
@@ -41,6 +49,16 @@ impl Component for Model {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
+            Msg::StartClock => {
+                self.time = String::from(js_sys::Date::new_0().to_time_string());
+                let handle = IntervalService::spawn(
+                    Duration::from_secs(1),
+                    self.link.callback(|_| Msg::Tock),
+                );
+                self.clock_job = Some(Box::new(handle));
+
+                true
+            }
             Msg::StartTimeout => {
                 let handle = TimeoutService::spawn(
                     Duration::from_secs(3),
@@ -89,6 +107,10 @@ impl Component for Model {
                 ConsoleService::count_named("Tick");
                 true
             }
+            Msg::Tock => {
+                self.time = String::from(js_sys::Date::new_0().to_time_string());
+                true
+            }
         }
     }
 
@@ -109,6 +131,9 @@ impl Component for Model {
                 <button disabled=!has_job onclick=self.link.callback(|_| Msg::Cancel)>
                     { "Cancel!" }
                 </button>
+                <div class="has-text-centered is-size-1 has-text-weight-bold">
+                { &self.time }
+                </div>
                 <div>
                     { for self.messages.iter().map(|message| html! { <p>{ message }</p> }) }
                 </div>
