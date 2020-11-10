04a79a131f23321be5cf021b30e04c20dbc3359a
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-11-10 16:22:37 +0100

    Update yew/src/html/classes.rs
    Co-authored-by: Simon <simon@siku2.io>

--- yew/src/html/classes.rs
+++ yew/src/html/classes.rs
@@ -39,7 +39,7 @@ impl Classes {
     /// If the provided class has already been added, this method will ignore it.
     ///
     /// This method won't check if there are multiple classes in the input string.
-    pub fn unchecked_push<T: Into<Cow<'static, str>>>(&mut self, class: T) {
+    pub unsafe fn unchecked_push<T: Into<Cow<'static, str>>>(&mut self, class: T) {
         self.set.insert(class.into());
     }
 
