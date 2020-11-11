08e0d07a979860769591aff87b54cd2f80094e4a
Hamza <muhammadhamza1311@gmail.com>
2020-11-11 12:02:35 +0500

    add examples in docstrings

--- yew-functional/src/lib.rs
+++ yew-functional/src/lib.rs
@@ -178,6 +178,43 @@ where
 ///
 /// It is important to note that you do not get notified of state changes.
 /// If you need the component to be re-rendered on state change, consider using [`use_state`].
+///
+/// # Example
+///
+/// ```rust
+/// # use yew_functional::{function_component, use_state, use_ref};
+/// # use yew::prelude::*;
+/// # use std::rc::Rc;
+/// # use std::cell::RefCell;
+/// # use std::ops::{Deref, DerefMut};
+///
+/// #[function_component(UseRef)]
+/// fn ref_hook() -> Html {
+///     let (outer_html, set_outer_html) = use_state(|| "".to_string());
+///
+///     let node_ref: Rc<RefCell<NodeRef>> = use_ref(|| NodeRef::default());
+///
+///     let on_click = {
+///         let node_ref = Rc::clone(&node_ref);
+///
+///         Callback::from(move |e| {
+///             let to_set = (*node_ref.borrow().deref())
+///                 .cast::<yew::web_sys::Element>()
+///                 .unwrap()
+///                 .outer_html();
+///             set_outer_html(to_set)
+///         })
+///     };
+///     html! {
+///         <>
+///             <div id="result" ref=(*node_ref.borrow_mut().deref_mut()).clone()>{"Filler"}</div>
+///             {outer_html}
+///             <br />
+///             <button onclick=on_click>{"Refresh"}</button>
+///         </>
+///     }
+/// }
+/// ```
 pub fn use_ref<T: 'static, InitialProvider>(initial_value: InitialProvider) -> Rc<RefCell<T>>
 where
     InitialProvider: FnOnce() -> T,
@@ -204,6 +241,61 @@ where
 /// and the component is re-rendered.
 ///
 /// For lazy initialization, consider using [`use_reducer_with_init`] instead.
+///
+/// # Example
+///
+/// ```rust
+/// # use yew_functional::{function_component, use_reducer};
+/// # use yew::prelude::*;
+/// # use std::rc::Rc;
+/// # use std::ops::DerefMut;
+///
+/// #[function_component(UseReducer)]
+/// fn reducer() -> Html {
+///     //// reducer's Action
+///     enum Action {
+///         Double,
+///         Square,
+///     }
+///
+///     //// reducer's State
+///     struct CounterState {
+///         counter: i32,
+///     }
+///
+///     let (
+///         counter, // the state
+///         // function to update the state
+///         // as the same suggests, it dispatches the values to the reducer function
+///         dispatch
+///     ) = use_reducer(
+///         // the reducer function
+///         |prev: Rc<CounterState>, action: Action| CounterState {
+///             counter: match action {
+///                 Action::Double => prev.counter * 2,
+///                 Action::Square => prev.counter * prev.counter,
+///             }
+///         },
+///         // initial state
+///         CounterState { counter: 1 },
+///     );
+///
+///     let double_onclick = {
+///         let dispatch = Rc::clone(&dispatch);
+///         Callback::from(move |_| dispatch(Action::Double))
+///     };
+///     let square_onclick = Callback::from(move |_| dispatch(Action::Square));
+///
+///     html! {
+///         <>
+///             <div id="result">{counter.counter}</div>
+///
+///             <button onclick=double_onclick>{"Double"}</button>
+///             <button onclick=square_onclick>{"Square"}</button>
+///         </>
+///     }
+/// }
+/// ```
 pub fn use_reducer<Action: 'static, Reducer, State: 'static>(
     reducer: Reducer,
     initial_state: State,
@@ -220,6 +312,37 @@ where
 ///
 /// This is useful for lazy initialization where it is beneficial not to perform expensive
 /// computation up-front
+///
+/// # Example
+/// ```rust
+/// # use yew_functional::{function_component, use_reducer_with_init};
+/// # use yew::prelude::*;
+/// # use std::rc::Rc;
+///
+/// #[function_component(UseReducerWithInit)]
+/// fn reducer_with_init() -> Html {
+///     struct CounterState {
+///         counter: i32,
+///     }
+///     let (counter, dispatch) = use_reducer_with_init(
+///         |prev: Rc<CounterState>, action: i32| CounterState {
+///             counter: prev.counter + action,
+///         },
+///         0,
+///         |initial: i32| CounterState {
+///             counter: initial + 10,
+///         },
+///     );
+///
+///     html! {
+///         <>
+///             <div id="result">{counter.counter}</div>
+///
+///             <button onclick=Callback::from(move |_| dispatch(10))>{"Increment by 10"}</button>
+///         </>
+///     }
+/// }
+/// ```
 pub fn use_reducer_with_init<Action: 'static, Reducer, State: 'static, InitialState, InitFn>(
     reducer: Reducer,
     initial_state: InitialState,
@@ -267,6 +390,36 @@ where
 /// This value remains up-to-date on subsequent renders.
 ///
 /// The setter function is used to update the value and trigger a re-render.
+///
+/// # Example
+///
+/// ```rust
+/// # use yew_functional::{function_component, use_state, use_ref};
+/// # use yew::prelude::*;
+/// # use std::rc::Rc;
+///
+/// #[function_component(UseState)]
+/// fn state() -> Html {
+///     let (
+///         counter, // the returned state
+///         set_counter // setter to update the state
+///     ) = use_state(|| 0);
+///     let onclick = {
+///         let counter = Rc::clone(&counter);
+///         Callback::from(move |_| set_counter(*counter + 1))
+///     };
+///
+///     html! {
+///         <div>
+///             <button onclick=onclick>{ "Increment value" }</button>
+///             <p>
+///                 <b>{ "Current value: " }</b>
+///                 { counter }
+///             </p>
+///         </div>
+///     }
+/// }
+/// ```
 pub fn use_state<T, F>(initial_state_fn: F) -> (Rc<T>, Rc<impl Fn(T)>)
 where
     F: FnOnce() -> T,
@@ -305,6 +458,37 @@ where
 /// The said function returns another function, the destructor function,
 /// which called when the component is destroyed. It can be used to clean up the effects introduced.
 /// This is similar to `destroyed` method of [`Component`] trait.
+///
+/// # Example
+///
+/// ```rust
+/// # use yew_functional::{function_component, use_effect, use_state};
+/// # use yew::prelude::*;
+/// # use std::rc::Rc;
+///
+/// #[function_component(UseEffect)]
+/// fn effect() -> Html {
+///     let (counter, set_counter) = use_state(|| 0);
+///
+///     let counter_one = counter.clone();
+///     use_effect(move || {
+///         /// Make a call to DOM API after component is rendered
+///         yew::utils::document().set_title(&format!("You clicked {} times", counter_one));
+///
+///         /// Perform the cleanup
+///         || yew::utils::document().set_title(&format!("You clicked 0 times"))
+///     });
+///
+///     let onclick = {
+///         let counter = Rc::clone(&counter);
+///         Callback::from(move |_| set_counter(*counter + 1))
+///     };
+///
+///     return html! {<>
+///         <button onclick=onclick>{ format!("Increment to {}", counter) }</button>
+///     </>};
+/// }
+/// ```
 pub fn use_effect<F, Destructor>(callback: F)
 where
     F: FnOnce() -> Destructor + 'static,
@@ -341,6 +525,28 @@ where
     );
 }
 
+/// Sometimes, it's needed to manually define dependencies for [`use_effect`].
+/// In such cases, we use `use_effect_with_deps`.
+///
+/// # Example
+///
+/// ```rust
+/// use yew_functional::{function_component, use_effect_with_deps};
+/// use yew::prelude::*;
+///
+/// # #[function_component(UseEffectWithDeps)]
+/// # fn effect_with_deps() -> Html {
+///     use_effect_with_deps(
+///         move |_| {
+///             // ...
+///             || {}
+///         },
+///         (), // dependents
+///     );
+///
+/// #    html! {}
+/// # }
+/// ```
 pub fn use_effect_with_deps<F, Destructor, Dependents>(callback: F, deps: Dependents)
 where
     F: FnOnce(&Dependents) -> Destructor + 'static,
