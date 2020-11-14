5b02d2b195f3d46de6894ac79e7736469bb15094
Hamza <muhammadhamza1311@gmail.com>
2020-11-14 16:01:11 +0500

    slim down hooks docstrings and module docs

--- yew-functional/src/lib.rs
+++ yew-functional/src/lib.rs
@@ -1,8 +1,6 @@
 //! Function components are a simplified version of normal components.
-//! They consist of a single function that receives props and determines what should be rendered by returning [`Html`].
-//! Basically, it's a component that's been reduced to just the [`Component::view`] method.
-//! On its own that would be quite limiting because you can only create pure components, but that's where Hooks come in.
-//! Hooks allow function components to use state and other Yew features without implementing the [`Component`] trait.
+//! They consist of a single function annotated with the attribute `#[function_component(_)]`
+//! that receives props and determines what should be rendered by returning [`Html`].
 //!
 //! ```rust
 //! # use yew_functional::function_component;
@@ -14,52 +12,7 @@
 //! }
 //! ```
 //!
-//! # Defining function components
-//!
-//! The simplest way to define function components is use the [`function_component`] attribute.
-//! It is also possible to define them manually by implementing [`FunctionProvider`] trait and
-//! using [`FunctionComponent`] struct. [`function_component`] provides an abstraction over
-//! implementing [`FunctionProvider`] and exposing [`FunctionComponent`] type.
-//!
-//! # Hooks
-//!
-//! Hooks are simply functions that let you “hook into” components' state and/or lifecycle and perform actions. Yew comes with a few pre-defined Hooks.
-//! You can also create your own by using [`use_hook`] function.
-//!
-//! A full guide for consuming Hooks and define custom ones can be found on [Yew's website](https://yew.rs/)
-//!
-//! ## Why do Hooks return an [`Rc`]?
-//!
-//! In most cases, you'll be cloning the values returned from the Hooks.
-//! As it may be expensive to clone such values, they're `Rc`ed, so they can be cloned relatively cheaply.
-//! An example of such a situation would be when you have to clone a a large HashMap or Vector.
-//! It will be much cheaper to clone an `Rc` than to clone such a type.
-//!
-//! It is worth noting that this problem doesn't occur with premitives and with types that implment [`Copy`]
-//!
-//! Following example shows one of the most common cases which requires cloning the values:
-//!
-//! ```rust
-//! # use yew_functional::{function_component, use_state};
-//! # use yew::prelude::*;
-//! # use std::rc::Rc;
-//! #
-//! fn component() -> Html {
-//!     let (data, set_data) = use_state(|| "".to_string());
-//!     let onclick = {
-//!         let data = Rc::clone(&data);
-//!         // Values must be moved into this closure so in order to be able to use them later on, they must be cloned
-//!         Callback::from(move |_| set_data(format!("{}MoreData", data)))
-//!     };
-//!
-//!     html! { <>
-//!         // If `data` wasn't cloned above, it would've been impossible to use it here
-//!         { &data }
-//!         <button onclick=onclick>{"Show data"}</button>
-//!     </>}
-//! }
-//! ```
-//!
+//! More details about function components and Hooks can be found on [Yew's website](https://yew.rs/docs/en/next/concepts/function-components)
 
 use std::borrow::Borrow;
 use std::cell::RefCell;
@@ -298,11 +251,6 @@ where
 /// `use_reducer` is an alternative to [`use_state`]. It is used to handle component's state and is used
 /// when complex actions needs to be performed on said state.
 ///
-/// It accepts a reducer function and initial state and returns [`Rc`] of the state, and a dispatch function.
-/// The dispatch function takes one argument of `Action`. When called, the action and current value
-/// are passed to the reducer function which computes a new state which is returned,
-/// and the component is re-rendered.
-///
 /// For lazy initialization, consider using [`use_reducer_with_init`] instead.
 ///
 /// # Example
@@ -369,9 +317,7 @@ where
     use_reducer_with_init(reducer, initial_state, |a| a)
 }
 
-/// [`use_reducer`] but with init argument. The Hook is passed the initial state
-/// which is then passed down to `init` function which initializes the state and returns it.
-/// The hook then returns this state.
+/// [`use_reducer`] but with init argument.
 ///
 /// This is useful for lazy initialization where it is beneficial not to perform expensive
 /// computation up-front
@@ -447,12 +393,6 @@ where
 }
 
 /// `use_state` is used to mange state in a function component.
-/// It returns a `Rc` of the stateful value, and a setter function.
-///
-/// Initially, the state is set to the result of the function passed.
-/// This value remains up-to-date on subsequent renders.
-///
-/// The setter function is used to update the value and trigger a re-render.
 ///
 /// # Example
 ///
@@ -515,12 +455,6 @@ where
 }
 
 /// `use_effect` is used for hooking into the component's lifecycle.
-/// Similar to `rendered` method of [`Component`] trait,
-/// `use_effect` takes a function which is called after the render finishes.
-///
-/// The said function returns another function, the destructor function,
-/// which called when the component is destroyed. It can be used to clean up the effects introduced.
-/// This is similar to `destroyed` method of [`Component`] trait.
 ///
 /// # Example
 ///
