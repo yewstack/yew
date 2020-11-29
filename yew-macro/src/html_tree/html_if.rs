88cadeb21c1f58f826cfbb7f3d58f667a41f4fe9
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-11-29 12:15:56 +0100

    WIP

--- yew-macro/src/html_tree/html_if.rs
+++ yew-macro/src/html_tree/html_if.rs
@@ -1,13 +1,23 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 use crate::PeekValue;
 use boolinator::Boolinator;
-use proc_macro2::TokenStream;
-use quote::{quote_spanned, ToTokens};
+use proc_macro2::{TokenStream, Delimiter};
+use quote::{quote_spanned, ToTokens, quote};
 use syn::buffer::Cursor;
 use syn::parse::{Parse, ParseStream, Result as ParseResult};
 use syn::spanned::Spanned;
-use syn::{Expr, ExprIf};
+use syn::{Expr, ExprIf, Token, braced, token};
+use super::HtmlRoot;
 
-pub struct HtmlIf(ExprIf);
+pub struct HtmlIf {
+    if_token: Token![if],
+    cond: Box<Expr>,
+    then_branch: HtmlBranch,
+    else_branch: Option<(Token![else], Box<HtmlBranchOrIf>)>,
+}
 
 impl PeekValue<()> for HtmlIf {
     fn peek(cursor: Cursor) -> Option<()> {
@@ -18,22 +28,26 @@ impl PeekValue<()> for HtmlIf {
 
 impl Parse for HtmlIf {
     fn parse(input: ParseStream) -> ParseResult<Self> {
-        let expr = match input.parse() {
-            Ok(expr) => expr,
-            _ => {
-                return Err(syn::Error::new(
-                    input.span(),
-                    "expected a valid `if` expression",
-                ))
-            }
-        };
+        let if_token = input.parse()?;
+        let cond = input.parse()?;
+        let then_branch = input.parse()?;
+        let else_branch = input.parse::<Token![else]>()
+            .ok()
+            .map(|else_token| input.parse().map(|branch| (else_token, branch)))
+            .transpose()?;
 
-        Ok(HtmlIf(expr))
+        Ok(HtmlIf {
+            if_token,
+            cond,
+            then_branch,
+            else_branch,
+        })
     }
 }
 
 impl ToTokens for HtmlIf {
     fn to_tokens(&self, tokens: &mut TokenStream) {
+        /*
         let expr = &self.0;
         let cond = &expr.cond;
         let then_branch = &expr.then_branch;
@@ -46,7 +60,89 @@ impl ToTokens for HtmlIf {
         let new_tokens = quote_spanned! {expr.span()=>
             if #cond #then_branch else #else_branch
         };
+        */
+        let HtmlIf {
+            if_token,
+            cond,
+            then_branch,
+            else_branch,
+        } = self;
+        let default_else_branch = syn::parse_str("{}").unwrap();
+        let else_branch = else_branch
+            .as_ref()
+            .map(|(_, branch)| branch)
+            .unwrap_or(&default_else_branch);
+        let new_tokens = quote_spanned! {if_token.span()=>
+            if #cond #then_branch else #else_branch
+        };
 
         tokens.extend(new_tokens);
+    }
+}
+
+pub struct HtmlBranch {
+    brace: token::Brace,
+    root: HtmlRoot,
+}
+
+impl PeekValue<()> for HtmlBranch {
+    fn peek(cursor: Cursor) -> Option<()> {
+        cursor.group(Delimiter::Brace).map(|_| ())
+    }
+}
+
+impl Parse for HtmlBranch {
+    fn parse(input: ParseStream) -> ParseResult<Self> {
+        let content;
+        let brace = braced!(content in input);
+        let root = content.parse()?;
+
+        Ok(HtmlBranch {
+            brace,
+            root,
+        })
+    }
+}
+
+impl ToTokens for HtmlBranch {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let Self {
+            root,
+            ..
+        } = self;
+
+        tokens.extend(quote! {
+            { #root }
+        });
+    }
+}
+
+pub enum HtmlBranchOrIf {
+    Branch(HtmlBranch),
+    If(HtmlIf),
+}
+
+impl PeekValue<()> for HtmlBranchOrIf {
+    fn peek(cursor: Cursor) -> Option<()> {
+        HtmlBranch::peek(cursor).or_else(|| HtmlIf::peek(cursor))
+    }
+}
+
+impl Parse for HtmlBranchOrIf {
+    fn parse(input: ParseStream) -> ParseResult<Self> {
+        if HtmlBranch::peek(input.cursor()).is_some() {
+            Ok(Self::Branch(input.parse()?))
+        } else {
+            Ok(Self::If(input.parse()?))
+        }
+    }
+}
+
+impl ToTokens for HtmlBranchOrIf {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            Self::Branch(x) => x.to_tokens(tokens),
+            Self::If(x) => x.to_tokens(tokens),
+        }
     }
 }
--- yew-macro/tests/html_macro/html-if-pass.rs
+++ yew-macro/tests/html_macro/html-if-pass.rs
@@ -1,17 +1,21 @@
 use yew::prelude::*;
 
 fn compile_pass() {
-    let boolean = true;
-    html! { if boolean { html!() } };
-    html! { if boolean { html!() } else { html!() } };
-    html! { <div>if boolean { html!() }</div> };
-    html! { <div>if boolean { html!() } else { html!() }</div> };
+    html! { if true { <div/> } };
+    html! { if true { html!() } };
+    html! { if true {} };
+    html! { if true { let _x = 42; html!() } };
+    html! { if true { <div/> } else { html!() } };
+    html! { if false { <div/> } else if true { html!() } else { html!() } };
+    html! { if false { <div/> } else if false { html!() } else {} };
+    /*
 
     let option = Some("text");
     html! { if let Some(text) = option { html!( {text} ) } };
     html! { if let Some(text) = option { html!( {text} ) } else { html!() } };
     html! { <div>if let Some(text) = option { html!( {text} ) }</div> };
     html! { <div>if let Some(text) = option { html!( {text} ) } else { html!() }</div> };
+    */
 }
 
 fn main() {}
