6bfb7af6a444b3172bb41b28be644a0a6ca5e43b
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-12-04 17:05:29 +0100

    Update yew-macro/tests/html_macro/html-if-pass.rs
    Co-authored-by: Simon <simon@siku2.io>

--- yew-macro/tests/html_macro/html-if-pass.rs
+++ yew-macro/tests/html_macro/html-if-pass.rs
@@ -9,7 +9,7 @@ fn compile_pass() {
     html! { if true {} else {} };
     html! { if true {} else if true {} };
     html! { if true {} else if true {} else {} };
-    html! { if let Some(text) = Some("text") { <span>{text}</span> } };
+    html! { if let Some(text) = Some("text") { <span>{ text }</span> } };
     html! { <><div/>if true {}<div/></> };
     html! { <div>if true {}</div> };
 }
