8fde193537eb1d2ec1b45d005b369d45ce959b22
Justin Starry <justin.starry@icloud.com>
2021-01-10 14:28:53 +0800

    Refactor html module into new component submodule (#1689)
    * Refactor html module into new component submodule
    
    * Fix macro tests

---
+++ yew/src/html/component/properties.rs
@@ -0,0 +1,30 @@
+//! Component properties module
+
+pub use yew_macro::Properties;
+
+/// Trait for building properties for a component
+pub trait Properties: Clone {
+    /// Builder that will be used to construct properties
+    type Builder;
+
+    /// Entrypoint for building properties
+    fn builder() -> Self::Builder;
+}
+
+/// Builder for when a component has no properties
+#[derive(Debug)]
+#[doc(hidden)]
+pub struct EmptyBuilder;
+
+impl Properties for () {
+    type Builder = EmptyBuilder;
+
+    fn builder() -> Self::Builder {
+        EmptyBuilder
+    }
+}
+
+impl EmptyBuilder {
+    /// Build empty properties
+    pub fn build(self) {}
+}
