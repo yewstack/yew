11028f3be70cca906f157a0e00be08b1c510f2e2
Hamza <muhammadhamza1311@gmail.com>
2021-01-23 17:38:28 +0500

    okay, it should compile now

--- packages/yew-services/src/callback_test_util.rs
+++ packages/yew-services/src/callback_test_util.rs
@@ -63,7 +63,7 @@ mod web_sys {
     }
 
     impl<T> CallbackFuture<T> {
-        fn ready(&self) -> Option<T> {
+        pub fn ready(&self) -> Option<T> {
             self.0.borrow_mut().output.take()
         }
 
