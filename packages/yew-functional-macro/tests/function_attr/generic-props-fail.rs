5ea6f81130c3a24ec2aced6e116ea7537d47d0e5
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-02-24 10:05:48 -0800

    Generic functional components (#1756)
    * Generic functional components
    
    * Add some tests
    
    * Clippy
    
    * Fix generic-props-fail snapshot
    
    * Add docs for generic function components
    
    * Add some more docs
    
    * Fix trait bounds in docs
    
    * Fix docs
    
    * Improve lifetime error messages
    
    * Fix parsing for const generics
    
    * Use fully qualified path for pass test
    
    * Remove TODO message
    
    * Suggested change
    
    Co-authored-by: Simon <simon@siku2.io>
    
    * Update test
    
    Co-authored-by: Simon <simon@siku2.io>
    
    * Update test
    
    Co-authored-by: Simon <simon@siku2.io>
    
    * Update stderr snapshots
    
    * Combine quote implementations
    
    * Fix warning about type alias bounds
    
    Co-authored-by: Simon <simon@siku2.io>

---
+++ packages/yew-functional-macro/tests/function_attr/generic-props-fail.rs
@@ -0,0 +1,34 @@
+use yew::prelude::*;
+use yew_functional::function_component;
+
+#[derive(Clone, Properties, PartialEq)]
+struct Props {
+    a: usize,
+}
+
+#[function_component(Comp)]
+fn comp<P>(_props: &P) -> Html
+where
+    P: Properties + PartialEq,
+{
+    html! {
+        <p></p>
+    }
+}
+
+struct MissingTypeBounds;
+
+fn compile_fail() {
+    // missing prop 'a'
+    html! { <Comp<Props> /> };
+
+    // invalid type parameter
+    html! { <Comp<INVALID> /> };
+    // parameter doesn't match bounds
+    html! { <Comp<MissingTypeBounds> /> };
+
+    // missing type param
+    html! { <Comp /> };
+}
+
+fn main() {}
