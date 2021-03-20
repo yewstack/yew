d1b9f76a7970f473fae2eec41fd777ede38f4590
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-03-20 09:25:31 +0100

    Field became not required

--- packages/yew/src/html/component/lifecycle.rs
+++ packages/yew/src/html/component/lifecycle.rs
@@ -255,6 +255,7 @@ mod tests {
     #[derive(Clone, Properties, Default)]
     struct Props {
         lifecycle: Rc<RefCell<Vec<String>>>,
+        #[cfg(feature = "wasm_test")]
         create_message: Option<bool>,
         update_message: RefCell<Option<bool>>,
         view_message: RefCell<Option<bool>>,
@@ -272,6 +273,7 @@ mod tests {
 
         fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
             props.lifecycle.borrow_mut().push("create".into());
+            #[cfg(feature = "wasm_test")]
             if let Some(msg) = props.create_message {
                 link.send_message(msg);
             }
@@ -351,6 +353,7 @@ mod tests {
         test_lifecycle(
             Props {
                 lifecycle: lifecycle.clone(),
+                #[cfg(feature = "wasm_test")]
                 create_message: Some(false),
                 ..Props::default()
             },
@@ -430,6 +433,7 @@ mod tests {
         test_lifecycle(
             Props {
                 lifecycle,
+                #[cfg(feature = "wasm_test")]
                 create_message: Some(true),
                 update_message: RefCell::new(Some(true)),
                 ..Props::default()
