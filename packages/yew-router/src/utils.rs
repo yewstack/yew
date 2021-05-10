9bad46a571849997af240656ad23b2bebefb5f8c
Hamza <muhammadhamza1311@gmail.com>
2021-05-10 23:39:39 +0500

    use `href` getter instead of reading attribute

--- packages/yew-router/src/utils.rs
+++ packages/yew-router/src/utils.rs
@@ -2,21 +2,22 @@ use std::collections::HashMap;
 use wasm_bindgen::{JsCast, JsValue};
 
 pub fn base_url() -> Option<String> {
-    match yew::utils::document().query_selector("base") {
-        Ok(Some(base)) => base
-            .unchecked_into::<web_sys::Element>()
-            .attributes()
-            .get_named_item("href")
-            .map(|it| it.value())
-            .map(|base| {
-                if base != "/" {
-                    base.strip_suffix("/")
-                        .map(|it| it.to_string())
-                        .unwrap_or(base)
-                } else {
-                    base
-                }
-            }),
+    match yew::utils::document().query_selector("base[href]") {
+        Ok(Some(base)) => {
+            let base = base.unchecked_into::<web_sys::HtmlBaseElement>().href();
+
+            let url = web_sys::Url::new(&base).unwrap();
+            let base = url.pathname();
+
+            let base = if base != "/" {
+                base.strip_suffix("/")
+                    .map(|it| it.to_string())
+                    .unwrap_or(base)
+            } else {
+                base
+            };
+            Some(base)
+        }
         _ => None,
     }
 }
