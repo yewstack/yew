85143b309d94f3ba3c1b5b4da9f28c1d30a3efb3
Nicklas Warming Jacobsen <nicklaswj@gmail.com>
2021-05-17 08:54:19 +0200

    Use Self instead of AppHandle to create AppHandle
    Co-authored-by: Simon <simon@siku2.io>

--- packages/yew/src/app_handle.rs
+++ packages/yew/src/app_handle.rs
@@ -28,8 +28,7 @@ where
     /// Creates a new `App` with a component in a context.
     pub(crate) fn new() -> Self {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        let scope = Scope::new(None);
-        AppHandle { scope }
+        Self { scope: Scope::new(None) }
     }
 
     /// The main entry point of a Yew program which also allows passing properties. It works
