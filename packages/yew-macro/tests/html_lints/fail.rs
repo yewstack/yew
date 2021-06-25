daea5c73b9a8028a3484e363d33aa8796ab772a5
teymour-aldridge <teymour.aldridge@icloud.com>
2021-06-25 20:58:09 +0100

    Fix nightly warning message tests.

--- packages/yew-macro/tests/html_lints/fail.rs
+++ packages/yew-macro/tests/html_lints/fail.rs
@@ -5,7 +5,7 @@ fn main() {
         <a>{ "I don't have a href attribute" }</a>
     };
     let still_bad_a = html! {
-        <a href="javascript:void(0)">{ "I have a malformed href attribute" }</a>
+        <a href="#">{ "I have a malformed href attribute" }</a>
     };
     let bad_img = html! {
         <img src="img.jpeg"/>
