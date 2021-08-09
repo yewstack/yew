f3ca015ecaaf73b15eced4a672360d1a214362e8
Muhammad Hamza <muhammadhamza1311@gmail.com>
2021-08-09 20:53:49 +0500

    Allow taking `Fn(EVENT)` for callbacks too  (#1989)
    * Allow taking `Fn(EVENT)` for callbacks too
    
    * fix bad test
    
    * fmt
    
    * add tests

--- packages/yew/src/html/listener/mod.rs
+++ packages/yew/src/html/listener/mod.rs
@@ -8,6 +8,7 @@ use web_sys::{
     HtmlTextAreaElement as TextAreaElement, InputEvent,
 };
 
+use crate::Callback;
 pub use events::*;
 
 /// A type representing data from `oninput` event.
@@ -82,5 +83,68 @@ fn onchange_handler(this: &Element) -> ChangeData {
         _ => {
             panic!("only an InputElement, TextAreaElement or SelectElement can have an onchange event listener");
         }
+    }
+}
+
+/// A trait similar to `Into<T>` which allows conversion of a value into a [`Callback`].
+/// This is used for event listeners.
+pub trait IntoEventCallback<EVENT> {
+    /// Convert `self` to `Option<Callback<EVENT>>`
+    fn into_event_callback(self) -> Option<Callback<EVENT>>;
+}
+
+impl<EVENT> IntoEventCallback<EVENT> for Callback<EVENT> {
+    fn into_event_callback(self) -> Option<Callback<EVENT>> {
+        Some(self)
+    }
+}
+
+impl<EVENT> IntoEventCallback<EVENT> for &Callback<EVENT> {
+    fn into_event_callback(self) -> Option<Callback<EVENT>> {
+        Some(self.clone())
+    }
+}
+
+impl<EVENT> IntoEventCallback<EVENT> for Option<Callback<EVENT>> {
+    fn into_event_callback(self) -> Option<Callback<EVENT>> {
+        self
+    }
+}
+
+impl<T, EVENT> IntoEventCallback<EVENT> for T
+where
+    T: Fn(EVENT) + 'static,
+{
+    fn into_event_callback(self) -> Option<Callback<EVENT>> {
+        Some(Callback::from(self))
+    }
+}
+
+impl<T, EVENT> IntoEventCallback<EVENT> for Option<T>
+where
+    T: Fn(EVENT) + 'static,
+{
+    fn into_event_callback(self) -> Option<Callback<EVENT>> {
+        Some(Callback::from(self?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn supported_into_event_callback_types() {
+        let f = |_: usize| ();
+        let cb = Callback::from(f);
+
+        // Callbacks
+        let _: Option<Callback<usize>> = cb.clone().into_event_callback();
+        let _: Option<Callback<usize>> = (&cb).into_event_callback();
+        let _: Option<Callback<usize>> = Some(cb).into_event_callback();
+
+        // Fns
+        let _: Option<Callback<usize>> = f.into_event_callback();
+        let _: Option<Callback<usize>> = Some(f).into_event_callback();
     }
 }
