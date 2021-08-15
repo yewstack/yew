d1ea63910652875d2d182f3d25e1bf8ed24137ff
bakape <bakape@gmail.com>
2021-08-15 14:49:01 +0300

    ci: let's find the new MSRV

--- packages/yew/src/html/component/lifecycle.rs
+++ packages/yew/src/html/component/lifecycle.rs
@@ -234,8 +234,8 @@ mod tests {
     #[derive(Clone, Properties, Default)]
     struct Props {
         lifecycle: Rc<RefCell<Vec<String>>>,
-        #[cfg(feature = "wasm_test")]
         #[allow(dead_code)]
+        #[cfg(feature = "wasm_test")]
         create_message: Option<bool>,
         update_message: RefCell<Option<bool>>,
         view_message: RefCell<Option<bool>>,
