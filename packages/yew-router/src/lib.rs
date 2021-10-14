118a8dc506d8a771b4d9cc567e80f1f3d6df120f
Kaede Hoshikawa <futursolo@icloud.com>
2021-10-14 19:21:43 +0900

    Remove Redirect.

--- packages/yew-router/src/lib.rs
+++ packages/yew-router/src/lib.rs
@@ -74,7 +74,7 @@ pub mod prelude {
     //!
     //! This module re-exports the frequently used types from the crate.
 
-    pub use crate::components::{Link, Redirect};
+    pub use crate::components::Link;
     #[doc(no_inline)]
     pub use crate::Routable;
     pub use crate::Router;
