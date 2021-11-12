2432aa9811570c0931009cb15b4c0ec99cd6d470
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2021-11-12 17:04:03 +0900

    `use_bridge` hook for agents (#2125)
    * Add Redirect Comp.
    
    * Fix router behaviour.
    
    * Fix output.
    
    * Fix pr-flow.
    
    * Remove Redirect.
    
    * Readd 77b46bf.
    
    * Add use_bridge hook.
    
    * Add documentation.

---
+++ packages/yew-agent/src/hooks.rs
@@ -0,0 +1,56 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use crate::*;
+use yew::prelude::*;
+
+/// State handle for [`use_bridge`] hook
+pub struct UseBridgeHandle<T>
+where
+    T: Bridged,
+{
+    inner: Rc<RefCell<Box<dyn Bridge<T>>>>,
+}
+
+impl<T> UseBridgeHandle<T>
+where
+    T: Bridged,
+{
+    /// Send a message to an agent.
+    pub fn send(&self, msg: T::Input) {
+        let mut bridge = self.inner.borrow_mut();
+        bridge.send(msg);
+    }
+}
+
+/// A hook to bridge to an Agent.
+///
+/// This hooks will only bridge the agent once over the entire component lifecycle.
+///
+/// Takes a callback as the only argument. The callback will be updated on every render to make
+/// sure captured values (if any) are up to date.
+pub fn use_bridge<T, F>(on_output: F) -> UseBridgeHandle<T>
+where
+    T: Bridged,
+    F: Fn(T::Output) + 'static,
+{
+    let on_output = Rc::new(on_output);
+
+    let on_output_clone = on_output.clone();
+    let on_output_ref = use_ref(move || on_output_clone);
+
+    // Refresh the callback on every render.
+    {
+        let mut on_output_ref = on_output_ref.borrow_mut();
+        *on_output_ref = on_output;
+    }
+
+    let bridge = use_ref(move || {
+        T::bridge(Callback::from(move |output| {
+            let on_output = on_output_ref.borrow().clone();
+            on_output(output);
+        }))
+    });
+
+    UseBridgeHandle { inner: bridge }
+}
