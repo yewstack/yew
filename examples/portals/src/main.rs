21ce1974629dc75480b302e52e9bbe2d4098b038
Martin Molzer <ga65guy@mytum.de>
2021-11-16 00:45:16 +0100

    add shadow dom example

--- examples/portals/src/main.rs
+++ examples/portals/src/main.rs
@@ -1,4 +1,71 @@
-use yew::{create_portal, html, Component, Context, Html};
+use wasm_bindgen::JsCast;
+use web_sys::{Element, ShadowRootInit, ShadowRootMode};
+use yew::{create_portal, html, Children, Component, Context, Html, NodeRef, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct ShadowDOMProps {
+    #[prop_or_default]
+    pub children: Children,
+}
+
+pub struct ShadowDOMHost {
+    host_ref: NodeRef,
+    inner_host: Option<Element>,
+}
+
+impl Component for ShadowDOMHost {
+    type Message = ();
+    type Properties = ShadowDOMProps;
+
+    fn create(_: &Context<Self>) -> Self {
+        Self {
+            host_ref: NodeRef::default(),
+            inner_host: None,
+        }
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        if first_render {
+            let shadow_root = self
+                .host_ref
+                .get()
+                .expect("rendered host")
+                .unchecked_into::<Element>()
+                .attach_shadow(&ShadowRootInit::new(ShadowRootMode::Closed))
+                .expect("installing shadow root succeeds");
+            let inner_host = gloo_utils::document()
+                .create_element("div")
+                .expect("can create inner wrapper");
+            shadow_root
+                .append_child(&inner_host)
+                .expect("can attach inner host");
+            self.inner_host = Some(inner_host);
+            ctx.link().send_message(());
+        }
+    }
+
+    fn update(&mut self, _: &Context<Self>, _: Self::Message) -> bool {
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let contents = if let Some(ref inner_host) = self.inner_host {
+            create_portal(
+                html! {
+                    {for ctx.props().children.iter()}
+                },
+                inner_host.clone(),
+            )
+        } else {
+            html! { <></> }
+        };
+        html! {
+            <div ref={self.host_ref.clone()}>
+                {contents}
+            </div>
+        }
+    }
+}
 
 pub struct Model {
     pub style_html: Html,
@@ -26,6 +93,9 @@ impl Component for Model {
             <>
             {self.style_html.clone()}
             <p>{"This paragraph is colored red, and its style is mounted into "}<pre>{"document.head"}</pre>{" with a portal"}</p>
+            <ShadowDOMHost>
+                <p>{"This paragraph is rendered in a shadow dom and thus not affected by the surrounding styling context"}</p>
+            </ShadowDOMHost>
             </>
         }
     }
