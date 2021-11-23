c0f584c761edba97f6b073d2fbcf666d6883cefc
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-11-23 13:05:01 +0200

    Add multithreading example (#2171)
    * correctly reflect the status of multi_thread example
    
    * commiting a partially working example ported from 0.18 to next
    
    * make the app actually start
    
    * remove example from github workflows
    
    * rust-fmt
    
    * cargo make pr-flow done succesfully :)
    
    * update per comments
    
    * fix build errors
    
    * fix cargo fmt
    
    * fix stuff
    
    * Apply suggestions from code review
    
    Co-authored-by: mc1098 <m.cripps1@uni.brighton.ac.uk>
    
    * Update examples/web_worker_fib/src/app.rs
    
    Co-authored-by: mc1098 <m.cripps1@uni.brighton.ac.uk>
    
    * Remove space
    
    * Update .github/workflows/publish-examples.yml
    
    Co-authored-by: WorldSEnder <WorldSEnder@users.noreply.github.com>
    
    Co-authored-by: SpanishPear <shrey.somaiya@gmail.com>
    Co-authored-by: mc1098 <m.cripps1@uni.brighton.ac.uk>
    Co-authored-by: WorldSEnder <WorldSEnder@users.noreply.github.com>

---
+++ examples/web_worker_fib/src/agent.rs
@@ -0,0 +1,55 @@
+use serde::{Deserialize, Serialize};
+use yew_agent::{Agent, AgentLink, HandlerId, Public};
+
+pub struct Worker {
+    link: AgentLink<Self>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct WorkerInput {
+    pub n: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct WorkerOutput {
+    pub value: u32,
+}
+
+impl Agent for Worker {
+    type Reach = Public<Self>;
+    type Message = ();
+    type Input = WorkerInput;
+    type Output = WorkerOutput;
+
+    fn create(link: AgentLink<Self>) -> Self {
+        Self { link }
+    }
+
+    fn update(&mut self, _msg: Self::Message) {
+        // no messaging
+    }
+
+    fn handle_input(&mut self, msg: Self::Input, id: HandlerId) {
+        // this runs in a web worker
+        // and does not block the main
+        // browser thread!
+
+        let n = msg.n;
+
+        fn fib(n: u32) -> u32 {
+            if n <= 1 {
+                1
+            } else {
+                fib(n - 1) + fib(n - 2)
+            }
+        }
+
+        let output = Self::Output { value: fib(n) };
+
+        self.link.respond(id, output);
+    }
+
+    fn name_of_resource() -> &'static str {
+        "wasm.js"
+    }
+}
