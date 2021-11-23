f34c5824ad02096ae798584967b175e38866e713
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-11-23 13:05:01 +0200

    Add multithreading example (#2171)
    * correctly reflect the status of multi_thread example
    
    * commiting a partially working example ported from 0.18 to next
    
    * make the app actually start
    
    * remove example from github workflows
    
    * rust-fmt
    
    * cargo make pr-flow done succesfully :)
    
    * update per comments
    
    * fix build errors
    
    * fix cargo fmt
    
    * fix stuff
    
    * Apply suggestions from code review
    
    Co-authored-by: mc1098 <m.cripps1@uni.brighton.ac.uk>
    
    * Update examples/web_worker_fib/src/app.rs
    
    Co-authored-by: mc1098 <m.cripps1@uni.brighton.ac.uk>
    
    * Remove space
    
    * Update .github/workflows/publish-examples.yml
    
    Co-authored-by: WorldSEnder <WorldSEnder@users.noreply.github.com>
    
    Co-authored-by: SpanishPear <shrey.somaiya@gmail.com>
    Co-authored-by: mc1098 <m.cripps1@uni.brighton.ac.uk>
    Co-authored-by: WorldSEnder <WorldSEnder@users.noreply.github.com>

---
+++ examples/web_worker_fib/src/lib.rs
@@ -0,0 +1,19 @@
+#![recursion_limit = "1024"]
+#![allow(clippy::large_enum_variant)]
+
+pub mod agent;
+pub mod app;
+use app::Model;
+use wasm_bindgen::prelude::*;
+use yew_agent::Threaded;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    use js_sys::{global, Reflect};
+
+    if Reflect::has(&global(), &JsValue::from_str("window")).unwrap() {
+        yew::start_app::<Model>();
+    } else {
+        agent::Worker::register();
+    }
+}
