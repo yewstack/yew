6158413e7e903d48c6f6b0b906efc1f006896ebf
Kaede Hoshikawa <futursolo@icloud.com>
2021-12-06 00:23:48 +0900

    Fix pr-flow.

--- packages/yew-router/tests/utils.rs
+++ packages/yew-router/tests/utils.rs
@@ -1,14 +1,14 @@
 use wasm_bindgen::JsCast;
 
 pub fn obtain_result_by_id(id: &str) -> String {
-    gloo_utils::document()
+    gloo::utils::document()
         .get_element_by_id(id)
         .expect("No result found. Most likely, the application crashed and burned")
         .inner_html()
 }
 
 pub fn click(selector: &str) {
-    gloo_utils::document()
+    gloo::utils::document()
         .query_selector(selector)
         .unwrap()
         .unwrap()
@@ -18,7 +18,7 @@ pub fn click(selector: &str) {
 }
 
 pub fn history_length() -> u32 {
-    gloo_utils::window()
+    gloo::utils::window()
         .history()
         .expect("No history found")
         .length()
