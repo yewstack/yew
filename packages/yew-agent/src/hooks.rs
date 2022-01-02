9360a65ce0e5cd9ed6f13b705874379376f345fe
Hamza <muhammadhamza1311@gmail.com>
2022-01-02 21:49:41 +0500

    rename: agent -> worker

--- packages/yew-agent/src/hooks.rs
+++ packages/yew-agent/src/hooks.rs
@@ -16,16 +16,16 @@ impl<T> UseBridgeHandle<T>
 where
     T: Bridged,
 {
-    /// Send a message to an agent.
+    /// Send a message to an worker.
     pub fn send(&self, msg: T::Input) {
         let mut bridge = self.inner.borrow_mut();
         bridge.send(msg);
     }
 }
 
-/// A hook to bridge to an Agent.
+/// A hook to bridge to an [`Worker`].
 ///
-/// This hooks will only bridge the agent once over the entire component lifecycle.
+/// This hooks will only bridge the worker once over the entire component lifecycle.
 ///
 /// Takes a callback as the only argument. The callback will be updated on every render to make
 /// sure captured values (if any) are up to date.
@@ -57,7 +57,7 @@ where
     UseBridgeHandle { inner: bridge }
 }
 
-impl<T: Agent> Clone for UseBridgeHandle<T> {
+impl<T: Worker> Clone for UseBridgeHandle<T> {
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
