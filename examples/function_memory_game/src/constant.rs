fff1ffaab8681e2a2a4260f5d6ec617005cd6c87
Howard.Zuo <leftstick@qq.com>
2022-01-12 21:40:38 +0800

    Example of function memory game (#2255)
    * example of function memory game
    
    * refactor: remove unnecessary manual PartialEq implementation
    
    * refactor: remove unnecessary license note
    
    * refactor: update implementation and doc with suggestions
    
    * refactor: fix format and lint issues

---
+++ examples/function_memory_game/src/constant.rs
@@ -0,0 +1,42 @@
+use serde::{Deserialize, Serialize};
+use strum_macros::{Display, EnumIter};
+
+pub const KEY_BEST_SCORE: &str = "memory.game.best.score";
+
+#[derive(Clone, Copy, Debug, EnumIter, Display, PartialEq, Serialize, Deserialize)]
+pub enum CardName {
+    EightBall,
+    Kronos,
+    BakedPotato,
+    Dinosaur,
+    Rocket,
+    SkinnyUnicorn,
+    ThatGuy,
+    Zeppelin,
+}
+
+#[derive(Clone, Copy, Debug, EnumIter, Display, PartialEq, Serialize, Deserialize)]
+pub enum Status {
+    Ready,
+    Playing,
+    Passed,
+}
+
+pub const RAW_CARDS: [CardName; 16] = [
+    CardName::EightBall,
+    CardName::Kronos,
+    CardName::BakedPotato,
+    CardName::Dinosaur,
+    CardName::Rocket,
+    CardName::SkinnyUnicorn,
+    CardName::ThatGuy,
+    CardName::Zeppelin,
+    CardName::EightBall,
+    CardName::Kronos,
+    CardName::BakedPotato,
+    CardName::Dinosaur,
+    CardName::Rocket,
+    CardName::SkinnyUnicorn,
+    CardName::ThatGuy,
+    CardName::Zeppelin,
+];
