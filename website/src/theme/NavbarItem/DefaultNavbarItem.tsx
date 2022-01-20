ed2aa7db1be75fbb452bad3197550f23ddb0630b
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-01-20 17:05:08 +0500

    the app crashes

---
+++ website/src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import OriginalNavbarItem from '@theme-original/NavbarItem/DefaultNavbarItem';
+
+export default function DefaultNavbarItem(props) {
+    console.log({props})
+    return (
+        <OriginalNavbarItem {...props} />
+    )
+}
+
+
