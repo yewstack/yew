c25c6122e6e192619b127c12bf4db3929e8c0149
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-01-23 17:46:46 +0500

    a proper test

--- packages/yew/src/virtual_dom/typings.rs
+++ packages/yew/src/virtual_dom/typings.rs
@@ -40,15 +40,62 @@ pub struct ElementData {
     children: Vec<VNode>,
 }
 
-#[cfg(test)]
+#[cfg(all(test, feature = "wasm_test"))]
 mod tests {
-    use crate::{html, Callback};
+    use std::cell::RefCell;
+    use std::rc::Rc;
 
-    fn _compiles() {
+    use wasm_bindgen::JsCast;
+    use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
+
+    use crate::{function_component, html, props, Callback, Html, Properties};
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[test]
+    fn it_works() {
         use super::button as Btn;
 
-        let _ = html! {
-            <Btn disabled="true" class="fuck" on_click={Callback::noop()} />
+        const TEXT: &'static str = "Inner Text";
+        const CLICKED_TEXT: &'static str = "Clicked Text";
+
+        #[derive(PartialEq, Properties)]
+        struct Props {
+            on_click: Callback<web_sys::MouseEvent>,
+        }
+
+        #[function_component]
+        fn Comp(props: &Props) -> Html {
+            html! {
+                <Btn class="ccc" name="yes" on_click={props.on_click.clone()}>{ TEXT }</Btn>
+            }
+        }
+
+        let event_data = Rc::new(RefCell::new(None));
+
+        let on_click = {
+            let event_data = Rc::clone(&event_data);
+            Callback::from(move |_| {
+                (*event_data).borrow_mut().replace(CLICKED_TEXT);
+            })
         };
+
+        let document = gloo_utils::document();
+        yew::start_app_with_props_in_element::<Comp>(
+            document.get_element_by_id("output").unwrap(),
+            props!(Props { on_click: on_click }),
+        );
+        let button = gloo_utils::document()
+            .query_selector("#output button")
+            .unwrap()
+            .unwrap()
+            .unchecked_into::<web_sys::HtmlElement>();
+        assert_eq!(button.get_attribute("class").unwrap(), "ccc");
+        assert_eq!(button.get_attribute("name").unwrap(), "yes");
+        assert_eq!(button.inner_text(), TEXT);
+
+        button.click();
+        let data = *event_data.borrow();
+        assert_eq!(data, Some(CLICKED_TEXT))
     }
 }
