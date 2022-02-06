a50f3f0842f902d9ad54ec7d15c2067ba7cb288b
Kaede Hoshikawa <futursolo@icloud.com>
2022-02-06 13:47:57 +0900

    Remove a unused struct in test case.

--- packages/yew-macro/tests/hook_attr/hook-impl-trait-pass.rs
+++ packages/yew-macro/tests/hook_attr/hook-impl-trait-pass.rs
@@ -1,13 +1,6 @@
 // we need to re-test the macro hygiene here as it uses a different implementation for impl traits.
 #![no_implicit_prelude]
 
-#[derive(
-    ::std::prelude::rust_2021::Debug,
-    ::std::prelude::rust_2021::PartialEq,
-    ::std::prelude::rust_2021::Clone,
-)]
-struct Ctx;
-
 #[::yew::prelude::hook]
 fn use_some_string(a: impl ::std::convert::Into<::std::string::String>) -> ::std::string::String {
     a.into()
