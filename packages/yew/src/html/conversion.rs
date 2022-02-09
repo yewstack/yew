eeb09aae6b5f4114524ea0e2007a1dcdce2cde3b
Cecile Tonglet <cecile.tonglet@cecton.com>
2022-02-09 15:14:07 +0100

    Implement ImplicitClone on primitive types
    Especially those that are Copy.

--- packages/yew/src/html/conversion.rs
+++ packages/yew/src/html/conversion.rs
@@ -12,6 +12,20 @@ impl ImplicitClone for NodeRef {}
 impl<Comp: Component> ImplicitClone for Scope<Comp> {}
 // TODO there are still a few missing
 
+macro_rules! impl_implicit_clone {
+    ($($ty:ty),+ $(,)?) => {
+        $(impl ImplicitClone for $ty {})*
+    };
+}
+
+#[rustfmt::skip]
+impl_implicit_clone!(
+    u8, u16, u32, u64, u128,
+    i8, i16, i32, i64, i128,
+    f32, f64,
+    &'static str,
+);
+
 /// A trait similar to `Into<T>` which allows conversion to a value of a `Properties` struct.
 pub trait IntoPropValue<T> {
     /// Convert `self` to a value of a `Properties` struct.
