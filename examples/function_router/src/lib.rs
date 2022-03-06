e372309d53452e827bb29ebc15553526e8602158
Kaede Hoshikawa <futursolo@icloud.com>
2022-03-06 14:57:20 +0900

    Update Implementation Note.

--- examples/function_router/src/lib.rs
+++ examples/function_router/src/lib.rs
@@ -1,13 +1,17 @@
 // # Implementation Note:
 //
-// This example is also used to demonstrate SSR hydration. It is important to follow the following
-// rules when updating the example:
+// This example is also used to demonstrate SSR hydration.
+// It is important to follow the following rules when updating this example:
 //
-// Do not use usize for randomised contents. usize differs in memory size in 32-bit and 64-bit
-// targets (wasm32 is a 32-bit target.) and would lead to a different value even if the Rng at the same state.
+// - Do not use usize for randomised contents.
 //
-// Do not swap StdRng for SmallRng. SmallRng uses different algorithms depending on the platform so
-// it may not yield the same value on the client and server side.
+//   usize differs in memory size in 32-bit and 64-bit targets (wasm32 is a 32-bit target family.)
+//   and would lead to a different value even if the Rng at the same state.
+//
+// - Do not swap StdRng for SmallRng.
+//
+//   SmallRng uses different algorithms depending on the platform.
+//   Hence, it may not yield the same value on the client and server side.
 
 mod app;
 mod components;
