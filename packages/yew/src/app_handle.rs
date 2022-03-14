7feac4af95eb8b5dd942b7c6abb7b510967d0ad5
Kaede Hoshikawa <futursolo@icloud.com>
2022-03-14 21:44:41 +0900

    Renderer::hydrate.

--- packages/yew/src/app_handle.rs
+++ packages/yew/src/app_handle.rs
@@ -57,3 +57,35 @@ fn clear_element(element: &Element) {
         element.remove_child(&child).expect("can't remove a child");
     }
 }
+
+#[cfg_attr(documenting, doc(cfg(feature = "hydration")))]
+#[cfg(feature = "hydration")]
+mod feat_hydration {
+    use super::*;
+
+    use crate::dom_bundle::Fragment;
+
+    impl<ICOMP> AppHandle<ICOMP>
+    where
+        ICOMP: IntoComponent,
+    {
+        pub(crate) fn hydrate_with_props(element: Element, props: Rc<ICOMP::Properties>) -> Self {
+            let app = Self {
+                scope: Scope::new(None),
+            };
+
+            let mut fragment = Fragment::collect_children(&element);
+
+            app.scope
+                .hydrate_in_place(element.clone(), &mut fragment, NodeRef::default(), props);
+
+            // We remove all remaining nodes, this mimics the clear_element behaviour in
+            // mount_with_props.
+            for node in fragment.iter() {
+                element.remove_child(node).unwrap();
+            }
+
+            app
+        }
+    }
+}
