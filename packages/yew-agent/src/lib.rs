c764a24703066563f5babe419e2c1fdb721ef0e4
Kaede Hoshikawa <futursolo@icloud.com>
2022-03-19 18:42:55 +0900

    Make private bridges.

--- packages/yew-agent/src/lib.rs
+++ packages/yew-agent/src/lib.rs
@@ -1,7 +1,20 @@
 //! This module contains Yew's web worker implementation.
+//!
+//! There're 2 kinds of agents:
+//!
+//! ### Private
+//!
+//! Each time a bridge is created with the `use_private_bridge`, a new instance
+//! of agent is spawned. This allows parallel computing between agents.
+//!
+//! ### Public
+//!
+//! Public agents are shared among all children of a [PublicAgentProvider].
+//! Only 1 instance will be spawned for each public agents provider.
 
-mod hooks;
+mod primitives;
+mod private;
 
 #[doc(inline)]
-pub use gloo_worker::*;
-pub use hooks::{use_bridge, UseBridgeHandle};
+pub use primitives::{Agent, AgentScope, Bridge, Spawnable, Spawner};
+pub use private::{use_private_bridge, UsePrivateBridgeHandle};
---
+++ packages/yew-agent/src/primitives.rs
@@ -0,0 +1,3 @@
+//! This module contains low-level primitives exported from gloo-worker.
+
+pub use gloo_worker::{Bridge, Spawnable, Spawner, Worker as Agent, WorkerScope as AgentScope};
