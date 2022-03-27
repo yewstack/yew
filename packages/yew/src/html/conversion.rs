00f0f3589463d82850629b335d6b368985554e9e
Finn Bear <finnbearone@gmail.com>
2022-03-27 15:43:53 -0700

    Fix #2553 - automatically convert closure to callback for component properties (#2554)
    * Fix #2553 - automatically convert closure to callback for component properties.
    
    * Support F -> Option<Callback<_>> too.
    
    * test stderr.

--- packages/yew/src/html/conversion.rs
+++ packages/yew/src/html/conversion.rs
@@ -1,3 +1,4 @@
+use super::super::callback::Callback;
 use super::{Component, NodeRef, Scope};
 use crate::virtual_dom::AttrValue;
 use std::rc::Rc;
@@ -64,6 +65,33 @@ where
     }
 }
 
+impl<I, O, F> IntoPropValue<Callback<I, O>> for F
+where
+    F: 'static + Fn(I) -> O,
+{
+    fn into_prop_value(self) -> Callback<I, O> {
+        Callback::from(self)
+    }
+}
+
+impl<I, O, F> IntoPropValue<Option<Callback<I, O>>> for F
+where
+    F: 'static + Fn(I) -> O,
+{
+    fn into_prop_value(self) -> Option<Callback<I, O>> {
+        Some(Callback::from(self))
+    }
+}
+
+impl<I, O, F> IntoPropValue<Option<Callback<I, O>>> for Option<F>
+where
+    F: 'static + Fn(I) -> O,
+{
+    fn into_prop_value(self) -> Option<Callback<I, O>> {
+        self.map(|f| Callback::from(f))
+    }
+}
+
 macro_rules! impl_into_prop {
     (|$value:ident: $from_ty:ty| -> $to_ty:ty { $conversion:expr }) => {
         // implement V -> T
@@ -110,5 +138,15 @@ mod test {
         let _: AttrValue = "foo".into_prop_value();
         let _: Option<AttrValue> = "foo".into_prop_value();
         let _: Option<AttrValue> = Rc::<str>::from("foo").into_prop_value();
+    }
+
+    #[test]
+    fn test_callback() {
+        let _: Callback<String> = (|_: String| ()).into_prop_value();
+        let _: Option<Callback<String>> = (|_: String| ()).into_prop_value();
+        let _: Option<Callback<String>> = Some(|_: String| ()).into_prop_value();
+        let _: Callback<String, String> = (|s: String| s).into_prop_value();
+        let _: Option<Callback<String, String>> = (|s: String| s).into_prop_value();
+        let _: Option<Callback<String, String>> = Some(|s: String| s).into_prop_value();
     }
 }
