5fb8ea6a679dc5981ef908e769df97df449f4883
Kaede Hoshikawa <futursolo@icloud.com>
2022-04-01 18:07:57 +0900

    Add more description.

--- packages/yew-agent/src/lib.rs
+++ packages/yew-agent/src/lib.rs
@@ -34,6 +34,40 @@
 //!
 //! Public agents are shared among all children of a [WorkerProvider].
 //! Only 1 instance will be spawned for each public agents provider.
+//!
+//! ## Provider
+//!
+//! Each Agent requires a provider to provide communications and maintain bridges.
+//! All hooks must be called within a provider.
+//!
+//! ## Communications with Agents
+//!
+//! Hooks provides means to communicate with agent instances.
+//!
+//! ### Bridge
+//!
+//! See: [`use_worker_bridge`](worker::use_worker_bridge),
+//! [`use_station_bridge`](station::use_station_bridge),
+//! [`use_reactor_bridge`](reactor::use_reactor_bridge)
+//!
+//! A bridge takes a callback to receive outputs from agents
+//! and provides a handle to send inputs to agents.
+//!
+//! ### Subscription
+//!
+//! See: [`use_worker_subscription`](worker::use_worker_subscription),
+//! [`use_station_subscription`](station::use_station_subscription),
+//! [`use_reactor_subscription`](reactor::use_reactor_subscription)
+//!
+//! Similar to bridges, a subscription produces a handle to send inputs to agents. However, instead
+//! of notifying the receiver with a callback, it collect all outputs into a slice.
+//!
+//! ### Task
+//!
+//! See: [`use_task`](task::use_task), [`use_memorized_task`](task::use_memorized_task)
+//!
+//! Unlike other agents, tasks provides a `use_task` hook for mutation-like usage and a
+//! `use_memoized_task` hook for query like usage.
 
 #![deny(
     clippy::all,
