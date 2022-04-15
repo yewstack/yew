57bd9ce64ca9a748cc88e89d02a1802fe60acce6
Martin Molzer <ga65guy@mytum.de>
2022-04-15 12:52:24 +0200

    introduce use_future_with_deps

--- packages/yew/src/suspense/hooks.rs
+++ packages/yew/src/suspense/hooks.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(documenting, doc(cfg(any(target_arch = "wasm32", feature = "tokio"))))]
 #[cfg(any(target_arch = "wasm32", feature = "tokio"))]
 mod feat_futures {
+    use std::cell::Cell;
     use std::fmt;
     use std::future::Future;
     use std::ops::Deref;
@@ -51,6 +52,46 @@ mod feat_futures {
                     })
                 },
                 (),
+            )
+        };
+
+        if suspension.resumed() {
+            Ok(UseFutureHandle { inner: output })
+        } else {
+            Err((*suspension).clone())
+        }
+    }
+
+    #[hook]
+    pub fn use_future_with_deps<F, D, T, O>(f: F, deps: D) -> SuspensionResult<UseFutureHandle<O>>
+    where
+        F: FnOnce(&D) -> T + 'static,
+        T: Future<Output = O> + 'static,
+        O: 'static,
+        D: PartialEq + 'static,
+    {
+        let output = use_state(|| None);
+        // We only commit a result if it comes from the latest spawned future. Otherwise, this
+        // might trigger pointless updates or even override newer state.
+        let latest_id = use_state(|| Cell::new(0u32));
+
+        let suspension = {
+            let output = output.clone();
+
+            use_memo(
+                move |deps| {
+                    let self_id = latest_id.get().wrapping_add(1);
+                    // As long as less than 2**32 futures are in flight wrapping_add is fine
+                    (*latest_id).set(self_id);
+                    let task = f(deps);
+                    Suspension::from_future(async move {
+                        let result = task.await;
+                        if latest_id.get() == self_id {
+                            output.set(Some(result));
+                        }
+                    })
+                },
+                deps,
             )
         };
 
