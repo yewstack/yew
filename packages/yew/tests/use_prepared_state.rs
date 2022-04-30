3faa58c64313141bfbece54fc063bea811011676
Kaede Hoshikawa <futursolo@icloud.com>
2022-05-01 01:45:48 +0900

    Add tests for use_transitive_state.

--- packages/yew/tests/use_prepared_state.rs
+++ packages/yew/tests/use_prepared_state.rs
@@ -1,3 +1,4 @@
+#![cfg(target_arch = "wasm32")]
 #![cfg(feature = "hydration")]
 
 use std::time::Duration;
---
+++ packages/yew/tests/use_transitive_state.rs
@@ -0,0 +1,63 @@
+#![cfg(feature = "hydration")]
+#![cfg(target_arch = "wasm32")]
+
+use std::time::Duration;
+
+mod common;
+
+use common::obtain_result_by_id;
+use gloo::timers::future::sleep;
+use wasm_bindgen_test::*;
+use yew::prelude::*;
+use yew::{Renderer, ServerRenderer};
+
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+async fn use_transitive_state_works() {
+    #[function_component]
+    fn Comp() -> Html {
+        let ctr = use_transitive_state!(|_| -> u32 { 12345 }, ()).unwrap_or_default();
+
+        html! {
+            <div>
+                {*ctr}
+            </div>
+        }
+    }
+
+    #[function_component]
+    fn App() -> Html {
+        html! {
+            <div>
+                <Comp />
+            </div>
+        }
+    }
+
+    let s = ServerRenderer::<App>::new().render().await;
+
+    assert_eq!(
+        s,
+        // div text content should be 0 but state should be 12345.
+        r#"<!--<[use_transitive_state::use_transitive_state_works::{{closure}}::App]>--><div><!--<[use_transitive_state::use_transitive_state_works::{{closure}}::Comp]>--><div>0</div><script type="application/x-yew-comp-state">ATkwAAAB</script><!--</[use_transitive_state::use_transitive_state_works::{{closure}}::Comp]>--></div><!--</[use_transitive_state::use_transitive_state_works::{{closure}}::App]>-->"#
+    );
+
+    gloo::utils::document()
+        .query_selector("#output")
+        .unwrap()
+        .unwrap()
+        .set_inner_html(&s);
+
+    sleep(Duration::ZERO).await;
+
+    Renderer::<App>::with_root(gloo_utils::document().get_element_by_id("output").unwrap())
+        .hydrate();
+
+    sleep(Duration::ZERO).await;
+
+    let result = obtain_result_by_id("output");
+
+    // no placeholders, hydration is successful and div text content now becomes 12345.
+    assert_eq!(result, r#"<div><div>12345</div></div>"#);
+}
