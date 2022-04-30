e6b3fc8523bda6c1a2c97848c7cca001e63ff6f1
Kaede Hoshikawa <futursolo@icloud.com>
2022-05-01 03:46:55 +0900

    Switch switch to function component.

--- packages/yew-router/src/switch.rs
+++ packages/yew-router/src/switch.rs
@@ -1,13 +1,9 @@
 //! The [`Switch`] Component.
 
-use std::marker::PhantomData;
-
 use gloo::console;
-use wasm_bindgen::UnwrapThrowExt;
 use yew::prelude::*;
 
 use crate::prelude::*;
-use crate::scope_ext::LocationHandle;
 
 /// Props for [`Switch`]
 #[derive(Properties, PartialEq, Clone)]
@@ -21,11 +17,6 @@ where
     pub pathname: Option<String>,
 }
 
-#[doc(hidden)]
-pub enum Msg {
-    ReRender,
-}
-
 /// A Switch that dispatches route among variants of a [`Routable`].
 ///
 /// When a route can't be matched, including when the path is matched but the deserialization fails,
@@ -34,52 +25,26 @@ pub enum Msg {
 /// Otherwise `html! {}` is rendered and a message is logged to console
 /// stating that no route can be matched.
 /// See the [crate level document][crate] for more information.
-pub struct Switch<R: Routable + 'static> {
-    _listener: LocationHandle,
-    _phantom: PhantomData<R>,
-}
-
-impl<R> Component for Switch<R>
+#[function_component]
+pub fn Switch<R>(props: &SwitchProps<R>) -> Html
 where
     R: Routable + 'static,
 {
-    type Message = Msg;
-    type Properties = SwitchProps<R>;
+    let route = use_route::<R>();
 
-    fn create(ctx: &Context<Self>) -> Self {
-        let link = ctx.link();
-        let listener = link
-            .add_location_listener(link.callback(move |_| Msg::ReRender))
-            .expect_throw("failed to create history handle. Do you have a router registered?");
+    let route = props
+        .pathname
+        .as_ref()
+        .and_then(|p| R::recognize(p))
+        .or(route);
 
-        Self {
-            _listener: listener,
-            _phantom: PhantomData,
+    let children = match route {
+        Some(route) => props.render.emit(route),
+        None => {
+            console::warn!("no route matched");
+            Html::default()
         }
-    }
+    };
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::ReRender => true,
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let route = ctx
-            .props()
-            .pathname
-            .as_ref()
-            .and_then(|p| R::recognize(p))
-            .or_else(|| ctx.link().route::<R>());
-
-        let children = match route {
-            Some(route) => ctx.props().render.emit(route),
-            None => {
-                console::warn!("no route matched");
-                Html::default()
-            }
-        };
-
-        html! {<>{children}</>}
-    }
+    html! {<>{children}</>}
 }
