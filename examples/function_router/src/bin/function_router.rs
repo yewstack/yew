23a70e9087674c942e97e3443d859d147b9dd426
Martin Molzer <WorldSEnder@users.noreply.github.com>
2022-05-07 14:51:50 +0200

    use required-features for function_router

--- examples/function_router/src/bin/function_router.rs
+++ examples/function_router/src/bin/function_router.rs
@@ -2,8 +2,5 @@ pub use function_router::*;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
-    #[cfg(feature = "csr")]
     yew::Renderer::<App>::new().render();
-    #[cfg(not(feature = "csr"))]
-    panic!("You must enable the csr feature to run this binary");
 }
