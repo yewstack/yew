769897062e1c001e7f3ffe275d272e36239de35e
Martin Molzer <WorldSEnder@users.noreply.github.com>
2022-05-14 15:11:38 +0200

    convert light_switch to example

---
+++ examples/light_switch/README.md
@@ -0,0 +1,23 @@
+# Counter Example
+
+[![Demo](https://img.shields.io/website?label=demo&url=https%3A%2F%2Fexamples.yew.rs%2Flight_switch)](https://examples.yew.rs/light_switch)
+
+A simple example of a light switch controlling the light setting in neighboring component
+
+## Running
+
+Run a debug version of this application:
+
+```bash
+trunk serve
+```
+
+Run a release version of this application:
+
+```bash
+trunk serve --release
+```
+
+## Concepts
+
+Demonstrates the use of ComponentRef to send messages to children and sibling components.
---
+++ examples/light_switch/src/main.rs
@@ -0,0 +1,77 @@
+use yew::html::{BindableRef, Scope};
+use yew::prelude::*;
+
+#[derive(PartialEq, Properties)]
+struct SwitchProps {
+    on_toggle: Callback<MouseEvent>,
+}
+
+#[function_component]
+fn Switch(SwitchProps { on_toggle }: &SwitchProps) -> Html {
+    html! {
+        <div class="switchlabel">
+            <label class="switch">
+                <input type="checkbox" onclick={on_toggle} />
+                <span class="drive">
+                    <span class="buffer" />
+                    <span class="pin" />
+                </span>
+            </label>
+            {"Light switch"}
+        </div>
+    }
+}
+
+enum LightMessage {
+    Toggle,
+}
+
+struct Light {
+    is_on: bool,
+}
+
+impl ComponentWithRef for Light {
+    type Message = LightMessage;
+    type Properties = ();
+    type Reference = Scope<Self>;
+
+    fn create(ctx: &Context<Self>, bindable_ref: BindableRef<Self::Reference>) -> Self {
+        bindable_ref.bind(ctx.link().clone());
+        Light { is_on: false }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, LightMessage::Toggle: LightMessage) -> bool {
+        self.is_on = !self.is_on;
+        true
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> HtmlResult {
+        Ok(html! {
+            <div class={classes!["light", self.is_on.then(|| "on")]} />
+        })
+    }
+}
+
+#[function_component]
+fn Room() -> Html {
+    let light_ref: ComponentRef<Light> = use_html_ref();
+    let on_toggle = {
+        let light_ref = light_ref.clone();
+        Callback::from(move |_| {
+            light_ref
+                .get()
+                .expect("a light to have rendered")
+                .send_message(LightMessage::Toggle)
+        })
+    };
+    html! {
+        <>
+            <Light ref={&light_ref} />
+            <Switch {on_toggle} />
+        </>
+    }
+}
+
+fn main() {
+    yew::Renderer::<Room>::new().render();
+}
