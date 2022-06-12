f73f68bdc5ead0ff952e971ee1a72309ea1efcfc
Udasi Tharani <56196911+udasitharani@users.noreply.github.com>
2022-06-12 18:41:13 +0530

    Better NodeRefs Example  (#2727)
    * redesign node refs example with form validation
    
    * use char for single character string
    
    * update readme
    
    * fix placeholder type

--- examples/node_refs/src/main.rs
+++ examples/node_refs/src/main.rs
@@ -6,11 +6,14 @@ use yew::prelude::*;
 
 pub enum Msg {
     HoverIndex(usize),
+    Submit,
 }
 
 pub struct App {
     refs: Vec<NodeRef>,
     focus_index: usize,
+    email_error: String,
+    password_error: String,
 }
 impl App {
     fn apply_focus(&self) {
@@ -27,6 +30,8 @@ impl Component for App {
         Self {
             focus_index: 0,
             refs: vec![NodeRef::default(), NodeRef::default()],
+            email_error: "".to_string(),
+            password_error: "".to_string(),
         }
     }
 
@@ -43,33 +48,66 @@ impl Component for App {
                 self.apply_focus();
                 false
             }
+            Msg::Submit => {
+                let email = &self.refs[0];
+                let password = &self.refs[1];
+                let email_value = email.cast::<HtmlInputElement>().unwrap().value();
+                let password_value = password.cast::<HtmlInputElement>().unwrap().value();
+
+                self.email_error.clear();
+                self.password_error.clear();
+
+                if !(email_value.contains('@') && email_value.contains('.')) {
+                    self.email_error.push_str("Invalid email.")
+                }
+                if password_value.len() < 8 {
+                    self.password_error
+                        .push_str("Password must be at least 8 characters long.")
+                }
+                true
+            }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <div class="main">
-                <h1>{ "Node Refs Example" }</h1>
-                <p>{ "Refs can be used to access and manipulate DOM elements directly" }</p>
-                <ul>
-                    <li>{ "First input will focus on mount" }</li>
-                    <li>{ "Each input will focus on hover" }</li>
-                </ul>
-                <div>
-                    <label>{ "Using tag ref: " }</label>
-                    <input
-                        type="text"
-                        ref={self.refs[0].clone()}
-                        class="input-element"
-                        onmouseover={ctx.link().callback(|_| Msg::HoverIndex(0))}
-                    />
+                <div id="left-pane">
+                    <div>
+                        <h1>{"Create your account"}</h1>
+                        <div class="input-container">
+                            <label>{ "Email" }</label>
+                            <input
+                                type="text"
+                                ref={self.refs[0].clone()}
+                                class="input-element"
+                                onmouseover={ctx.link().callback(|_| Msg::HoverIndex(0))}
+                                placeholder="abcd@xyz.com"
+                            />
+                            <div class="error">{self.email_error.clone()}</div>
+                        </div>
+                        <div class="input-container">
+                            <label>{ "Password" }</label>
+                            <InputComponent
+                                ref={self.refs[1].clone()}
+                                on_hover={ctx.link().callback(|_| Msg::HoverIndex(1))}
+                                placeholder="password"
+                            />
+                            <div class="error">{self.password_error.clone()}</div>
+                        </div>
+                        <button onclick={ctx.link().callback(|_| Msg::Submit)}>{"Create"}</button>
+                    </div>
                 </div>
-                <div>
-                    <label>{ "Using component ref: " }</label>
-                    <InputComponent
-                        ref={self.refs[1].clone()}
-                        on_hover={ctx.link().callback(|_| Msg::HoverIndex(1))}
-                    />
+                <div id="right-pane">
+                    <div>
+                        <div id="graphic"></div>
+                        <h1>{ "Node Refs Example" }</h1>
+                        <p>{ "Refs can be used to access and manipulate DOM elements directly" }</p>
+                        <ul>
+                            <li>{ "First input will focus on mount" }</li>
+                            <li>{ "Each input will focus on hover" }</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         }
