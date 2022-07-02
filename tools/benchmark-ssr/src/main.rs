5aa5d93497737b5e17323f57edec38f40ab1c54d
Kaede Hoshikawa <futursolo@icloud.com>
2022-07-02 23:07:38 +0900

    merge local-runtime into "agent-v2"

--- tools/benchmark-ssr/src/main.rs
+++ tools/benchmark-ssr/src/main.rs
@@ -1,22 +1,44 @@
 use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
 use std::time::{Duration, Instant};
 
+use average::Variance;
+use clap::Parser;
 use function_router::{ServerApp, ServerAppProps};
+use indicatif::{ProgressBar, ProgressStyle};
+use serde::{Deserialize, Serialize};
+use tabled::{Style, TableIteratorExt, Tabled};
 use yew::platform::LocalRuntime;
 use yew::prelude::*;
 
 #[global_allocator]
 static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-fn fib(n: u32) -> u32 {
-    if n <= 1 {
-        1
-    } else {
-        fib(n - 1) + fib(n - 2)
-    }
+#[derive(Parser)]
+struct Args {
+    /// Disable terminal support.
+    #[clap(long)]
+    no_term: bool,
+    /// Write the report to an output path in json format.
+    #[clap(long)]
+    output_path: Option<PathBuf>,
 }
 
-fn baseline() -> Duration {
+fn dur_to_millis(dur: Duration) -> f64 {
+    i32::try_from(dur.as_micros()).map(f64::from).unwrap() / 1000.0
+}
+
+fn bench_baseline() -> Duration {
+    fn fib(n: u32) -> u32 {
+        if n <= 1 {
+            1
+        } else {
+            fib(n - 1) + fib(n - 2)
+        }
+    }
+
     let start_time = Instant::now();
     fib(40);
     start_time.elapsed()
@@ -64,43 +86,168 @@ fn bench_router_app() -> Duration {
     start_time.elapsed()
 }
 
-fn print_result(name: &str, dur: Duration) {
-    let dur_millis = i32::try_from(dur.as_micros()).map(f64::from).unwrap() / 1000.0;
-
-    println!("{}: {:.3}ms", name, dur_millis);
+#[derive(Debug, Tabled, Serialize, Deserialize)]
+struct Statistics {
+    #[tabled(rename = "Benchmark")]
+    name: String,
+    #[tabled(rename = "Round")]
+    round: String,
+    #[tabled(rename = "Min (ms)")]
+    min: String,
+    #[tabled(rename = "Max (ms)")]
+    max: String,
+    #[tabled(rename = "Mean (ms)")]
+    mean: String,
+    #[tabled(rename = "Standard Deviation")]
+    std_dev: String,
 }
 
-fn main() {
-    let mut baselines = Vec::with_capacity(10);
-    let mut hello_world_apps = Vec::with_capacity(10);
-    let mut function_router_apps = Vec::with_capacity(10);
+static ROUND: u16 = 10;
 
-    for _ in 0..10 {
-        let baseline = baseline();
-        baselines.push(baseline);
-        print_result("Baseline", baseline);
-        let bench_hello_world = bench_hello_world();
-        hello_world_apps.push(bench_hello_world);
-        print_result("Hello World (1,000,000 runs)", bench_hello_world);
-        let bench_router_app = bench_router_app();
-        function_router_apps.push(bench_router_app);
-        print_result("Function Router (100,000 runs)", bench_router_app);
+fn main() {
+    let args = Args::parse();
+
+    let mut baseline_results = Vec::new();
+    let mut hello_world_results = Vec::new();
+    let mut function_router_results = Vec::new();
+
+    let bar = if args.no_term {
+        None
+    } else {
+        // There are 3 items per round.
+        let bar = ProgressBar::new(u64::from(ROUND * 3));
+        // Progress Bar needs to be updated in a different thread.
+        {
+            let bar = bar.downgrade();
+            std::thread::spawn(move || {
+                while let Some(bar) = bar.upgrade() {
+                    bar.tick();
+                    std::thread::sleep(Duration::from_millis(100));
+                }
+            });
+        }
+
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template(&format!(
+                    "{{spinner:.green}} {{prefix}} [{{elapsed_precise}}] [{{bar:40.cyan/blue}}] \
+                     round {{msg}}/{}",
+                    ROUND
+                ))
+                // .tick_chars("-\\|/")
+                .progress_chars("=>-"),
+        );
+
+        Some(bar)
+    };
+
+    for i in 0..=ROUND {
+        if let Some(ref bar) = bar {
+            bar.set_message(i.to_string());
+            if i == 0 {
+                bar.set_prefix("Warming up");
+            } else {
+                bar.set_prefix("Running   ");
+            }
+        }
+
+        let dur = bench_baseline();
+        if i > 0 {
+            baseline_results.push(dur);
+            if let Some(ref bar) = bar {
+                bar.inc(1);
+            }
+        }
+
+        let dur = bench_hello_world();
+        if i > 0 {
+            hello_world_results.push(dur);
+            if let Some(ref bar) = bar {
+                bar.inc(1);
+            }
+        }
+
+        let dur = bench_router_app();
+        if i > 0 {
+            function_router_results.push(dur);
+            if let Some(ref bar) = bar {
+                bar.inc(1);
+            }
+        }
     }
 
-    print_result(
-        "Baseline",
-        baselines.into_iter().fold(Duration::ZERO, |l, r| l + r),
-    );
-    print_result(
-        "Hello World (10,000,000 runs)",
-        hello_world_apps
-            .into_iter()
-            .fold(Duration::ZERO, |l, r| l + r),
-    );
-    print_result(
-        "Function Router (1,000,000 runs)",
-        function_router_apps
-            .into_iter()
-            .fold(Duration::ZERO, |l, r| l + r),
-    );
+    if let Some(ref bar) = bar {
+        bar.finish_and_clear();
+    }
+    drop(bar);
+
+    baseline_results.sort();
+    hello_world_results.sort();
+    function_router_results.sort();
+
+    let base_var: Variance = baseline_results
+        .iter()
+        .cloned()
+        .map(dur_to_millis)
+        .collect();
+
+    let hw_var: Variance = hello_world_results
+        .iter()
+        .cloned()
+        .map(dur_to_millis)
+        .collect();
+
+    let fr_var: Variance = function_router_results
+        .iter()
+        .cloned()
+        .map(dur_to_millis)
+        .collect();
+
+    let output = [
+        Statistics {
+            name: "Baseline".into(),
+            round: ROUND.to_string(),
+            min: format!("{:.3}", dur_to_millis(baseline_results[0])),
+            max: format!("{:.3}", dur_to_millis(baseline_results[9])),
+            std_dev: format!("{:.3}", base_var.sample_variance().sqrt()),
+            mean: format!("{:.3}", base_var.mean()),
+        },
+        Statistics {
+            name: "Hello World".into(),
+            round: ROUND.to_string(),
+            min: format!("{:.3}", dur_to_millis(hello_world_results[0])),
+            max: format!("{:.3}", dur_to_millis(hello_world_results[9])),
+            std_dev: format!("{:.3}", hw_var.sample_variance().sqrt()),
+            mean: format!("{:.3}", hw_var.mean()),
+        },
+        Statistics {
+            name: "Function Router".into(),
+            round: ROUND.to_string(),
+            min: format!("{:.3}", dur_to_millis(function_router_results[0])),
+            max: format!("{:.3}", dur_to_millis(function_router_results[9])),
+            std_dev: format!("{:.3}", fr_var.sample_variance().sqrt()),
+            mean: format!("{:.3}", fr_var.mean()),
+        },
+    ];
+
+    println!("{}", output.as_ref().table().with(Style::rounded()));
+
+    if let Some(ref p) = args.output_path {
+        let mut f = fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(p)
+            .expect("failed to write output.");
+
+        f.write_all(
+            serde_json::to_string_pretty(&output)
+                .expect("failed to write output.")
+                .as_bytes(),
+        )
+        .expect("failed to write output.");
+
+        println!();
+        println!("Result has been written to: {}", p.display());
+    }
 }
