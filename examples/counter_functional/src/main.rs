43ab9d01857d228600217310cfe492d53c8dedf0
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-08-08 23:41:03 +0500

    This was not meant to be committed

--- examples/counter_functional/src/main.rs
+++ examples/counter_functional/src/main.rs
@@ -1,29 +1,28 @@
 use yew::prelude::*;
-// #[function_component]
-// fn App() -> Html {
-//     let state = use_state(|| 0);
-//
-//     let incr_counter = {
-//         let state = state.clone();
-//         Callback::from(move |_| state.set(*state + 1))
-//     };
-//
-//     let decr_counter = {
-//         let state = state.clone();
-//         Callback::from(move |_| state.set(*state - 1))
-//     };
-//
-//     html! {
-//         <>
-//             <p @disabled={false}> {"current count: "} {*state} </p>
-//             <button onclick={incr_counter}> {"+"} </button>
-//             <button onclick={decr_counter}> {"-"} </button>
-//         </>
-//     }
-// }
+
+#[function_component]
+fn App() -> Html {
+    let state = use_state(|| 0);
+
+    let incr_counter = {
+        let state = state.clone();
+        Callback::from(move |_| state.set(*state + 1))
+    };
+
+    let decr_counter = {
+        let state = state.clone();
+        Callback::from(move |_| state.set(*state - 1))
+    };
+
+    html! {
+        <>
+            <p @disabled={false}> {"current count: "} {*state} </p>
+            <button onclick={incr_counter}> {"+"} </button>
+            <button onclick={decr_counter}> {"-"} </button>
+        </>
+    }
+}
 
 fn main() {
-    let _d = html! {
-            <div class={format!("fail{}", "")}></div>
-    };
+    yew::Renderer::<App>::new().render();
 }
