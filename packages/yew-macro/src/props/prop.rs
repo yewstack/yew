0ef49190e8de149e060a448e8a1a74954d649819
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-09-25 20:34:43 +0500

    remove dead code

--- packages/yew-macro/src/props/prop.rs
+++ packages/yew-macro/src/props/prop.rs
@@ -23,15 +23,6 @@ pub struct Prop {
     pub value: Expr,
 }
 
-// impl std::fmt::Debug for Prop {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         f.debug_struct("Prop")
-//             .field("label", &self.label.to_string())
-//             .field("label", &self.value)
-//             .finish_non_exhaustive()
-//     }
-// }
-
 impl Parse for Prop {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let directive = input
