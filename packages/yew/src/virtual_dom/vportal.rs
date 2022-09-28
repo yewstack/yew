0ecee11a2f8a8814e1997c2d0b4ab0b49da949d0
WorldSEnder <WorldSEnder@users.noreply.github.com>
2022-09-28 08:32:19 +0200

    Fix portal shifting on reconciliation too often (#2891)
    * fix portal shifting on reconciliation too often
    
    the public vdom api changes to only allow directly
    setting a Node as sibling (still optional) instead of a NodeRef.
    This was the intention all along, since the NodeRef was
    not dynamically tracked, and creating a portal into a subtree
    already controlled by yew is not supported anway.
    
    * fix feature soundness
    
    * fix doc tests

--- packages/yew/src/virtual_dom/vportal.rs
+++ packages/yew/src/virtual_dom/vportal.rs
@@ -3,14 +3,13 @@
 use web_sys::{Element, Node};
 
 use super::VNode;
-use crate::html::NodeRef;
 
 #[derive(Debug, Clone)]
 pub struct VPortal {
     /// The element under which the content is inserted.
     pub host: Element,
     /// The next sibling after the inserted content. Most be a child of `host`.
-    pub inner_sibling: NodeRef,
+    pub inner_sibling: Option<Node>,
     /// The inserted node
     pub node: Box<VNode>,
 }
@@ -20,7 +19,7 @@ impl VPortal {
     pub fn new(content: VNode, host: Element) -> Self {
         Self {
             host,
-            inner_sibling: NodeRef::default(),
+            inner_sibling: None,
             node: Box::new(content),
         }
     }
@@ -31,11 +30,7 @@ impl VPortal {
     pub fn new_before(content: VNode, host: Element, inner_sibling: Option<Node>) -> Self {
         Self {
             host,
-            inner_sibling: {
-                let sib_ref = NodeRef::default();
-                sib_ref.set(inner_sibling);
-                sib_ref
-            },
+            inner_sibling,
             node: Box::new(content),
         }
     }
