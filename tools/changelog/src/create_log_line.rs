99ed15cdb93c41ada9334246e461796b925eb4d5
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-11-21 00:03:50 +0500

    Improve changelog generator

--- tools/changelog/src/create_log_line.rs
+++ tools/changelog/src/create_log_line.rs
@@ -79,6 +79,14 @@ pub fn create_log_line(
 
     if !is_issue_for_this_package {
         println!("Issue {issue_id} is not for {package_labels:?} packages");
+        let leftovers = issue_labels.iter().filter(|label| {
+            !(label.starts_with("A-") || label == "documentation" || label == "meta")
+        });
+        let count = leftovers.count();
+        if count > 0 {
+            println!("Potentially invalidly labeled issue: {issue_id}. Neither A-* (area), documentation nor meta labels found. \
+            inspect/re-tag at https://github.com/yewstack/yew/issues/{issue_id}");
+        }
         return Ok(None);
     }
 
