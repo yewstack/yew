f0209c786f1f36d54b2076fdcc359751f5707f8f
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-11-25 15:19:07 +0500

    Prepare for Yew 0.20 (#2973)
    * 0.20 Changelog
    
    * Improve changelog generator
    
    * Add blog post
    
    * Add blog post
    
    * Apply suggestions from code review
    
    Co-authored-by: WorldSEnder <WorldSEnder@users.noreply.github.com>
    Co-authored-by: Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
    
    * update Changelog
    
    * update Cargo.toml
    
    * changelog gen compiles
    
    * website version 0.20
    
    * add migration guides
    
    * prettier
    
    * i18n
    
    Co-authored-by: WorldSEnder <WorldSEnder@users.noreply.github.com>
    Co-authored-by: Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>

--- tools/changelog/src/create_log_line.rs
+++ tools/changelog/src/create_log_line.rs
@@ -74,11 +74,19 @@ pub fn create_log_line(
         .with_context(|| format!("Could not find GitHub labels for issue: {}", issue_id))?;
 
     let is_issue_for_this_package = issue_labels
-        .into_iter()
+        .iter()
         .any(|label| package_labels.contains(&label.as_str()));
 
     if !is_issue_for_this_package {
         println!("Issue {issue_id} is not for {package_labels:?} packages");
+        let leftovers = issue_labels.iter().filter(|label| {
+            !(label.starts_with("A-") || *label == "documentation" || *label == "meta")
+        });
+        let count = leftovers.count();
+        if count > 0 {
+            println!("Potentially invalidly labeled issue: {issue_id}. Neither A-* (area), documentation nor meta labels found. \
+            inspect/re-tag at https://github.com/yewstack/yew/issues/{issue_id}");
+        }
         return Ok(None);
     }
 
