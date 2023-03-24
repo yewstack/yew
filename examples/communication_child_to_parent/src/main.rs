92349be1369a327b6a965b6db3f65bc49a9dbb59
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-03-25 00:11:05 +1100

    Nightly fmt

--- examples/communication_child_to_parent/src/main.rs
+++ examples/communication_child_to_parent/src/main.rs
@@ -1,7 +1,6 @@
-use yew::{html, AttrValue, Callback, Component, Context, Html, Properties};
-
 use child::Child;
 use parent::Parent;
+use yew::{html, AttrValue, Callback, Component, Context, Html, Properties};
 
 mod child;
 mod parent;
