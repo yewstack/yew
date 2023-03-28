b03b36172be725012e15b5ce296ecaae9b2b7e77
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-03-28 17:48:30 +1100

    FQN Again

--- packages/yew-router-macro/src/routable_derive.rs
+++ packages/yew-router-macro/src/routable_derive.rs
@@ -176,7 +176,7 @@ impl Routable {
                     }
 
                     quote! {
-                        Self::#ident { #(#fields),* } => ::std::format!(#right, #(#fields = ::yew_router::encode_for_url(&format!("{}", #fields))),*)
+                        Self::#ident { #(#fields),* } => ::std::format!(#right, #(#fields = ::yew_router::encode_for_url(&::std::format!("{}", #fields))),*)
                     }
                 }
                 Fields::Unnamed(_) => unreachable!(), // already checked
