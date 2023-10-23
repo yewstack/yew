4980ee2744c611a8bd535f28f36a696231bab4f2
schvv31n <tim.kurdov@gmail.com>
2023-10-23 20:13:56 +0100

    fixed hydration test

--- packages/yew/tests/hydration.rs
+++ packages/yew/tests/hydration.rs
@@ -404,7 +404,7 @@ async fn hydration_with_suspense_not_suspended_at_start() {
 
     assert_eq!(
         result.as_str(),
-        r#"<div class="content-area"><textarea value="I am writing a long story..."></textarea><div class="action-area"><button class="take-a-break">Take a break!</button></div></div>"#
+        r#"<div class="content-area"><textarea>I am writing a long story</textarea><div class="action-area"><button class="take-a-break">Take a break!</button></div></div>"#
     );
     gloo::utils::document()
         .query_selector(".take-a-break")
