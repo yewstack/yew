dbdd3b78e1f0aada1834dec5c6ee83449db9d220
Finn Bear <finnbearlabs@gmail.com>
2024-05-09 19:25:22 -0700

    Namespace support for `VRaw`. (#3640)
    * WIP.
    
    * fmt.
    
    * Fix parameter order.
    
    * Cleanup.
    
    * Change the docs.
    
    * WIP test.
    
    * fmt.
    
    * Cast node to element.
    
    * Fix test.
    
    * Fix html in test.
    
    * Typo.

--- packages/yew/src/dom_bundle/utils.rs
+++ packages/yew/src/dom_bundle/utils.rs
@@ -64,10 +64,23 @@ mod tests {
 
     use crate::dom_bundle::{BSubtree, DomSlot};
     use crate::html::AnyScope;
+    use crate::virtual_dom::vtag::SVG_NAMESPACE;
 
     pub fn setup_parent() -> (BSubtree, AnyScope, Element) {
         let scope = AnyScope::test();
         let parent = document().create_element("div").unwrap();
+        let root = BSubtree::create_root(&parent);
+
+        document().body().unwrap().append_child(&parent).unwrap();
+
+        (root, scope, parent)
+    }
+
+    pub fn setup_parent_svg() -> (BSubtree, AnyScope, Element) {
+        let scope = AnyScope::test();
+        let parent = document()
+            .create_element_ns(Some(SVG_NAMESPACE), "svg")
+            .unwrap();
         let root = BSubtree::create_root(&parent);
 
         document().body().unwrap().append_child(&parent).unwrap();
