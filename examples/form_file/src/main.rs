c854ca612c6db9a7297bc3b7ba5c971282ea76bd
Kenzi Connor <git@knz.ai>
2024-07-20 00:14:01 -0700

    file processing async starts upon file selection
    form submit button disabled till it's done. A more thorough check wouldn't let the form submit if there are readers left

--- examples/form_file/src/main.rs
+++ examples/form_file/src/main.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
 use base64::{engine::general_purpose::STANDARD, Engine};
-use gloo::file::{callbacks::FileReader, File};
+use gloo::file::{callbacks::FileReader, File, FileList};
 use gloo_console::debug;
-use web_sys::{FormData, HtmlFormElement, File as RawFile};
+use web_sys::{File as RawFile, FormData, HtmlFormElement, HtmlInputElement};
 use yew::prelude::*;
 
 pub struct FileDetails {
@@ -14,13 +14,16 @@ pub struct FileDetails {
 }
 
 pub enum Msg {
-    Loaded(FileDetails),
+    Loaded(String, Vec<u8>),
     Submit(SubmitEvent),
+    File(FileList),
 }
 
 pub struct App {
     readers: HashMap<String, FileReader>,
     files: Vec<FileDetails>,
+    button: NodeRef,
+    file_data: Vec<u8>,
 }
 
 impl Component for App {
@@ -31,39 +34,52 @@ impl Component for App {
         Self {
             readers: HashMap::default(),
             files: Vec::default(),
+            button: NodeRef::default(),
+            file_data: Vec::default(),
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Loaded(file) => {
-                let name = file.name.clone();
-                self.files.push(file);
+            Msg::Loaded(name, data) => {
+                let submit = self.button.cast::<HtmlInputElement>().expect("button");
+                self.file_data = data;
+                submit.set_disabled(false);
                 self.readers.remove(&name);
             }
+            Msg::File(files) => {
+                let submit = self.button.cast::<HtmlInputElement>().expect("button");
+                submit.set_disabled(true);
+
+                let file = files[0].clone();
+                let link = ctx.link().clone();
+                let name = file.name().clone();
+                let task = {
+                    gloo::file::callbacks::read_as_bytes(&file, move |res| {
+                        link.send_message(Msg::Loaded(name, res.expect("failed to read file")));
+                    })
+                };
+                self.readers.insert(file.name(), task);
+            }
             Msg::Submit(event) => {
-            	debug!(event.clone());
-            	event.prevent_default();
+                debug!(event.clone());
+                event.prevent_default();
                 let form: HtmlFormElement = event.target_unchecked_into();
                 let form_data = FormData::new_with_form(&form).expect("form data");
                 let image_file = File::from(RawFile::from(form_data.get("file")));
-                let alt_text = form_data.get("alt-text").as_string().unwrap();
 
-                let link = ctx.link().clone();
-                let name = image_file.name().clone();
+                let alt_text = form_data.get("alt-text").as_string().unwrap();
+                let name = image_file.name();
+                let data = self.file_data.clone();
+
                 let file_type = image_file.raw_mime_type();
-                let task = {
-                    gloo::file::callbacks::read_as_bytes(&image_file, move |res| {
-                        link.send_message(Msg::Loaded(FileDetails{
-                            name,
-                            alt_text,
-                            file_type,
-                            data: res.expect("failed to read file"),                        	
-                        }));
-                  	})
-                };
-                self.readers.insert(image_file.name(), task);
-                
+                self.files.push(FileDetails {
+                    alt_text,
+                    name,
+                    data,
+                    file_type,
+                });
+                self.file_data = Vec::default();
             }
         }
         true
@@ -73,16 +89,20 @@ impl Component for App {
         html! {
             <div id="wrapper">
                 <form onsubmit={ctx.link().callback(Msg::Submit)}>
-                	<lable for="alt-text">{"Alt Text"}</lable>
-                	<input id="alt-text" name="alt-text" />
-	                <input
-	                    id="file"
-	                    name="file"
-	                    type="file"
-	                    accept="image/*"
-	                    multiple={false}
-	                />
-	                <input type="submit"/>
+                    <lable for="alt-text">{"Alt Text"}</lable>
+                    <input id="alt-text" name="alt-text" />
+                    <input
+                        id="file"
+                        name="file"
+                        type="file"
+                        accept="image/*"
+                        multiple={false}
+                        onchange={ctx.link().callback(move |e: Event| {
+                            let input: HtmlInputElement = e.target_unchecked_into();
+                            Msg::File(gloo::file::FileList::from(input.files().expect("file")))
+                            })}
+                    />
+                    <input ref={&self.button} type="submit"/>
                 </form>
                 <div id="preview-area">
                     { for self.files.iter().map(Self::view_file) }
