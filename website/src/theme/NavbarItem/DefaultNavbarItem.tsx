af1de231f1f0f6bee936ed732778e262099a88b0
brilliantkid87 <brilliantkid87@gmail.com>
2024-11-06 15:29:14 +0700

    fix: apply Prettier formatting to DefaultNavbarItem.tsx

--- website/src/theme/NavbarItem/DefaultNavbarItem.tsx
+++ website/src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -7,33 +7,35 @@ import { API_BUTTON } from '../../constants.js'
 const VERSION_REGEX = /\/docs\/(0.([0-9]+)(\.[0-9]+)?|next)?/
 const API_BASE_URLS = {
     next: 'https://api.yew.rs/next/yew',
-    default: 'https://docs.rs/yew'
-};
+    default: 'https://docs.rs/yew',
+}
 
 /**
- * @returns {string} 
+ * @returns {string}
  */
 const useVersion = () => {
     const location = useLocation()
     const match = location.pathname.match(VERSION_REGEX)
     return match ? (match[1] ?? '') : ''
-};
+}
 
 /**
- * @param {string} version 
- * @returns {string} 
+ * @param {string} version
+ * @returns {string}
  */
 const getApiUrl = (version) => {
     if (version === 'next') {
         return API_BASE_URLS.next
     }
-    return version ? `${API_BASE_URLS.default}/${version}` : API_BASE_URLS.default
+    return version
+        ? `${API_BASE_URLS.default}/${version}`
+        : API_BASE_URLS.default
 }
 
 /**
- * @param {Object} props 
- * @param {string} props.label 
- * @returns {React.ReactElement} 
+ * @param {Object} props
+ * @param {string} props.label
+ * @returns {React.ReactElement}
  */
 export default function DefaultNavbarItem(props) {
     const { label, ...restProps } = props
