6342b1bb9c71dd88430c4b1897097f9388afbaa1
Matt Yan <syan4@ualberta.ca>
2025-08-09 14:54:50 +0900

    rustversion 1.0.22 turns off incompatible_msrv automatically

--- packages/yew-macro/src/html_tree/html_element.rs
+++ packages/yew-macro/src/html_tree/html_element.rs
@@ -457,11 +457,8 @@ impl ToTokens for HtmlElement {
                 #[rustversion::since(1.88)]
                 fn derive_debug_tag(vtag: &Ident) -> String {
                     let span = vtag.span().unwrap();
-                    #[allow(clippy::incompatible_msrv)]
-                    {
                     // the file, line, column methods are stable since 1.88
                     format!("[{}:{}:{}] ", span.file(), span.line(), span.column())
-                    }
                 }
                 #[rustversion::before(1.88)]
                 fn derive_debug_tag(_: &Ident) -> &'static str {
