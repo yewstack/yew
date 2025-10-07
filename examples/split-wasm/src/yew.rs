7a5487efe7d346de82dab383587feee3a052d89b
Martin Molzer <WorldSEnder@users.noreply.github.com>
2025-10-07 03:15:18 +0200

    initial (very incomplete) integration of split-wasm tooling

---
+++ examples/split-wasm/src/yew.rs
@@ -0,0 +1,79 @@
+use std::future::pending;
+
+use wasm_split::wasm_split as split;
+use web_sys::HtmlInputElement;
+use yew::lazy::{Lazy, LazyComponent, LazyVTable};
+use yew::prelude::*;
+use yew::suspense::Suspension;
+use yew::Renderer;
+
+use super::{COUNTER, GLOBAL_FOO};
+
+// ---------------------------------------------------------------------------
+// A counter component — the one we'll load lazily.
+// Uses use_state, which triggers re-renders via the scope it was created with.
+// ---------------------------------------------------------------------------
+
+#[derive(Clone, Debug, PartialEq, Properties)]
+pub struct CounterProps {
+    pub label: AttrValue,
+}
+
+#[component]
+fn Counter(props: &CounterProps) -> Html {
+    let count = use_state(|| 0_i32);
+    let onclick = {
+        let count = count.clone();
+        Callback::from(move |_| count.set(*count + 1))
+    };
+    let global_foo = GLOBAL_FOO.with(|f| *f);
+    let render_counter = COUNTER.with(|cnt| {
+        let c = cnt.get();
+        cnt.set(c + 1);
+        c
+    });
+
+    html! {
+        <div class="counter">
+            <p>{"This component is loaded from a separate bundle, render count: "}{render_counter}</p>
+            <p>{"Here is a number loaded from (shared) memory: "}{global_foo}</p>
+            <h3>{ &props.label }</h3>
+            <p class="count">{ *count }</p>
+            <button {onclick}>{ "+1" }</button>
+        </div>
+    }
+}
+
+impl LazyComponent for Counter {
+    async fn fetch() -> LazyVTable<Self> {
+        #[split(lazy_addition)]
+        fn split_fetch() -> LazyVTable<Counter> {
+            LazyVTable::<Counter>::vtable()
+        }
+        split_fetch().await
+    }
+}
+
+#[component]
+fn Pending() -> HtmlResult {
+    Err(Suspension::from_future(pending()).into())
+}
+
+#[component]
+fn App() -> Html {
+    let toggle = use_state(|| false);
+    let show = *toggle;
+    html! {
+        <>
+        <input id="additional" type="checkbox" checked={*toggle} oninput={move |ev: InputEvent| toggle.set(ev.target_unchecked_into::<HtmlInputElement>().checked())} />
+        <label for="additional">{"Display additional content"}</label>
+        <Suspense fallback={html! { <p>{"not yet loaded"}</p> }}>
+            if show { <Lazy<Counter> label="A lazily loaded counter" /> } else { <Pending /> }
+        </Suspense>
+        </>
+    }
+}
+
+pub fn main() {
+    let _ = Renderer::<App>::new().render();
+}
---
+++ packages/yew/src/lazy.rs
@@ -0,0 +1,173 @@
+//! Implements lazy fetching of components
+
+// A simple wrapper is easy to implement. This module exists to support message passing and more
+// involved logic
+
+use std::future::Future;
+
+use crate::html::{Scope, Scoped};
+use crate::suspense::Suspension;
+use crate::{BaseComponent, Context, HtmlResult};
+
+// we might be able to erase the BaseComponent bound here, then monomorphize for some size savings
+/// This struct is (mentally) the same as a `dyn BaseComponent` but without informing the linker
+/// about this.
+#[derive(Debug)]
+#[repr(C)]
+struct CompVTableImpl<C: BaseComponent> {
+    create: fn(&Context<C>) -> C,
+    update: fn(&mut C, &Context<C>, C::Message) -> bool,
+    changed: fn(&mut C, &Context<C>, &C::Properties) -> bool,
+    view: fn(&C, &Context<C>) -> HtmlResult,
+    rendered: fn(&mut C, &Context<C>, bool),
+    destroy: fn(&mut C, &Context<C>),
+    prepare_state: fn(&C) -> Option<String>,
+    //...
+}
+/// Component vtable for a component.
+///
+/// Return `LazyVTable::<YourComponent>::vtable()` from your implementation of
+/// [`LazyComponent::fetch`] after resolving it.
+pub struct LazyVTable<C: BaseComponent> {
+    imp: &'static CompVTableImpl<C>,
+}
+impl<C: BaseComponent> std::fmt::Debug for LazyVTable<C> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LazyVTable")
+            .field("vtable", &"...")
+            .finish()
+    }
+}
+impl<C: BaseComponent> LazyVTable<C> {
+    /// Returns the singleton vtable for a component.
+    ///
+    /// Return this from [`LazyComponent::fetch`] for your lazy component.
+    pub fn vtable() -> LazyVTable<C> {
+        LazyVTable {
+            imp: &const {
+                CompVTableImpl {
+                    create: C::create,
+                    update: C::update,
+                    changed: C::changed,
+                    view: C::view,
+                    rendered: C::rendered,
+                    destroy: C::destroy,
+                    prepare_state: C::prepare_state,
+                }
+            },
+        }
+    }
+}
+/// Implement this trait to support lazily loading your component.
+///
+/// Used in conjunction with the [`Lazy`] component.
+pub trait LazyComponent: BaseComponent {
+    /// Start fetching your component
+    fn fetch() -> impl Future<Output = LazyVTable<Self>> + Send;
+}
+
+#[derive(Debug)]
+enum LazyState<C: BaseComponent> {
+    Pending(Suspension),
+    Created(LazyVTable<C>, C),
+}
+/// Wrapper for a lazily fetched component
+///
+/// This component suspends as long as the underlying component is still being fetched,
+/// then behaves as the underlying component itself.
+#[derive(Debug)]
+pub struct Lazy<C: BaseComponent> {
+    inner_scope: Scope<C>,
+    state: LazyState<C>,
+}
+
+/// Message to send to a lazy component
+#[derive(Debug)]
+pub enum LazyMessage<C: BaseComponent> {
+    /// Forward the message to the underlying component once that is fetched
+    Forward(C::Message),
+    #[doc(hidden)]
+    FetchFinished(LazyVTable<C>, C),
+}
+
+impl<C: LazyComponent> BaseComponent for Lazy<C> {
+    type Message = LazyMessage<C>;
+    type Properties = C::Properties;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let inner_scope = Scope::new(Some(ctx.link().to_any()));
+        let creation_ctx = ctx.narrow_scope(&inner_scope);
+
+        let link = ctx.link().clone();
+        // TODO: fetch once per component type not instance?
+        let suspension = Suspension::from_future(async move {
+            // Ignore error in case receiver was dropped
+            let vtable = C::fetch().await;
+            let comp = (vtable.imp.create)(&creation_ctx);
+            link.send_message(LazyMessage::FetchFinished(vtable, comp));
+        });
+        Self {
+            inner_scope,
+            state: LazyState::Pending(suspension),
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        let msg = match msg {
+            LazyMessage::FetchFinished(vtable, comp) => {
+                self.state = LazyState::Created(vtable, comp);
+                return true;
+            }
+            LazyMessage::Forward(msg) => msg,
+        };
+        match &mut self.state {
+            LazyState::Pending(_) => {
+                // has a queueing implementation. We don't rerender until the suspension resolves
+                self.inner_scope.send_message(msg);
+                false
+            }
+            LazyState::Created(vtable, comp) => {
+                (vtable.imp.update)(comp, &ctx.narrow_scope(&self.inner_scope), msg)
+            }
+        }
+    }
+
+    fn changed(&mut self, ctx: &Context<Self>, old_props: &Self::Properties) -> bool {
+        if let LazyState::Created(vtable, comp) = &mut self.state {
+            (vtable.imp.changed)(comp, &ctx.narrow_scope(&self.inner_scope), old_props)
+        } else {
+            false
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> crate::HtmlResult {
+        match &self.state {
+            LazyState::Pending(suspension) => Err(suspension.clone().into()),
+            LazyState::Created(vtable, comp) => {
+                (vtable.imp.view)(comp, &ctx.narrow_scope(&self.inner_scope))
+            }
+        }
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        if let LazyState::Created(vtable, comp) = &mut self.state {
+            (vtable.imp.rendered)(comp, &ctx.narrow_scope(&self.inner_scope), first_render)
+        } else {
+            unreachable!("can't get rendered before fetching the vtable")
+        }
+    }
+
+    fn destroy(&mut self, ctx: &Context<Self>) {
+        if let LazyState::Created(vtable, comp) = &mut self.state {
+            (vtable.imp.destroy)(comp, &ctx.narrow_scope(&self.inner_scope))
+        }
+    }
+
+    fn prepare_state(&self) -> Option<String> {
+        if let LazyState::Created(vtable, comp) = &self.state {
+            (vtable.imp.prepare_state)(comp)
+        } else {
+            None
+        }
+    }
+}
