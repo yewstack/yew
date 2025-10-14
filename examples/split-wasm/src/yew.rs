6816229d9c9d45b65362d890b94f7b34848412a6
Martin Molzer <WorldSEnder@users.noreply.github.com>
2025-10-14 22:57:13 +0200

    add helper macro for lazy loading

--- examples/split-wasm/src/yew.rs
+++ examples/split-wasm/src/yew.rs
@@ -1,9 +1,8 @@
 use std::future::{pending, Future};
-use std::pin::Pin;
 
 use wasm_split_helpers::wasm_split as split;
 use web_sys::HtmlInputElement;
-use yew::lazy::{Lazy, LazyComponent, LazyVTable};
+use yew::lazy::declare_lazy_component;
 use yew::prelude::*;
 use yew::suspense::Suspension;
 use yew::Renderer;
@@ -45,37 +44,7 @@ fn Counter(props: &CounterProps) -> Html {
     }
 }
 
-struct LazyAdditionProxy;
-
-impl LazyComponent for LazyAdditionProxy {
-    type Underlying = Counter;
-
-    async fn fetch() -> LazyVTable<Self::Underlying> {
-        #[split(lazy_addition)]
-        fn split_fetch() -> LazyVTable<Counter> {
-            LazyVTable::<Counter>::vtable()
-        }
-        struct F(Option<Pin<Box<dyn Future<Output = LazyVTable<Counter>> + Send>>>);
-        impl Future for F {
-            type Output = LazyVTable<Counter>;
-
-            fn poll(
-                mut self: Pin<&mut Self>,
-                cx: &mut std::task::Context<'_>,
-            ) -> std::task::Poll<Self::Output> {
-                self.0
-                    .get_or_insert_with(|| Box::pin(split_fetch()))
-                    .as_mut()
-                    .poll(cx)
-            }
-        }
-        static CACHE: async_once_cell::Lazy<LazyVTable<Counter>, F> =
-            async_once_cell::Lazy::new(F(None));
-        *Pin::static_ref(&CACHE).await.get_ref()
-    }
-}
-
-type LazyAddition = Lazy<LazyAdditionProxy>;
+declare_lazy_component!(Counter as LazyAddition in lazy_addition);
 
 #[component]
 fn Pending() -> HtmlResult {
