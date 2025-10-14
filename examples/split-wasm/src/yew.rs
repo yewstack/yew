5be7344a88801cc856c249e0dc4f35128c0ce007
Martin Molzer <WorldSEnder@users.noreply.github.com>
2025-10-14 22:37:01 +0200

    update to release candidate and improve caching
    don't start fetching once per component, cache the vtable.

--- examples/split-wasm/src/yew.rs
+++ examples/split-wasm/src/yew.rs
@@ -1,6 +1,7 @@
-use std::future::pending;
+use std::future::{pending, Future};
+use std::pin::Pin;
 
-use wasm_split::wasm_split as split;
+use wasm_split_helpers::wasm_split as split;
 use web_sys::HtmlInputElement;
 use yew::lazy::{Lazy, LazyComponent, LazyVTable};
 use yew::prelude::*;
@@ -22,15 +23,37 @@ fn Addition() -> Html {
     }
 }
 
-impl LazyComponent for Addition {
-    async fn fetch() -> LazyVTable<Self> {
+struct LazyAdditionProxy;
+
+impl LazyComponent for LazyAdditionProxy {
+    type Underlying = Addition;
+
+    async fn fetch() -> LazyVTable<Self::Underlying> {
         #[split(lazy_addition)]
         fn split_fetch() -> LazyVTable<Addition> {
             LazyVTable::<Addition>::vtable()
         }
-        split_fetch().await
+        struct F(Option<Pin<Box<dyn Future<Output = LazyVTable<Addition>> + Send>>>);
+        impl Future for F {
+            type Output = LazyVTable<Addition>;
+
+            fn poll(
+                mut self: Pin<&mut Self>,
+                cx: &mut std::task::Context<'_>,
+            ) -> std::task::Poll<Self::Output> {
+                self.0
+                    .get_or_insert_with(|| Box::pin(split_fetch()))
+                    .as_mut()
+                    .poll(cx)
+            }
+        }
+        static CACHE: async_once_cell::Lazy<LazyVTable<Addition>, F> =
+            async_once_cell::Lazy::new(F(None));
+        *Pin::static_ref(&CACHE).await.get_ref()
     }
 }
+
+type LazyAddition = Lazy<LazyAdditionProxy>;
 
 #[component]
 fn Pending() -> HtmlResult {
@@ -46,7 +69,7 @@ fn App() -> Html {
         <input id="additional" type="checkbox" checked={*toggle} oninput={move |ev: InputEvent| toggle.set(ev.target_unchecked_into::<HtmlInputElement>().checked())} />
         <label for="additional">{"Display additional content"}</label>
         <Suspense fallback={html! { <p>{"not yet loaded"}</p> }}>
-            if show { <Lazy<Addition> /> } else { <Pending /> }
+            if show { <LazyAddition /> } else { <Pending /> }
         </Suspense>
         </>
     }
