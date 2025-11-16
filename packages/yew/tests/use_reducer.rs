1b73c2f336e233f2e4d0420281364b3fec5dcab6
Pascal Sommer <p@pascalsommer.ch>
2025-11-16 14:23:46 +0100

    add failing test

--- packages/yew/tests/use_reducer.rs
+++ packages/yew/tests/use_reducer.rs
@@ -162,3 +162,94 @@ async fn use_reducer_eq_works() {
     let result = obtain_result();
     assert_eq!(result.as_str(), "3");
 }
+
+enum SometimesChangeAction {
+    /// If this action is sent, the state will remain the same
+    Keep,
+    /// If this action is sent, the state will change
+    Change,
+}
+
+/// A state that does not implement PartialEq
+#[derive(Clone)]
+struct SometimesChangingState {
+    value: i32,
+}
+
+impl Reducible for SometimesChangingState {
+    type Action = SometimesChangeAction;
+
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        use SometimesChangeAction::*;
+        match action {
+            Keep => self,
+            Change => {
+                let mut self_: Self = (*self).clone();
+                self_.value += 1;
+                self_.into()
+            }
+        }
+    }
+}
+
+#[wasm_bindgen_test]
+async fn use_reducer_does_not_rerender_when_rc_is_reused() {
+    #[component(UseReducerComponent)]
+    fn use_reducer_comp() -> Html {
+        let state = use_reducer(|| SometimesChangingState { value: 0 });
+        let render_count = use_mut_ref(|| 0);
+
+        let render_count = {
+            let mut render_count = render_count.borrow_mut();
+            *render_count += 1;
+
+            *render_count
+        };
+
+        let keep_state = {
+            let state = state.clone();
+            Callback::from(move |_| state.dispatch(SometimesChangeAction::Keep))
+        };
+
+        let change_state = Callback::from(move |_| state.dispatch(SometimesChangeAction::Change));
+
+        html! {
+            <>
+                <div>
+                    {"This component has been rendered: "}<span id="result">{render_count}</span>{" Time(s)."}
+                </div>
+                <button onclick={keep_state} id="keep-state">{"Keep State"}</button>
+                <button onclick={change_state} id="change-state">{"Change State"}</button>
+            </>
+        }
+    }
+
+    yew::Renderer::<UseReducerComponent>::with_root(
+        document().get_element_by_id("output").unwrap(),
+    )
+    .render();
+    sleep(Duration::ZERO).await;
+
+    let result = obtain_result();
+    assert_eq!(result.as_str(), "1");
+
+    document()
+        .get_element_by_id("change-state")
+        .unwrap()
+        .unchecked_into::<HtmlElement>()
+        .click();
+    sleep(Duration::ZERO).await;
+
+    let result = obtain_result();
+    assert_eq!(result.as_str(), "2");
+
+    document()
+        .get_element_by_id("keep-state")
+        .unwrap()
+        .unchecked_into::<HtmlElement>()
+        .click();
+    sleep(Duration::ZERO).await;
+
+    let result = obtain_result();
+    assert_eq!(result.as_str(), "2");
+}
