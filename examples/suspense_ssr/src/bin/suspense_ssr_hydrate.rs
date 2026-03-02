45a31f1cce369db2ed1f55881775f5b8366d3ffe
Matt Yan <syan4@ualberta.ca>
2026-03-02 21:51:16 +0900

    migrate ssr examples from wasm_logger to tracing-web
    this aligns with our template repo and works on both the frontend and backend

--- examples/suspense_ssr/src/bin/suspense_ssr_hydrate.rs
+++ examples/suspense_ssr/src/bin/suspense_ssr_hydrate.rs
@@ -1,7 +1,20 @@
 use suspense_ssr::App;
+use tracing_subscriber::filter::Targets;
+use tracing_subscriber::prelude::*;
+use tracing_web::MakeWebConsoleWriter;
 
 fn main() {
-    #[cfg(target_arch = "wasm32")]
-    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(true)
+        .without_time()
+        .with_writer(MakeWebConsoleWriter::new())
+        .with_filter(
+            Targets::new()
+                .with_target("yew", tracing::Level::DEBUG)
+                .with_default(tracing::Level::TRACE),
+        );
+
+    tracing_subscriber::registry().with(fmt_layer).init();
+
     yew::Renderer::<App>::new().hydrate();
 }
